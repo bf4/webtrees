bf4d44121d0cfaeed5597ba084e7e3f929fbbb34
NAME <USER@DOMAIN>
2006-05-22 15:48:11 +0000

    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@37 4201c862-f99b-1943-94d0-25fd05010cbc

---
+++ languages/help_text.ar.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Arabic Language file for PhpGedView.
+ *
+ * phpGedView: Genealogy Viewer
+ * Copyright (C) 2002 to 2005  Ezz (sfezz)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @package PhpGedView
+ * @author Ezz (sfezz)
+ * @version $Id: help_text.ar.php,v 1.1.2.4 2005/12/07 19:01:58 yalnifj Exp $
+ */
+
+if (preg_match("/help_text\...\.php$/", $_SERVER["SCRIPT_NAME"])>0) {
+print "You cannot access a language file directly.";
+exit;
+}
+
+?>
