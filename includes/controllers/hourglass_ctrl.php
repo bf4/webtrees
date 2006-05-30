4b69523dca17c0c6531541a82979431f47ef45f6
NAME <USER@DOMAIN>
2006-05-30 20:48:53 +0000

    Changed require into require_once when including functions_charts.php
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@80 4201c862-f99b-1943-94d0-25fd05010cbc

--- includes/controllers/hourglass_ctrl.php
+++ includes/controllers/hourglass_ctrl.php
@@ -28,7 +28,7 @@
 require_once("config.php");
 require_once 'includes/controllers/basecontrol.php';
 require_once 'includes/person_class.php';
-require("includes/functions_charts.php");
+require_once("includes/functions_charts.php");
 require($factsfile["english"]);
 if (file_exists( $factsfile[$LANGUAGE])) require  $factsfile[$LANGUAGE];
 
