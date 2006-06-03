2100505582b57036573c1755a512446bd8f8b435
NAME <USER@DOMAIN>
2006-06-03 11:00:43 +0000

    Norwegian language file
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@83 4201c862-f99b-1943-94d0-25fd05010cbc

---
+++ modules/googlemap/languages/lang.no.php
@@ -0,0 +1,30 @@
+<?php
+/*=================================================
+   charset=utf-8
+   Project:         phpGedView
+   File:            lang.no.php
+   Author:          Johan Borkhuis
+   Comments:        Norwegian Language file for Google map module
+===================================================*/
+
+
+$pgv_lang["googlemap"]              = "Kart";
+$pgv_lang["no_gmtab"]               = "Det er ingen kart-referanser for denne personen";
+
+$pgv_lang['gm_redraw_map']          = "Tegn kartet på nytt";
+$pgv_lang["gm_map"]                 = "Kart";
+$pgv_lang["gm_satellite"]           = "Satelitt";
+$pgv_lang["gm_hybrid"]              = "Kombinert";
+
+// Configuration texts
+$pgv_lang["gm_manage"]              = "Innstillinger for Googlemap";
+$pgv_lang["configure_googlemap"]    = "Googlemap innstillinger";
+$pgv_lang["gm_admin_error"]         = "Side bare for administratorer";
+$pgv_lang["googlemapkey"]           = "Nøkkel til Google Map API";
+$pgv_lang["gm_map_type"]            = "Standard karttype";
+$pgv_lang["gm_map_size"]            = "Størrelse på kartet (i punkter)";
+$pgv_lang["gm_map_size_x"]          = "Bredde";
+$pgv_lang["gm_map_size_y"]          = "Høyde";
+$pgv_lang["gm_map_zoom"]            = "Zoom-faktor for kart";
+
+?>
