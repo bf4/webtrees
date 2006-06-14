f56edd6f65bd6ea8d3fb346621969043ff6a2733
NAME <USER@DOMAIN>
2006-06-14 02:10:37 +0000

    Updated
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@93 4201c862-f99b-1943-94d0-25fd05010cbc

--- modules/googlemap/languages/lang.no.php
+++ modules/googlemap/languages/lang.no.php
@@ -1,30 +1,51 @@
 <?php
 /*=================================================
-   charset=utf-8
-   Project:         phpGedView
-   File:            lang.no.php
-   Author:          Johan Borkhuis
-   Comments:        Norwegian Language file for Google map module
+   charset=  utf-8
+   Project:  phpGedView
+   File:     lang.no.php
+   Author:   Johan Borkhuis
+   Comments: Norwegian Language file for Google map module
 ===================================================*/
 
 
 $pgv_lang["googlemap"]              = "Kart";
-$pgv_lang["no_gmtab"]               = "Det er ingen kart-referanser for denne personen";
+$pgv_lang["no_gmtab"]               = "Det er ingen kartreferanser knyttet til denne personen";
 
-$pgv_lang['gm_redraw_map']          = "Tegn kartet på nytt";
+$pgv_lang['gm_redraw_map']          = "Tegn kartet pÃ¥ nytt";
 $pgv_lang["gm_map"]                 = "Kart";
 $pgv_lang["gm_satellite"]           = "Satelitt";
 $pgv_lang["gm_hybrid"]              = "Kombinert";
 
 // Configuration texts
-$pgv_lang["gm_manage"]              = "Innstillinger for Googlemap";
-$pgv_lang["configure_googlemap"]    = "Googlemap innstillinger";
+$pgv_lang["gm_manage"]              = "Innstillinger for Google Map";
+$pgv_lang["configure_googlemap"]    = "Google Map innstillinger";
 $pgv_lang["gm_admin_error"]         = "Side bare for administratorer";
-$pgv_lang["googlemapkey"]           = "Nøkkel til Google Map API";
+$pgv_lang["gm_db_error"]            = "Fant ikke tabellen for kartreferanser til steder i databasen";
+$pgv_lang["gm_table_created"]       = "Tabell opprettet for kartreferanser til steder";
+$pgv_lang["googlemapkey"]           = "NÃ¸kkel til Google Map API";
 $pgv_lang["gm_map_type"]            = "Standard karttype";
-$pgv_lang["gm_map_size"]            = "Størrelse på kartet (i punkter)";
+$pgv_lang["gm_map_size"]            = "StÃ¸rrelse pÃ¥ kartet (i punkter)";
 $pgv_lang["gm_map_size_x"]          = "Bredde";
-$pgv_lang["gm_map_size_y"]          = "Høyde";
+$pgv_lang["gm_map_size_y"]          = "HÃ¸yde";
 $pgv_lang["gm_map_zoom"]            = "Zoom-faktor for kart";
+
+// Texts used on the Places location page
+$pgv_lang["edit_place_locations"]   = "Rediger kartreferanser til steder";
+$pgv_lang["pl_no_places_found"]     = "Fant ingen steder";
+$pgv_lang["pl_zoom_factor"]         = "Zoom-faktor";
+$pgv_lang["pl_place_icon"]          = "Ikon";
+$pgv_lang["pl_edit"]                = "Rediger kartreferanse";
+$pgv_lang["pl_add_place"]           = "Legg til sted";
+$pgv_lang["pl_import_gedcom"]       = "Importer fra aktiv slektsbase (GED)";
+$pgv_lang["pl_import_all_gedcom"]   = "Importer fra alle slektsbaser (GED)";
+$pgv_lang["pl_import_file"]         = "Importer fra fil";
+$pgv_lang["pl_export_file"]         = "Eksporter disse til en fil";
+$pgv_lang["pl_export_all_file"]     = "Eksporter alle kartreferanser til en fil";
+$pgv_lang["pl_north_short"]         = "N";
+$pgv_lang["pl_south_short"]         = "S";
+$pgv_lang["pl_east_short"]          = "Ã˜";
+$pgv_lang["pl_west_short"]          = "V";
+$pgv_lang["pl_places_filename"]     = "Fil som inneholder steder (CSV)";
+$pgv_lang["pl_clean_db"]            = "Fjerne alle lagrede kartreferanser til steder fÃ¸r import?";
 
 ?>
