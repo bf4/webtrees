353964b768f977130fcd883e690fa08ba2eb2ce3
NAME <USER@DOMAIN>
2006-06-14 02:25:25 +0000

    Updated
    git-svn-id: svn://svn.webtrees.net/trunk@94 4201c862-f99b-1943-94d0-25fd05010cbc

--- phpGedView/modules/googlemap/languages/help_text.no.php
+++ phpGedView/modules/googlemap/languages/help_text.no.php
@@ -24,20 +24,20 @@
  * @version $Id$
  */
 if (preg_match("/help_text\...\.php$/", $_SERVER["SCRIPT_NAME"])>0) {
-  print "Du kan ikke tilgang til en språkfil direkte.";
+  print "Du kan ikke tilgang til en sprÃ¥kfil direkte.";
   exit;
 }
 
-$pgv_lang["GOOGLEMAP_API_KEY"]          = "Google kart - API-nøkkel";
-$pgv_lang["GOOGLEMAP_API_KEY_help"]     = "~#pgv_lang[GOOGLEMAP_API_KEY]#~<br /><br />Oppgi nøkkelen din til Google Map API her.  Dersom du ikke har en nøkkel, kan du be om å få en på nettsiden til <a target=\"_blank\" href=\"http://www.google.com/apis/maps/\">Google kart</a>";
+$pgv_lang["GOOGLEMAP_API_KEY"]          = "Google kart - API-nÃ¸kkel";
+$pgv_lang["GOOGLEMAP_API_KEY_help"]     = "~#pgv_lang[GOOGLEMAP_API_KEY]#~<br /><br />Oppgi nÃ¸kkelen din til Google Map API her.  Dersom du ikke har en nÃ¸kkel, kan du be om Ã¥ fÃ¥ en pÃ¥ nettsiden til <a target=\"_blank\" href=\"http://www.google.com/apis/maps/\">Google kart</a>";
 
 $pgv_lang["GOOGLEMAP_MAP_TYPE"]	        = "Google kart - type";
 $pgv_lang["GOOGLEMAP_MAP_TYPE_help"]    = "~#pgv_lang[GOOGLEMAP_MAP_TYPE]#~<br /><br />Type kart som vil bli vist som standard.  Valgene er Kart, Satelitt eller Kombinert.";
 
-$pgv_lang["GOOGLEMAP_MAP_SIZE"]	        = "Google kart - størrelse";
-$pgv_lang["GOOGLEMAP_MAP_SIZE_help"]    = "~#pgv_lang[GOOGLEMAP_MAP_SIZE]#~<br /><br />Størrelsen på kartet (i punkter) som vil vises på siden med opplysninger for en person.";
+$pgv_lang["GOOGLEMAP_MAP_SIZE"]	        = "Google kart - stÃ¸rrelse";
+$pgv_lang["GOOGLEMAP_MAP_SIZE_help"]    = "~#pgv_lang[GOOGLEMAP_MAP_SIZE]#~<br /><br />StÃ¸rrelsen pÃ¥ kartet (i punkter) som vil vises pÃ¥ siden med opplysninger for en person.";
 
 $pgv_lang["GOOGLEMAP_MAP_ZOOM"]	        = "Google kart - zoom-faktor";
-$pgv_lang["GOOGLEMAP_MAP_ZOOM_help"]    = "~#pgv_lang[GOOGLEMAP_MAP_ZOOM]#~<br /><br />Her angir du hvilken forstørrelse kortet skal vise med.<br />1 viser hele jordkloden<br />15 viser enkelte hus<br /><br />NB! Det er ikke alle områder som har tilgjengelig kart som kan vise forstørrelse på 15.";
+$pgv_lang["GOOGLEMAP_MAP_ZOOM_help"]    = "~#pgv_lang[GOOGLEMAP_MAP_ZOOM]#~<br /><br />Her angir du hvilken forstÃ¸rrelse kortet skal vise med.<br />1 viser hele jordkloden<br />15 viser enkelte hus<br /><br />NB! Det er ikke alle omrÃ¥der som har tilgjengelig kart som kan vise forstÃ¸rrelse pÃ¥ 15.";
 
 ?>
