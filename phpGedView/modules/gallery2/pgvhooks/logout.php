9bdbe90b4125cdae19770c0fdd7c6f302e379f1c
NAME <USER@DOMAIN>
2006-06-20 21:33:09 +0000

    Fixed error when gallery2 is not installed.
    git-svn-id: svn://svn.webtrees.net/trunk@124 4201c862-f99b-1943-94d0-25fd05010cbc

--- phpGedView/modules/gallery2/pgvhooks/logout.php
+++ phpGedView/modules/gallery2/pgvhooks/logout.php
@@ -1,9 +1,11 @@
 <?php
-require_once 'modules/gallery2/embed.php';
+if(file_exists('modules/gallery2/embed.php')){require_once 'modules/gallery2/embed.php';}
+
 class mod_gallery2_logout
 {
 	function hook($params)
 	{
+		if(!file_exists('modules/gallery2/embed.php')){return null;}
 		$g2ret = GalleryEmbed::logout();
 	}
 }
