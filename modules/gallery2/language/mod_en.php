59579f40c71b88f9be18ec01a150b640e146cfd6
NAME <USER@DOMAIN>
2006-06-23 22:37:36 +0000

    Adding missing language file.
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@141 4201c862-f99b-1943-94d0-25fd05010cbc

---
+++ modules/gallery2/language/mod_en.php
@@ -0,0 +1,5 @@
+<?php
+$pgv_lang['mod_gallery2_error_user_create'] = "An error occurred during the on-the-fly user creation.";
+$pgv_lang['mod_gallery2_error_user_check'] = "An error occurred while checking if a user already exists.";
+$pgv_lang['mod_gallery2_error_init'] = "An error occurred while trying to initialize Gallery.";
+?>
