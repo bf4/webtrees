f73ec77dbf8d01063569de3b1d03a7310d7a6587
NAME <USER@DOMAIN>
2006-06-30 22:05:31 +0000

    Added Hebrew translation
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@192 4201c862-f99b-1943-94d0-25fd05010cbc

---
+++ modules/googlemap/languages/lang.he.php
@@ -0,0 +1,71 @@
+<?php
+/*=================================================
+   charset=utf-8
+   Project:         phpGedView
+   File:            lang.en.php
+   Author:          Johan Borkhuis
+   Translator:		Meliza Amity
+   Comments:        Hebrew Language file for Google map module
+===================================================*/
+
+
+$pgv_lang["googlemap"]              	= "מפה";
+$pgv_lang["no_gmtab"]               	= "אין נתוני מפה עבור אדם זה";
+
+$pgv_lang['gm_redraw_map']          	= "צייר מפה מחדש";
+$pgv_lang["gm_map"]                 	= "מפה";
+$pgv_lang["gm_satellite"]           	= "לויין";
+$pgv_lang["gm_hybrid"]              	= "מעורב";
+
+// Configuration texts
+$pgv_lang["gm_manage"]              	= "נהל תצורת Googlemap";
+$pgv_lang["configure_googlemap"]    	= "תצורת Googlemap";
+$pgv_lang["gm_admin_error"]         	= "דף רק של מנהלנים";
+$pgv_lang["gm_db_error"]            	= "טבלת מיקום לא נימצאה במאגר הנתונים";
+$pgv_lang["gm_table_created"]       	= "טבלת מיקום נוצרה";
+$pgv_lang["googlemapkey"]           	= "מפתח API של Google Map";
+$pgv_lang["gm_map_type"]            	= "ברירת מחדל של סוג המפה";
+$pgv_lang["gm_map_size"]            	= "גודל המפה (בפיקסלים)";
+$pgv_lang["gm_map_size_x"]          	= "רוחב";
+$pgv_lang["gm_map_size_y"]          	= "גובה";
+$pgv_lang["gm_map_zoom"]            	= "מקדם זום של המפה";
+$pgv_lang["gm_digits"]              		= "ספרות";
+
+$pgv_lang["GOOGLEMAP_API_KEY_help"]	= "~#pgv_lang[googlemapkey]#~<br /><br />הכנס כאן את מפתח ה-API של ה-Google Map שלך.";
+
+// Texts used on the Places location page
+$pgv_lang["edit_place_locations"]   	= "ערוך מיקומים";
+$pgv_lang["pl_no_places_found"]     	= "לא נמצאו מקומות";
+$pgv_lang["pl_zoom_factor"]         	= "מקדם זום";
+$pgv_lang["pl_place_icon"]          	= "צלמית";
+$pgv_lang["pl_edit"]                		= "ערוך מיקום";
+$pgv_lang["pl_add_place"]           	= "הוסף מקום";
+$pgv_lang["pl_import_gedcom"]       	= "ייבא מקובץ ה-GEDCOM הנוכחי";
+$pgv_lang["pl_import_all_gedcom"]   	= "ייבא מכל קובצי ה-GEDCOM";
+$pgv_lang["pl_import_file"]         	= "ייבא מקובץ";
+$pgv_lang["pl_export_file"]         	= "ייצא את המבט הנוכחי לקובץ";
+$pgv_lang["pl_export_all_file"]     	= "ייצא את כל המיקומים לקובץ";
+$pgv_lang["pl_north_short"]         	= "צפון";
+$pgv_lang["pl_south_short"]         	= "דרום";
+$pgv_lang["pl_east_short"]          	= "מזרח";
+$pgv_lang["pl_west_short"]          	= "מערב";
+$pgv_lang["pl_places_filename"]     	= "הקובץ כולל מיקומים (CSV)";
+$pgv_lang["pl_clean_db"]            	= "האם לנקות את כל המיקומים לפני ייבוא?";
+$pgv_lang["pl_no_places_found"]     	= "לא נמצאו מקומות";
+$pgv_lang["pl_use_this_value"]      	= "השתמש בערך הזה";
+$pgv_lang["pl_precision"]           	= "דיוק";
+$pgv_lang["pl_country"]             	= "ארץ";
+$pgv_lang["pl_state"]               		= "מדינה";
+$pgv_lang["pl_city"]                		= "עיר";
+$pgv_lang["pl_neighborhood"]        	= "שכונה";
+$pgv_lang["pl_house"]               	= "בית";
+$pgv_lang["pl_max"]                 		= "מכסימלי";
+
+$pgv_lang["pl_flag"]                		= "דגל";
+$pgv_lang["flags_edit"]             		= "בחר דגל";
+$pgv_lang["pl_change_flag"]         	= "החלף דגל";
+$pgv_lang["pl_remove_flag"]         	= "הסר דגל";
+
+$pgv_lang["pl_remove_location"]     	= "?הסר המיקום";
+$pgv_lang["pl_delete_error"]        	= "המיקום לא הורד: המיקום כולל תתי מיקומים";
+?>
