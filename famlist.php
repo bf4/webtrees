0b5af47cad2aee712a1e95a1d0d66a5f940f9f8d
NAME <USER@DOMAIN>
2006-07-21 18:26:12 +0000

    Setting bookmark title
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@280 4201c862-f99b-1943-94d0-25fd05010cbc

--- famlist.php
+++ famlist.php
@@ -42,10 +42,21 @@ $rlm = chr(0xE2).chr(0x80).chr(0x8F);
 if (isset($alpha)) {
 	$alpha = stripslashes($alpha);
 	$alpha = str_replace(array($lrm, $rlm), "", $alpha);
+	$doctitle = $pgv_lang["family_list"]." : ".$alpha;
 }
 if (isset($surname)) {
 	$surname = stripslashes($surname);
 	$surname = str_replace(array($lrm, $rlm), "", $surname);
+	$doctitle = $pgv_lang["family_list"]." : ".$surname;
+}
+if (isset($doctitle)) {
+	?>
+	<script language="JavaScript" type="text/javascript">
+	<!--
+		document.title = '<?php print $doctitle; ?>';
+	//-->
+	</script>
+	<?php
 }
 if (empty($show_all_firstnames)) $show_all_firstnames = "no";
 if (empty($DEBUG)) $DEBUG = false;
