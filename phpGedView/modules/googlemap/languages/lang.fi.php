c22d9fbdbe688b06e5034468b200915bcb7e0f1c
NAME <USER@DOMAIN>
2006-07-23 09:56:28 +0000

    Adding basic translations
    git-svn-id: svn://svn.webtrees.net/trunk@292 4201c862-f99b-1943-94d0-25fd05010cbc

---
+++ phpGedView/modules/googlemap/languages/lang.fi.php
@@ -0,0 +1,18 @@
+<?php
+/*=================================================
+   charset=utf-8
+   Project:         phpGedView
+   File:            lang.en.php
+   Author:          Johan Borkhuis
+   Translator:      Matti Valve 
+   Comments:        English Language file for Google map module
+===================================================*/
+
+
+$pgv_lang["googlemap"]              = "Kartta";
+
+$pgv_lang['gm_redraw_map']          = "Näytä kartta uudelleen";
+$pgv_lang["gm_map"]                 = "Kartta";
+$pgv_lang["gm_satellite"]           = "Satelliitti";
+$pgv_lang["gm_hybrid"]              = "Hybridi";
+?>
---
+++ phpGedView/modules/googlemap/languages/lang.sv.php
@@ -0,0 +1,18 @@
+<?php
+/*=================================================
+   charset=utf-8
+   Project:         phpGedView
+   File:            lang.en.php
+   Author:          Johan Borkhuis
+   Translator:      Matti Valve 
+   Comments:        English Language file for Google map module
+===================================================*/
+
+
+$pgv_lang["googlemap"]              = "Karta";
+
+$pgv_lang['gm_redraw_map']          = "Visa kartan på nyt";
+$pgv_lang["gm_map"]                 = "Karta";
+$pgv_lang["gm_satellite"]           = "Satellit";
+$pgv_lang["gm_hybrid"]              = "Hybrid";
+?>
