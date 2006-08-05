0f8cf058d0ee0740da0c8619ae8e339c4720377e
NAME <USER@DOMAIN>
2006-08-05 11:05:19 +0000

    tranlated a few lines
    git-svn-id: svn://svn.webtrees.net/trunk@337 4201c862-f99b-1943-94d0-25fd05010cbc

--- phpGedView/modules/googlemap/languages/lang.sv.php
+++ phpGedView/modules/googlemap/languages/lang.sv.php
@@ -10,8 +10,10 @@
 
 
 $pgv_lang["googlemap"]              = "Karta";
+$pgv_lang["no_gmtab"]               = "Det finns ingen kartdata för denna person";
+$pgv_lang["gm_disabled"]            = "Googlekartmodulen är avstängd";
 
-$pgv_lang['gm_redraw_map']          = "Visa kartan på nyt";
+$pgv_lang['gm_redraw_map']          = "Visa kartan på nytt";
 $pgv_lang["gm_map"]                 = "Karta";
 $pgv_lang["gm_satellite"]           = "Satellit";
 $pgv_lang["gm_hybrid"]              = "Hybrid";
