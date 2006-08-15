3dc17052898aad5f9dc52ea1421e2aabbdc79462
NAME <USER@DOMAIN>
2006-08-15 22:14:19 +0000

    Added Sitemap generator as module
    git-svn-id: svn://svn.webtrees.net/trunk@356 4201c862-f99b-1943-94d0-25fd05010cbc

---
+++ phpGedView/modules/sitemap/languages/lang.en.php
@@ -0,0 +1,36 @@
+<?php
+/*=================================================
+   charset=utf-8
+   Project:         phpGedView
+   File:            lang.en.php
+   Author:          Johan Borkhuis
+   Comments:        English Language file for Sitemap
+===================================================*/
+
+
+// Admin menu
+$pgv_lang["generate_sitemap"]       = "Generate Sitemap files";
+
+// Sitemap pages
+$pgv_lang["selected_item"]          = "Selected items to store in Sitemap:";
+$pgv_lang["gedcoms_selected"]       = "GEDCOMS to store in Sitemap:";
+$pgv_lang["sitemaps_generated"]     = "The following sitemaps are generated and can be downloaded:";
+$pgv_lang["sitemaps_placement"]     = "Place all the files in the root of your phpGedView installation.";
+$pgv_lang["sm_indi_info"]			= "Individual Information";
+$pgv_lang["sm_family_info"]			= "Family Information";
+$pgv_lang["sm_individual_list"]		= "Individual List";
+$pgv_lang["sm_family_list"]			= "Family List";
+$pgv_lang["sm_item"]                = "Item";
+$pgv_lang["sm_priority"]            = "Priority";
+$pgv_lang["sm_updates"]             = "Updates";
+$pgv_lang["sm_always"]              = "always";
+$pgv_lang["sm_hourly"]              = "hourly";
+$pgv_lang["sm_daily"]               = "daily";
+$pgv_lang["sm_weekly"]              = "weekly";
+$pgv_lang["sm_monthly"]             = "monthly";
+$pgv_lang["sm_yearly"]              = "yearly";
+$pgv_lang["sm_never"]               = "never";
+$pgv_lang["sm_generate"]            = "Generate";
+
+
+?>
---
+++ phpGedView/modules/sitemap/languages/lang.nl.php
@@ -0,0 +1,35 @@
+<?php
+/*=================================================
+   charset=utf-8
+   Project:         phpGedView
+   File:            lang.nl.php
+   Author:          Johan Borkhuis
+   Comments:        Dutch Language file for Sitemap
+===================================================*/
+
+// Admin menu
+$pgv_lang["generate_sitemap"]       = "Genereer Sitemap files";
+
+// Sitemap pages
+$pgv_lang["selected_item"]          = "Onderdelen welke in de Sitemap file opgenomen worden:";
+$pgv_lang["gedcoms_selected"]       = "Van de volgende GEDCOM's wordt een Sitemap file gemaakt:";
+$pgv_lang["sitemaps_generated"]     = "De volgende sitemaps zijn gegenereerd en kunnen worden gedownload:";
+$pgv_lang["sitemaps_placement"]     = "Plaats de sitemap files in de directory waarin phpGedView is geinstalleerd.";
+$pgv_lang["sm_indi_info"]			= "Persoonsinformatie";
+$pgv_lang["sm_family_info"]			= "Gezinsinformatie";
+$pgv_lang["sm_individual_list"]		= "Personen lijst";
+$pgv_lang["sm_family_list"]			= "Familie lijst";
+$pgv_lang["sm_item"]                = "Onderdelen";
+$pgv_lang["sm_priority"]            = "Prioriteit";
+$pgv_lang["sm_updates"]             = "Wijzigingen";
+$pgv_lang["sm_always"]              = "continue";
+$pgv_lang["sm_hourly"]              = "elk uur";
+$pgv_lang["sm_daily"]               = "dagelijks";
+$pgv_lang["sm_weekly"]              = "wekelijks";
+$pgv_lang["sm_monthly"]             = "maandelijks";
+$pgv_lang["sm_yearly"]              = "jaarlijks";
+$pgv_lang["sm_never"]               = "nooit";
+$pgv_lang["sm_generate"]            = "Genereer";
+
+
+?>
