c73e005bbdcb02f55a67d1853f94599321a50581
NAME <USER@DOMAIN>
2007-03-21 22:47:21 +0000

    Fixing bugs
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@986 4201c862-f99b-1943-94d0-25fd05010cbc

--- edit_interface.php
+++ edit_interface.php
@@ -387,6 +387,7 @@ case 'editraw':
 			print $pgv_lang["admin_override"]."</td><td class=\"optionbox wrap\">\n";
 			print "<input type=\"checkbox\" name=\"preserve_last_changed\" />\n";
 			print $pgv_lang["no_update_CHAN"]."<br />\n";
+			print_fact_date(get_sub_record(1, "1 CHAN", $gedrec), false, true);
 			print "</td></tr>\n";
 			print "</table>";
 		}
@@ -418,6 +419,7 @@ case 'edit':
 		print $pgv_lang["admin_override"]."</td><td class=\"optionbox wrap\">\n";
 		print "<input type=\"checkbox\" name=\"preserve_last_changed\" />\n";
 		print $pgv_lang["no_update_CHAN"]."<br />\n";
+		print_fact_date(get_sub_record(1, "1 CHAN", $gedrec), false, true);
 		print "</td></tr>\n";
 		}
 	print "</table>";
@@ -459,6 +461,7 @@ case 'add':
 		print $pgv_lang["admin_override"]."</td><td class=\"optionbox wrap\">\n";
 		print "<input type=\"checkbox\" name=\"preserve_last_changed\" />\n";
 		print $pgv_lang["no_update_CHAN"]."<br />\n";
+		print_fact_date(get_sub_record(1, "1 CHAN", $gedrec), false, true);
 		print "</td></tr>\n";
 	}
 	print "</table>";
