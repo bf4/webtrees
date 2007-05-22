bc3395df34af194977c6d40cfdf9be11a05a01e1
NAME <USER@DOMAIN>
2007-05-22 22:56:30 +0000

    git-svn-id: svn://svn.webtrees.net/trunk@1107 4201c862-f99b-1943-94d0-25fd05010cbc

--- phpGedView/includes/functions_import.php
+++ phpGedView/includes/functions_import.php
@@ -287,7 +287,7 @@ function import_record($indirec, $update = false) {
 			for ($j = 0; $j < $ct; $j++) {
 				$chil .= $match[$j][1] . ";";
 			}
-			$nchi = get_gedcom_value("NCHI", 1, $this->gedrec);
+			$nchi = get_gedcom_value("NCHI", 1, $indirec);
 			if ($nchi!=$ct) $ct = $nchi;
 			$fam = array ();
 			$fam["HUSB"] = $parents["HUSB"];
