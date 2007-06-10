5d072b3324e8e624c45fe0b1632b2737304d1a2a
NAME <USER@DOMAIN>
2007-06-10 09:37:36 +0000

    Date entry in local languages (new version of code taken out in SVN1119)
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@1182 4201c862-f99b-1943-94d0-25fd05010cbc

--- includes/functions_edit.php
+++ includes/functions_edit.php
@@ -1565,6 +1565,14 @@ function breakConts($newline, $level) {
  * @return string	the converted date string
  */
 function check_input_date($datestr) {
+	global $lang_short_cut, $LANGUAGE;
+	// Convert from natural language to gedcom format
+	$conversion_function="edit_to_gedcom_date_{$lang_short_cut[$LANGUAGE]}";
+	if (function_exists($conversion_function))
+		$datestr=$conversion_function($datestr);
+	else
+		$datestr=default_edit_to_gedcom_date($datestr);
+
 	$dates = parse_date($datestr);
 	// If we couldn't parse the date, leave it alone.
 	foreach ($dates as $date)
@@ -1691,6 +1699,7 @@ function create_add_form($fact) {
 function create_edit_form($gedrec, $linenum, $level0type) {
 	global $WORD_WRAPPED_NOTES, $pgv_lang, $factarray;
 	global $tags, $ADVANCED_PLAC_FACTS, $date_and_time, $templefacts;
+	global $lang_short_cut, $LANGUAGE;
 
 	$gedlines = split("\n", $gedrec);	// -- find the number of lines in the record
 	$fields = preg_split("/\s/", $gedlines[$linenum]);
@@ -1739,6 +1748,14 @@ function create_edit_form($gedrec, $linenum, $level0type) {
 
 		if ($type!="DATA" && $type!="CONC" && $type!="CONT") {
 			$tags[]=$type;
+			if ($type=='DATE') {
+				// Allow the user to edit the date in his/her own natural language
+				$conversion_function="gedcom_to_edit_date_{$lang_short_cut[$LANGUAGE]}";
+				if (function_exists($conversion_function))
+					$text=$conversion_function($text);
+				else
+					$text=default_gedcom_to_edit_date($text);
+			}
 			$subrecord = $level." ".$type." ".$text;
 			if ($inSource && $type=="DATE") add_simple_tag($subrecord, "", $pgv_lang["date_of_entry"]);
 			else add_simple_tag($subrecord, $level0type);
