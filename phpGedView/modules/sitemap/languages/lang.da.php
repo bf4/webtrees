837b961c4c5dd1f517fae7f2d4cd1271c788aeb0
NAME <USER@DOMAIN>
2007-06-12 20:46:09 +0000

    Dannish language updates.
    git-svn-id: svn://svn.webtrees.net/trunk@1194 4201c862-f99b-1943-94d0-25fd05010cbc

---
+++ phpGedView/modules/sitemap/languages/lang.da.php
@@ -0,0 +1,37 @@
+<?php
+/*=================================================
+   charset=utf-8
+   Project:         phpGedView
+   File:            lang.da.php
+   Author:          Jens Hyllegaard
+   Comments:        Danish Language file for Sitemap
+===================================================*/
+
+
+// Admin menu
+$pgv_lang["generate_sitemap"]       = "Opret sitemap filer";
+
+// Sitemap pages
+$pgv_lang["selected_item"]          = "Valgte emner vil blive gemt i sitemap:";
+$pgv_lang["gedcoms_selected"]       = "GEDCOM'er der skal gemmes i sitemap:";
+$pgv_lang["sitemaps_generated"]     = "Følgende sitemap er oprettet og kan downloades:";
+$pgv_lang["sitemaps_placement"]     = "Placer alle filerne i roden af din phpGedView installation.";
+$pgv_lang["sm_indi_info"]			= "Individ information";
+$pgv_lang["sm_family_info"]			= "Familie information";
+$pgv_lang["sm_individual_list"]		= "Individliste";
+$pgv_lang["sm_family_list"]			= "Famillieliste";
+$pgv_lang["sm_item"]                = "Emne";
+$pgv_lang["sm_priority"]            = "Prioritet";
+$pgv_lang["sm_updates"]             = "Opdateres";
+$pgv_lang["sm_always"]              = "altid";
+$pgv_lang["sm_hourly"]              = "per time";
+$pgv_lang["sm_daily"]               = "dagligt";
+$pgv_lang["sm_weekly"]              = "ugentligt";
+$pgv_lang["sm_monthly"]             = "månedligt";
+$pgv_lang["sm_yearly"]              = "årligt";
+$pgv_lang["sm_never"]               = "aldrig";
+$pgv_lang["sm_generate"]            = "Opret";
+$pgv_lang["gedcoms_privacy"]        = "Ingen links til private oplysninger";
+
+
+?>
