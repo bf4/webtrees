8d24ffb71e13a05da1fdf4d50ba3c84a41572052
NAME <USER@DOMAIN>
2007-08-18 12:55:43 +0000

    git-svn-id: svn://svn.webtrees.net/trunk@1453 4201c862-f99b-1943-94d0-25fd05010cbc

--- phpGedView/languages/lang.en.php
+++ phpGedView/languages/lang.en.php
@@ -33,7 +33,7 @@ if (stristr($_SERVER["SCRIPT_NAME"], basename(__FILE__))!==false) {
 
 $pgv_lang["differences"]			= "Differences";
 $pgv_lang["charts_block"]			= "Charts Block";
-$pgv_lang["charts_block_descr"]		= "The Charts Block allows you to place a chart embedded into a block on the welcome page or on the MyGedView portal.  You can configure the block to show an ancestors, descendants, or hourglass view.  You can also choose the root person for the chart.";
+$pgv_lang["charts_block_descr"]		= "The Charts block allows you to place a chart on the Welcome or the MyGedView portal page.  You can configure the block to show an ancestors, descendants, or hourglass view.  You can also choose the root person for the chart.";
 $pgv_lang["chart_type"]				= "Chart Type";
 $pgv_lang["changedate1"]			= "Ending range of change dates";
 $pgv_lang["changedate2"]			= "Starting range of change dates";
