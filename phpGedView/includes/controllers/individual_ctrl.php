9b2642d9a44e4f200d80b664776381d6d432ec68
NAME <USER@DOMAIN>
2007-08-22 16:44:37 +0000

    Fix approve changes on mediaviewer goes to index page.
    git-svn-id: svn://svn.webtrees.net/trunk@1485 4201c862-f99b-1943-94d0-25fd05010cbc

--- phpGedView/includes/controllers/individual_ctrl.php
+++ phpGedView/includes/controllers/individual_ctrl.php
@@ -288,7 +288,7 @@ class IndividualControllerRoot extends BaseController {
 			$this->accept_success=true;
 			//-- delete the record from the cache and refresh it
 			if (isset($indilist[$this->pid])) unset($indilist[$this->pid]);
-			$indirec = find_person_record($this->pid);
+			$indirec = find_gedcom_record($this->pid);
 			//-- check if we just deleted the record and redirect to index
 			if (empty($indirec)) {
 				header("Location: index.php?ctype=gedcom");
