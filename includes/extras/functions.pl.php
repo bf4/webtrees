dd3ba6c15928e766afb28443aaceb93edf9314e5
NAME <USER@DOMAIN>
2007-09-13 10:16:24 +0000

    Polish date localisation
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@1652 4201c862-f99b-1943-94d0-25fd05010cbc

--- includes/extras/functions.pl.php
+++ includes/extras/functions.pl.php
@@ -19,7 +19,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  * @package PhpGedView
- * @version $Id:$
+ * @version $Id$
  */
 
 if (stristr($_SERVER["SCRIPT_NAME"], basename(__FILE__))!==false) {
@@ -32,79 +32,51 @@ if (stristr($_SERVER["SCRIPT_NAME"], basename(__FILE__))!==false) {
 ////////////////////////////////////////////////////////////////////////////////
 function date_localisation_pl(&$q1, &$d1, &$q2, &$d2, &$q3) {
 	global $pgv_lang;
-	static $NOMINATIVE_POLISH_MONTHS=NULL;
-	static $GENITIVE_POLISH_MONTHS=NULL;
-	static $INSTRUMENTAL_POLISH_MONTHS=NULL;
-	static $LOCATIVE_POLISH_MONTHS=NULL;
+	static $NOMINATIVE_MONTHS=NULL;
+	static $GENITIVE_MONTHS=NULL;
+	static $INSTRUMENTAL_MONTHS=NULL;
+	static $LOCATIVE_MONTHS=NULL;
 
-	if (empty($NOMINATIVE_POLISH_MONTHS)) {
-		$NOMINATIVE_POLISH_MONTHS=array($pgv_lang['jan'], $pgv_lang['feb'], $pgv_lang['mar'], $pgv_lang['apr'], $pgv_lang['may'], $pgv_lang['jun'], $pgv_lang['jul'], $pgv_lang['aug'], $pgv_lang['sep'], $pgv_lang['oct'], $pgv_lang['nov'], $pgv_lang['dec']);
-		$GENITIVE_POLISH_MONTHS=array('stycznia', 'lutego', 'marca', 'kwietnia', 'maja', 'czerwca', 'lipca', 'sierpnia', 'września', 'października', 'listopada', 'grudnia');
-		$INSTRUMENTAL_POLISH_MONTHS=array('styczniem', 'lutym', 'marcem', 'kwietniem', 'majem', 'czerwcem', 'lipcem', 'sierpniem', 'wrześniem', 'październikiem', 'listopadem', 'grudniem');
-		$LOCATIVE_POLISH_MONTHS=array('styczniu', 'lutym', 'marcu', 'kwietniu', 'maju', 'czerwcu', 'lipcu', 'sierpniu', 'wrześniu', 'październiku', 'listopadzie', 'grudniu');
+	if (empty($NOMINATIVE_MONTHS)) {
+		$NOMINATIVE_MONTHS=array($pgv_lang['jan'], $pgv_lang['feb'], $pgv_lang['mar'], $pgv_lang['apr'], $pgv_lang['may'], $pgv_lang['jun'], $pgv_lang['jul'], $pgv_lang['aug'], $pgv_lang['sep'], $pgv_lang['oct'], $pgv_lang['nov'], $pgv_lang['dec']);
+		$GENITIVE_MONTHS=array('stycznia', 'lutego', 'marca', 'kwietnia', 'maja', 'czerwca', 'lipca', 'sierpnia', 'września', 'października', 'listopada', 'grudnia');
+		$INSTRUMENTAL_MONTHS=array('styczniem', 'lutym', 'marcem', 'kwietniem', 'majem', 'czerwcem', 'lipcem', 'sierpniem', 'wrześniem', 'październikiem', 'listopadem', 'grudniem');
+		$LOCATIVE_MONTHS=array('styczniu', 'lutym', 'marcu', 'kwietniu', 'maju', 'czerwcu', 'lipcu', 'sierpniu', 'wrześniu', 'październiku', 'listopadzie', 'grudniu');
 	}
 
-	// Dates with a number are genitive
-	foreach ($NOMINATIVE_POLISH_MONTHS as $key=>$value) {
-		$d1=preg_replace("/(\d+ ){$value}/", "$1 ".$GENITIVE_POLISH_MONTHS[$key], $d1);
-		$d2=preg_replace("/(\d+ ){$value}/", "$1 ".$GENITIVE_POLISH_MONTHS[$key], $d2);
+	// Months with a day number are genitive, regardless of qualifier
+	for ($i=0; $i<12; ++$i) {
+		$d1=preg_replace("/(\d+ ){$NOMINATIVE_MONTHS[$i]}/", "$1{$GENITIVE_MONTHS[$i]}", $d1);
+		$d2=preg_replace("/(\d+ ){$NOMINATIVE_MONTHS[$i]}/", "$1{$GENITIVE_MONTHS[$i]}", $d2);
 	}
-	// Dates without a number depend on the qualifier
+
+	// Months without a day number (i.e. month at start) depend on the qualifier
 	switch ($q1) {
-		case 'est': case 'int': case 'cal': case '':
-			foreach ($NOMINATIVE_POLISH_MONTHS as $key=>$value) {
-				$date['month']=$NOMINATIVE_POLISH_MONTHS[$date['mon']];
-			}
-			break;
-		case 'from': case 'to': case 'abt': case 'apx': case 'cir':
-			foreach ($NOMINATIVE_POLISH_MONTHS as $key=>$value) {
-				$date['month']=$GENITIVE_POLISH_MONTHS[$date['mon']];
-			}
-			break;
-		case 'bet': case 'bef':
-			foreach ($NOMINATIVE_POLISH_MONTHS as $key=>$value) {
-				$date['month']=$INSTRUMENTAL_POLISH_MONTHS[$date['mon']];
-			}
-			break;
-		case 'aft':
-			foreach ($NOMINATIVE_POLISH_MONTHS as $key=>$value) {
-				$date['month']=$LOCATIVE_POLISH_MONTHS[$date['mon']];
-			}
-	 		break;
-		}
+	case 'from': case 'to': case 'abt': case 'apx': case 'cir':
+		for ($i=0; $i<12; ++$i)
+			$d1=preg_replace("/^{$NOMINATIVE_MONTHS[$i]}/", $GENITIVE_MONTHS[$i], $d1);
+		break;
+	case 'bet': case 'bef':
+		for ($i=0; $i<12; ++$i)
+			$d1=preg_replace("/^{$NOMINATIVE_MONTHS[$i]}/", $INSTRUMENTAL_MONTHS[$i], $d1);
+		break;
+	case 'aft':
+		for ($i=0; $i<12; ++$i)
+			$d1=preg_replace("/^{$NOMINATIVE_MONTHS[$i]}/", $LOCATIVE_MONTHS[$i], $d1);
+	 	break;
+	}
 	switch ($q2) {
-		case 'to':
-			foreach ($NOMINATIVE_POLISH_MONTHS as $key=>$value) {
-				$date['month']=$GENITIVE_POLISH_MONTHS[$date['mon']];
-			}
-			break;
-		case 'and':
-			foreach ($NOMINATIVE_POLISH_MONTHS as $key=>$value) {
-				$date['month']=$INSTRUMENTAL_POLISH_MONTHS[$date['mon']];
-			}
-			break;
-		}
+	case 'to':
+		for ($i=0; $i<12; ++$i)
+			$d2=preg_replace("/^{$NOMINATIVE_MONTHS[$i]}/", $GENITIVE_MONTHS[$i], $d2);
+		break;
+	case 'and':
+		for ($i=0; $i<12; ++$i)
+			$d2=preg_replace("/^{$NOMINATIVE_MONTHS[$i]}/", $INSTRUMENTAL_MONTHS[$i], $d2);
+		break;
 	}
 
-	if ($date['day']!=0) {
-		$date['month']=$GENITIVE_POLISH_MONTHS[$date['mon']];
-	} else {
-		switch ($date['ext']) {
-		case 'EST': case 'INT': case 'CAL': case '':
-			$date['month']=$NOMINATIVE_POLISH_MONTHS[$date['mon']];
-			break;
-		case 'FROM': case 'TO': case 'ABT': case 'APX': case 'CIR':
-			$date['month']=$GENITIVE_POLISH_MONTHS[$date['mon']];
-			break;
-		case 'BET': case 'AND': case 'BEF':
-			$date['month']=$INSTRUMENTAL_POLISH_MONTHS[$date['mon']];
-			break;
-		case 'AFT':
-			$date['month']=$LOCATIVE_POLISH_MONTHS[$date['mon']];
-	 		break;
-		}
-	}
-
+	// The qualifiers are simple translations
 	if (isset($pgv_lang[$q1]))
 		$q1=$pgv_lang[$q1];
 	if (isset($pgv_lang[$q2]))
