f5a294b9733e3f0fc60db25b910d1556b36fa077
NAME <USER@DOMAIN>
2007-09-26 21:05:01 +0000

    googlemap translation by Jani Miettinen
    git-svn-id: svn://svn.webtrees.net/trunk@1730 4201c862-f99b-1943-94d0-25fd05010cbc

--- phpGedView/modules/googlemap/languages/lang.fi.php
+++ phpGedView/modules/googlemap/languages/lang.fi.php
@@ -35,4 +35,99 @@ $pgv_lang['gm_redraw_map']          = "Näytä kartta uudelleen";
 $pgv_lang["gm_map"]                 = "Kartta";
 $pgv_lang["gm_satellite"]           = "Satelliitti";
 $pgv_lang["gm_hybrid"]              = "Hybridi";
+
+// Configuration texts
+$pgv_lang["gm_manage"]              = "Hallitse GoogleKartta konfiguraatiota";
+$pgv_lang["configure_googlemap"]    = "GoogleKartta konfiguraatio";
+$pgv_lang["gm_admin_error"]         = "Sivu vain ylläpitäjille";
+$pgv_lang["gm_db_error"]            = "paikkasijainti taulu ei löydy tietokannasta";
+$pgv_lang["gm_table_created"]       = "paikkasijainti taulu luotu";
+$pgv_lang["googlemap_enable"]       = "Ota käyttöön GoogleKartta";
+$pgv_lang["googlemapkey"]           = "GoogleKartta API avain";
+$pgv_lang["gm_map_type"]            = "Oletus kartta tyyppi";
+$pgv_lang["gm_map_size"]            = "Kartan koko (pikseleinä)";
+$pgv_lang["gm_map_size_x"]          = "Leveys";
+$pgv_lang["gm_map_size_y"]          = "Korkeus";
+$pgv_lang["gm_map_zoom"]            = "Kartan zoomaus kerroin";
+$pgv_lang["gm_digits"]              = "taso";
+$pgv_lang["gm_digits"]              = "luku";
+$pgv_lang["gm_min"]                 = "Min.";
+$pgv_lang["gm_max"]                 = "Max.";
+$pgv_lang["gm_default_level0"]      = "Vakio ylätason arvo";
+$pgv_lang["gm_nof_levels"]          = "Tasojen määrä";
+$pgv_lang["gm_config_per_level"]    = "Tasojen konfigurointi";
+$pgv_lang["gm_name_prefix"]         = "Etuliite";
+$pgv_lang["gm_name_postfix"]        = "Jälkiliite";
+$pgv_lang["gm_name_pre_post"]       = "Etuliite / jälkiliite järjestys";
+$pgv_lang["gm_level"]               = "Taso";
+$pgv_lang["gm_pp_none"]             = "Ei etu-/jälkiliitettä";
+$pgv_lang["gm_pp_n_pr_po_b"]        = "Normaali, etuliite, jälkiliite, kumpikin";
+$pgv_lang["gm_pp_n_po_pr_b"]        = "Normaali, jälkiliite, etuliite, kumpikin";
+$pgv_lang["gm_pp_pr_po_b_n"]        = "Etuliite, jälkiliite, kumpikin, normaali";
+$pgv_lang["gm_pp_po_pr_b_n"]        = "Jälkiliite, etuliite, kumpikin, normaali";
+$pgv_lang["gm_pp_pr_po_n_b"]        = "Etuliite, jälkiliite, normal, kumpikin";
+$pgv_lang["gm_pp_po_pr_n_b"]        = "Jälkiliite, etuliite, normal, kumpikin";
+$pgv_lang["googlemap_coord"]        = "Näytä kartta koordinaatit";
+
+
+// Texts used on the Places location page
+$pgv_lang["edit_place_locations"]   = "Muokkaa sijainti paikka tietoja";
+$pgv_lang["pl_no_places_found"]     = "Ei paikkoja lyötynyt";
+$pgv_lang["pl_zoom_factor"]         = "Zoomaus kerroin";
+$pgv_lang["pl_place_icon"]          = "Ikoni";
+$pgv_lang["pl_edit"]                = "Muokkaa paikan sijaintia";
+$pgv_lang["pl_add_place"]           = "Lisää paikka";
+$pgv_lang["pl_import_gedcom"]       = "Tuo nykyisestä GEDCOM:sta";
+$pgv_lang["pl_import_all_gedcom"]   = "Tuo kaikista GEDCOM:eista";
+$pgv_lang["pl_import_file"]         = "Tuo tiedostosta";
+$pgv_lang["pl_export_file"]         = "Vie nykyinen näkymä tiedostoon";
+$pgv_lang["pl_export_all_file"]     = "Vie kaikki sijainnit tiedostoon";
+$pgv_lang["pl_north_short"]         = "P";
+$pgv_lang["pl_south_short"]         = "E";
+$pgv_lang["pl_east_short"]          = "I";
+$pgv_lang["pl_west_short"]          = "L";
+$pgv_lang["pl_places_filename"]     = "Tiedosto sisältää paikkoja (CSV)";
+$pgv_lang["pl_clean_db"]            = "Siivoa kaikki paikkatiedot ennen tuontia?";
+$pgv_lang["pl_update_only"]         = "Päivitä vain olemassaolevat paikat?";
+$pgv_lang["pl_overwrite_data"]      = "Kirjoita paikkatiedot yli tiedoston tiedoilla?";
+$pgv_lang["pl_no_places_found"]     = "Ei paikkoja löytynyt";
+$pgv_lang["pl_use_this_value"]      = "Käytä tätä arvoa";
+$pgv_lang["pl_precision"]           = "Tarkkuus";
+$pgv_lang["pl_country"]             = "Maa";
+$pgv_lang["pl_state"]               = "Lääni";
+$pgv_lang["pl_city"]                = "Kaupunki";
+$pgv_lang["pl_neighborhood"]        = "Naapurusto";
+$pgv_lang["pl_house"]               = "Talo";
+$pgv_lang["pl_max"]                 = "Maksimi";
+$pgv_lang["pl_delete"]              = "Poista sijaintipaikka";
+
+$pgv_lang["pl_flag"]                = "Lippu";
+$pgv_lang["flags_edit"]             = "Valitse lippu";
+$pgv_lang["pl_change_flag"]         = "Vaihda lippu";
+$pgv_lang["pl_remove_flag"]         = "Poista lippu";
+
+$pgv_lang["pl_remove_location"]     = "Poista tämä sijainti?";
+$pgv_lang["pl_delete_error"]        = "Sijaintia ei poistettu: tämä sijainti sisältää alikohteita";
+$pgv_lang["list_inactive"]        	= "Klikkaa tässä nähdäksesi joutilaat paikat";
+
+//Placecheck specific text
+$pgv_lang["placecheck"]				= "Paikka Tarkistus";
+$pgv_lang['placecheck_text']		= "Tämä listaa kaikki paikat valitusta GEDCOM-tiedostosta. Oletuksena tämä ei sisällytä paikkoja, jotka täysin vastaavat GEDCOM tiedostoa ja GoogleKartta tauluja";
+$pgv_lang['placecheck_top']			= "Ylimmän tason paikat";
+$pgv_lang['placecheck_one']			= "Tason yksi paikat";
+$pgv_lang['placecheck_select1']		= "Valitse ylin taso...";
+$pgv_lang['placecheck_select2']		= "Valitse seuraava taso...";
+$pgv_lang['placecheck_key']			= "Avain värit joita käytetään";
+$pgv_lang['placecheck_key1']		= "tämä paikka ja sen koordinaatit ei löydy GoogleKartta taulusta";
+$pgv_lang['placecheck_key2']		= "tämä paikka on GoogleKartta taulussa, mutta ei sisällä koordinaattia";
+$pgv_lang['placecheck_key3']		= "Tämä paikka taso on tyhjä GEDCOM tiedostossasi. Se olisi hyvä lisätä <br/>GoogleKartta paikkoihin \"tuntematon\" koordinaattien kanssa edelliseltä<br/>tasolta ennen kuin lisäät paikkoja seuraavalle tasolle.";
+$pgv_lang['placecheck_key4']		= "tämä paikka taso on tyhjä sinun GEDCOM tiedostossasi, mutta esillä 'tuntematon'<br/> GoogleKartta paikka taulussa koordinaattien kera. Ei ei vaadi toimintaa<br/> ennen kuin kadoksissa oleva taso voidaan lisätä";
+$pgv_lang['placecheck_head']		= "Paikka lista GEDCOM tiedostosta";
+$pgv_lang['placecheck_gedheader']	= "GEDCOM tiedosto paikka data<br/>(2 PLAC tag)";
+$pgv_lang['placecheck_gm_header']	= "GoogleKartta Paikka taulu data";
+$pgv_lang['placecheck_unique']		= "yhteensä yksilöllisiä paikkoja";
+$pgv_lang["placecheck_zoom"]        = "Zoomaus=";
+$pgv_lang["placecheck_options"]     = "Paikkatarkistus lista valinnat";
+$pgv_lang["placecheck_filter_text"] = "Lista suodatus valinnat";
+$pgv_lang["placecheck_match"] 		= "Sisällytä täysin vastaavat paikat -";
 ?>
