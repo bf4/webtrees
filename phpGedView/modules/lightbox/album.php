160c71e8614b0981658ef73aa7333e38b4ff7597
NAME <USER@DOMAIN>
2007-10-08 23:28:41 +0000

    Lightbox: 1) New GUI for configuration parameters added. 2) Fixed several RTL (Hebrew) problems with Lightbox Shifting to right.    (addition of RTL CSS files and RTL album page files) 3) Disabled the bracketed [no./total.] item to the right of the title ( was causing problems with RTL )    I still have work to do when long titles are used too. 4. Fixed JavaScript bug in medialist with sound object not being recognized.
    git-svn-id: svn://svn.webtrees.net/trunk@1852 4201c862-f99b-1943-94d0-25fd05010cbc

--- phpGedView/modules/lightbox/album.php
+++ phpGedView/modules/lightbox/album.php
@@ -26,36 +26,50 @@
  * @version $Id$
  * @author Brian Holland
  */
-?>
-<?php 
+ 
+// Get Javascript variables from lb_config.php --------------------------- 
+	include('modules/lightbox/lb_config.php'); 
+	?>
+	<SCRIPT LANGUAGE=Javascript>
+	var myMusic 		= '<?php print $LB_MUSIC_FILE; ?>';  	// The music file
+	var CB_SlShowTime 	= '<?php print $LB_SS_SPEED; 	?>';	// Slide show timer
+	</script>
+	<?php
+
+// ------------------------------------------------------------------------------- 
 global $LANGUAGE;
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en">
 <head>
 
-  <title>Personal | Media Album v2.0</title>
+  <title>Personal | Media Album </title>
+
+
 <?php if ($LANGUAGE=="hebrew") { ?>
-  <link  href="modules/lightbox/css/clearbox_music_RTL.css" 	rel="stylesheet" type="text/css" />
+		<link href ="modules/lightbox/css/clearbox_music_RTL.css" 	rel="stylesheet" type="text/css" />
+		<link href ="modules/lightbox/css/album_page.css" 			rel="stylesheet" type="text/css" media="screen" /> 
+		<!--[if lte IE 7]>
+		<link href ="modules/lightbox/css/album_page_RTL.css" 			rel="stylesheet" type="text/css" media="screen" /> 
+		<![endif]-->
+
 <?php }else{ ?>
-  <link  href="modules/lightbox/css/clearbox_music.css" 	rel="stylesheet" type="text/css" />
+		<link href ="modules/lightbox/css/clearbox_music.css" 		rel="stylesheet" type="text/css" />
+		<link href ="modules/lightbox/css/album_page.css" 			rel="stylesheet" type="text/css" media="screen" />  
 <?php } ?>
   
-  <link  href="modules/lightbox/css/lightbox_plus.css" 		rel="stylesheet" type="text/css" media="screen" />  
- 
-  <script src="modules/lightbox/js/prototype.js" 			type="text/javascript"></script>  
-  <script src="modules/lightbox/js/Sound.js" 				type="text/javascript"></script>
-  <script src="modules/lightbox/js/clearbox.js" 			type="text/javascript"></script>
+	<script src="modules/lightbox/js/prototype.js" 				type="text/javascript"></script>  
+	<script src="modules/lightbox/js/Sound.js" 					type="text/javascript"></script>
+	<script src="modules/lightbox/js/clearbox.js" 				type="text/javascript"></script>
   
 </head>
 
 <body>
-  <script src="modules/lightbox/js/wz_tooltip.js" 			type="text/javascript"></script>  
-  
-  
+	<script src="modules/lightbox/js/wz_tooltip.js" 			type="text/javascript"></script>  
+	
 <?php
-
-global $edit, $controller, $tabno, $_REQUEST, $thumb_edit;
+//include('modules/lightbox/lb_config.php');
+global $edit, $controller, $tabno, $_REQUEST, $thumb_edit ;
 
 //------------------------------------------------------------------------------
 // Start Main Table
