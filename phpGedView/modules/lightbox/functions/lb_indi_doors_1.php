9434c919d44f82eeffc74a69f603408b9249979d
NAME <USER@DOMAIN>
2007-11-02 23:16:01 +0000

    Lightbox: [ 1824871 ] Fixed "Lozenge" (Empty Tabs) problem when Media Support disabled
    git-svn-id: svn://svn.webtrees.net/trunk@2113 4201c862-f99b-1943-94d0-25fd05010cbc

--- phpGedView/modules/lightbox/functions/lb_indi_doors_1.php
+++ phpGedView/modules/lightbox/functions/lb_indi_doors_1.php
@@ -51,18 +51,5 @@
 	<dd id="door0"><a href="javascript:;" onclick="tabswitch(0); if (loadedTabs[7]) {ResizeMap(); ResizeMap();} return false;" ><?php print $pgv_lang["all"]?></a></dd>
 	<!--<dd id="door0" style={background-color:transparent;} style={border:0px;} ></dd> -->
  
-<?php
-	// If not $MULTI_MEDIA hide the media and album tabs
-//	if (!$MULTI_MEDIA){ 
-		if (!file_exists("modules/googlemap/defaultconfig.php") && (file_exists("modules/lightbox/album.php") ) ) {  ?>
-			<dd id="door4" style={background-color:transparent;} style={border:0px;} ></dd> 
-			<dd id="door7" style={background-color:transparent;} style={border:0px;} ></dd> 
-		<?php 
-		}elseif (file_exists("modules/googlemap/defaultconfig.php") && (file_exists("modules/lightbox/album.php") ) ) { ?>
-			<dd id="door4" style={background-color:transparent;} style={border:0px;} ></dd> 
-			<dd id="door8" style={background-color:transparent;} style={border:0px;} ></dd> 
-		<?php 
-		} 
-//	}
-?>
+
 
