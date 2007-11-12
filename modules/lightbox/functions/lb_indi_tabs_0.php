b6a437db0457689af3bc33a636070f976cfc63fa
NAME <USER@DOMAIN>
2007-11-12 22:39:52 +0000

    Lightbox: correct number of "false" statements
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@2183 4201c862-f99b-1943-94d0-25fd05010cbc

--- modules/lightbox/functions/lb_indi_tabs_0.php
+++ modules/lightbox/functions/lb_indi_tabs_0.php
@@ -29,7 +29,7 @@
 
 <?php if (file_exists("modules/googlemap/defaultconfig.php") && file_exists("modules/lightbox/album.php")) { ?>
        var tabid = new Array('0','facts','notes','sources','media','relatives','researchlog','googlemap','lightbox2');
-       var loadedTabs = new Array(false,false,false,false,false,false,false,false);
+       var loadedTabs = new Array(false,false,false,false,false,false,false,false,false);
 <?php }else if (file_exists("modules/googlemap/defaultconfig.php") && !file_exists("modules/lightbox/album.php")) { ?>
        var tabid = new Array('0','facts','notes','sources','media','relatives','researchlog','googlemap');
        var loadedTabs = new Array(false,false,false,false,false,false,false,false);
