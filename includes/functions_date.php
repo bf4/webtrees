d11f3d79f9592f074c6ffb1e5cbc6cdb5c6ed756
NAME <USER@DOMAIN>
2007-11-24 13:41:53 +0000

    Finnish date entry bug
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@2286 4201c862-f99b-1943-94d0-25fd05010cbc

--- includes/functions_date.php
+++ includes/functions_date.php
@@ -151,11 +151,14 @@ function default_edit_to_gedcom_date($datestr)
 	// ads:adr_leap_year:adr to prevent "Adar" matching "Adar Sheni" or "Adar I" matching "Adar II"
 	// \b prevents the german JULI matching @#DJULIAN@, etc.
 
-	foreach (array('jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec','vend','brum','frim','nivo','pluv','vent','germ','flor','prai','mess','ther','fruc','comp','tsh','csh','ksl','tvt','shv','nsn','iyr','svn','tmz','aav','ell','abt','aft','and','bef','bet','cal','est','from','int','to','b.c.') as $keyword)
+	foreach (array('jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec','vend','brum','frim','nivo','pluv','vent','germ','flor','prai','mess','ther','fruc','comp','tsh','csh','ksl','tvt','shv','nsn','iyr','svn','tmz','aav','ell','abt','aft','bef','bet','cal','est','from','int','to','b.c.') as $keyword)
 		$datestr=preg_replace("/\b".str_replace('.','[.]?',$pgv_lang[$keyword])."\b/i", strtoupper($keyword), $datestr);
 
 	foreach (array('ads','adr_leap_year','adr','jan_1st','feb_1st','mar_1st','apr_1st','may_1st','jun_1st','jul_1st','aug_1st','sep_1st','oct_1st','nov_1st','dec_1st') as $keyword)
 		$datestr=preg_replace("/\b".str_replace('.','[.]?',$pgv_lang[$keyword])."\b/i", strtoupper(substr($keyword,0,3)), $datestr);
+
+	foreach (array('and') as $keyword)
+		$datestr=preg_replace("/\b".str_replace('.','[.]?',$pgv_lang[$keyword])."\b/i", strtoupper($keyword), $datestr);
 
 	// APX and CIR are not gedcom 5.5.1 keywords
 	foreach (array('apx','cir') as $keyword)
