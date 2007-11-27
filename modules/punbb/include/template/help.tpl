49c05aa742efd8800c6c248b43bbab0660d4d6da
NAME <USER@DOMAIN>
2007-11-27 12:05:52 +0000

    make way for the 1.2.16 punbb code
    git-svn-id: svn://svn.webtrees.net/branches/phpGedView-4.2@2306 4201c862-f99b-1943-94d0-25fd05010cbc

--- modules/punbb/include/template/help.tpl
+++
@@ -1,7 +0,0 @@
-<div id="punwrap">
-<div id="helpfile" class="pun">
-
-<pun_main>
-
-</div>
-</div>
--- modules/punbb/lang/Hebrew/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$s = file_get_contents("common22.php");
-$new=mb_convert_encoding($s,"UTF-8","iso-8859-8");
-file_put_contents("commons33.php", $new);
-
-?>
