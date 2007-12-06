b61daa906d965197905bee123dc04a439ce889c2
NAME <USER@DOMAIN>
2007-12-06 07:35:31 +0000

    more robust media tests, with comments
    git-svn-id: svn://svn.webtrees.net/branches/phpGedView-4.2@2380 4201c862-f99b-1943-94d0-25fd05010cbc

--- modules/testing/unittests/media_test.php
+++ modules/testing/unittests/media_test.php
@@ -1,12 +1,13 @@
 <?php
 	/*
-	 * Tests for the Person class
+	 * Tests for the Media class
 	 */
 
 	// this object contains all the different parameters we are testing
 	class MediaTestParameters {
 		var $use_media_firewall = false;
 		var $media_firewall_thumbs = false;
+		var $auto_generate_thumbs = true;
 		var $directory_levels = 0;
 		var $path = '';
 		var $filetype = '';
@@ -22,32 +23,55 @@
 	// this object performs the tests based on the parameters that were passed in
 	class MediaTestCase extends UnitTestCase {
 		function doMediaObject($testObj) {
-			global $MEDIA_DIRECTORY, $MEDIA_DIRECTORY_LEVELS, $USE_MEDIA_FIREWALL, $MEDIA_FIREWALL_ROOTDIR;
-			global $MEDIA_FIREWALL_THUMBS, $GEDCOM, $MEDIA_ID_PREFIX;
+			global $MULTI_MEDIA, $MEDIA_DIRECTORY, $MEDIA_DIRECTORY_LEVELS, $USE_MEDIA_FIREWALL, $MEDIA_FIREWALL_ROOTDIR;
+			global $AUTO_GENERATE_THUMBS, $MEDIA_FIREWALL_THUMBS, $GEDCOM, $MEDIA_ID_PREFIX;
 			// set global variables to known values
+			$MULTI_MEDIA = true;
+			$AUTO_GENERATE_THUMBS = $testObj->auto_generate_thumbs;
 			$MEDIA_DIRECTORY_LEVELS = $testObj->directory_levels;
 			$USE_MEDIA_FIREWALL = $testObj->use_media_firewall;
 			$MEDIA_FIREWALL_THUMBS = $testObj->media_firewall_thumbs;
 
+			if ( $USE_MEDIA_FIREWALL && (strpos($MEDIA_DIRECTORY,"../") !== false ) ) {
+				$this->assertTrue(0, "Can't test Media Firewall, MEDIA_DIRECTORY has ../");
+				return;
+			}
+
 			// set local variables
-			$serverPath = ($USE_MEDIA_FIREWALL) ? $MEDIA_FIREWALL_ROOTDIR.$MEDIA_DIRECTORY : $MEDIA_DIRECTORY;
+			$server_media_directory = ($USE_MEDIA_FIREWALL) ? $MEDIA_FIREWALL_ROOTDIR.$MEDIA_DIRECTORY : $MEDIA_DIRECTORY;
 			if (empty($testObj->thumbnail)) {
+				// dynamically determine what the thumbnail is
+				$thumbnail = $MEDIA_DIRECTORY.'thumbs/'.$testObj->path.$testObj->file;
 				if ($MEDIA_FIREWALL_THUMBS) {
-					$thumbnailFile = $serverPath.'thumbs/'.$testObj->path.$testObj->file;
+					// if media_firewall_thumbs is true and the main media item is in the media_firewall_rootdir, then thumbnail is stored in the media_firewall_rootdir too
+					$server_thumbnail = $server_media_directory.'thumbs/'.$testObj->path.$testObj->file;
 				} else {
-					$thumbnailFile = $MEDIA_DIRECTORY.'thumbs/'.$testObj->path.$testObj->file;
+					// if media_firewall_thumbs is false, the thumbnail is stored in the main media directory
+					$server_thumbnail = $thumbnail;
 				}
 			} else {
-				$thumbnailFile = $testObj->thumbnail;
+				// a specific thumbnail was passed in, it should be something in the images directory, not the media directory
+				$thumbnail = $testObj->thumbnail;
+				$server_thumbnail = $thumbnail;
+				$this->assertTrue(strpos($thumbnail,"images") === 0);
 			}
 
 			if ($testObj->copyfile) {
 				// copy image files into place
 				$this->assertTrue(file_exists(TESTING_ROOT.'data/media/'.$testObj->file));
-				$this->assertTrue(mkdirs($serverPath.$testObj->path));
-				$this->assertTrue(copy(TESTING_ROOT.'data/media/'.$testObj->file, $serverPath.$testObj->path.$testObj->file));
+				$this->assertTrue(mkdirs($server_media_directory.$testObj->path));
+				$this->assertTrue(copy(TESTING_ROOT.'data/media/'.$testObj->file, $server_media_directory.$testObj->path.$testObj->file));
+				$this->assertFalse(empty($server_thumbnail));
+			} else {
+				// the image file should not exist if we did not copy it there for this test
+				$this->assertFalse(file_exists($server_media_directory.$testObj->path.$testObj->file));
+			}
+			if ( (!empty($server_thumbnail)) && (strpos($server_thumbnail,"images") !== 0) ) {
+				// make sure the server_thumbnail does not already exist (unless it is in the images directory)
+				$this->assertFalse(file_exists($server_thumbnail));
 			}
 			
+			// pick a random MID
 			$mid = $MEDIA_ID_PREFIX.'123456';
 			$mediaGedcom = '0 @'.$mid.'@ OBJE
 2 FORM JPEG
@@ -59,19 +83,31 @@
 2 CONT more of the same note
 1 FILE /ignorepath/'.$testObj->path.$testObj->file;
 			$media = new Media($mediaGedcom);
-			$this->assertEqual($media->canDisplayDetails(), true);
+			$this->assertTrue($media->canDisplayDetails());
 			$this->assertEqual($media->getNote(), "This is a note\nmore of the same note");
 			$this->assertEqual($media->getTitle(), 'Title Goes Here');
 			$this->assertEqual($media->getName(), $media->getTitle());
 			$this->assertEqual($media->getAddName(), "<br />Hebrew Title<br />\nRoman Title");
 			$this->assertEqual($media->getAddTitle(), $media->getAddName());
 			$this->assertEqual($media->getSortableName(), $media->getTitle());
-			$this->assertEqual($media->getThumbnail(), $thumbnailFile);
+			// NOTE: getThumbnail() returns the standard media path, not the protected media path
+			$this->assertEqual($media->getThumbnail(), $thumbnail);
+			// NOTE: getFilename() returns the complete "1 FILE" line, which is not necessarily the standard or protected media path
 			$this->assertEqual($media->getFilename(), '/ignorepath/'.$testObj->path.$testObj->file);
-			// when files does not exist, getLocalFilename and getServerFilename should return whatever was passed in to them
+			// when file does not exist, getLocalFilename() and getServerFilename() should return whatever was passed in to them
 			$this->assertEqual($media->getLocalFilename(), $MEDIA_DIRECTORY.$testObj->path.$testObj->file);
-			$this->assertEqual($media->getServerFilename(), $serverPath.$testObj->path.$testObj->file);
+			$this->assertEqual($media->getServerFilename(), $server_media_directory.$testObj->path.$testObj->file);
+			// if we copied the file there, it should exist.  otherwise, it should not
 			$this->assertEqual($media->fileExists(), $testObj->copyfile);
+			if (!empty($server_thumbnail)) {
+				if (strpos($server_thumbnail,"images") !== 0) {
+					// the thumbnail is not in the images directory.  if we copied the file in, and auto_generate_thumbs is true, then the thumbnail should now exist. 
+					$this->assertEqual(file_exists($server_thumbnail), ($testObj->copyfile && $AUTO_GENERATE_THUMBS) );
+				} else {
+					// the thumbnail is in the images directory, it should exist
+					$this->assertEqual(file_exists($server_thumbnail), true);
+				}
+			}
 			$this->assertEqual($media->getFilesize(), $testObj->filesize);
 			$this->assertEqual($media->getFilesizeraw(), $testObj->filesizeraw);
 			$this->assertEqual($media->getMediatype(), 'photo');
@@ -86,11 +122,12 @@
 			
 			if ($testObj->copyfile) {
 				// remove the files that we initially copied
-				if (file_exists($serverPath.$testObj->path.$testObj->file)) {
-					$this->assertTrue(unlink($serverPath.$testObj->path.$testObj->file));
+				if (file_exists($server_media_directory.$testObj->path.$testObj->file)) {
+					$this->assertTrue(unlink($server_media_directory.$testObj->path.$testObj->file));
 				}
-				if (file_exists($thumbnailFile)) {
-					$this->assertTrue(unlink($thumbnailFile));
+				if ( (!empty($server_thumbnail)) && (strpos($server_thumbnail,"images") !== 0) && (file_exists($server_thumbnail)) ) {
+					// only delete the thumbnail if it is not in the images directory
+					$this->assertTrue(unlink($server_thumbnail));
 				}
 				// unfortunately, this still leaves some extra directories on the system
 			}
@@ -107,9 +144,15 @@
 		// test with different values for $MEDIA_DIRECTORY_LEVELS
 		
 		// still to do:
-		// test with AUTO_GENERATE_THUMBS on and off
 		// test external media with MEDIA_EXTERNAL on and off
+		// test RESN
 
+		// Notes for writing test cases:
+		// * Do not test with files from your own gedcom, only test with the files in the /modules/testing/data/media/ directory (the files are deleted at the end of the test)
+		// * If copyfile or xxx is false, you have to specify the default thumbnail or the test will fail
+		// * If you specify a default thumbnail, it must be from the images directory, not the media directory
+		// * All paths and filenames should start with "__" so they can be kept separate from real data
+		
   		function testMedia1() {
 			// jpg, does not exist
 			$test = new MediaTestParameters();
@@ -123,8 +166,8 @@
 			$this->doMediaObject($test);
 		}
 
-  		function testMedia2() {
-			// gif, exists, directory levels 2, media firewall off
+  		function testMedia2a() {
+			// gif, exists, directory levels 2, media firewall off, auto generate thumbs on
 			$test = new MediaTestParameters();
 			$test->use_media_firewall = false;
 			$test->directory_levels = 2;
@@ -140,8 +183,27 @@
 			$this->doMediaObject($test);
 		}
 
-  		function testMedia3() {
-			// jpg, exists, directory levels 1, media firewall on, media firewall thumbs off
+  		function testMedia2b() {
+			// gif, exists, directory levels 2, media firewall off, auto generate thumbs off
+			$test = new MediaTestParameters();
+			$test->use_media_firewall = false;
+			$test->auto_generate_thumbs = false;
+			$test->directory_levels = 2;
+			$test->path = '__testpath1/__testpath2/';
+			$test->file = '__testfile.gif';
+			$test->filetype = 'GIF';
+			$test->copyfile = true;
+			$test->mimetype = 'image/gif';
+			$test->filesize = 6.44;
+			$test->filesizeraw = 6591;
+			$test->width = 300;
+			$test->height = 68;
+			$test->thumbnail = 'images/media.gif';
+			$this->doMediaObject($test);
+		}
+
+  		function testMedia3a() {
+			// jpg, exists, directory levels 1, media firewall on, media firewall thumbs off, auto generate thumbs on
 			$test = new MediaTestParameters();
 			$test->use_media_firewall = true;
 			$test->media_firewall_thumbs = false;
@@ -155,6 +217,26 @@
 			$test->filesizeraw = 6123;
 			$test->width = 250;
 			$test->height = 57;
+			$this->doMediaObject($test);
+		}	
+
+  		function testMedia3b() {
+			// jpg, exists, directory levels 1, media firewall on, media firewall thumbs off, auto generate thumbs off
+			$test = new MediaTestParameters();
+			$test->use_media_firewall = true;
+			$test->media_firewall_thumbs = false;
+			$test->auto_generate_thumbs = false;
+			$test->directory_levels = 1;
+			$test->path = '__testpath1/';
+			$test->file = '__testfile.jpg';
+			$test->filetype = 'JPG';
+			$test->copyfile = true;
+			$test->mimetype = 'image/jpeg';
+			$test->filesize = 5.98;
+			$test->filesizeraw = 6123;
+			$test->width = 250;
+			$test->height = 57;
+			$test->thumbnail = 'images/media.gif';
 			$this->doMediaObject($test);
 		}	
 
@@ -206,4 +288,19 @@
 			$test->mimetype = 'application/pdf';
 			$this->doMediaObject($test);
 		}
+
+  		function testMedia7() {
+			// doc, doesn't exist
+			$test = new MediaTestParameters();
+			$test->use_media_firewall = false;
+			$test->directory_levels = 1;
+			$test->path = '__testpath1/';
+			$test->file = '__testfile.doc';
+			$test->filetype = 'DOC';
+			$test->copyfile = false;
+			$test->thumbnail = 'images/media/doc.gif';
+			$test->mimetype = 'application/msword';
+			$this->doMediaObject($test);
+		}
+
 	}
