dd507cab70dea31082409a6eda5d286c619ae818
NAME <USER@DOMAIN>
2007-12-07 18:36:43 +0000

    Add French to punBB; Add Turkish to gallery2 and research assistant
    git-svn-id: svn://svn.webtrees.net/trunk@2410 4201c862-f99b-1943-94d0-25fd05010cbc

---
+++ phpGedView/modules/punbb/lang/French/forum.php
@@ -0,0 +1,17 @@
+<?php
+
+////////////////////////////////////
+//Translate By Alain Bouquet [vizar]
+//Email:alamo29@gmail.com
+////////////////////////////////////
+
+// Language definitions used in viewforum.php
+$lang_forum = array(
+ 
+'Post topic'	=>	'Nouvelle discussion',
+'Views'			=>	'Vues',
+'Moved'			=>	'Déplacé',
+'Sticky'		=>	'Épinglé',
+'Empty forum'	=>	'Le forum est vide.'
+ 
+);
