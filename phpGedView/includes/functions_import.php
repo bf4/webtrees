2a7740a66eabbedb87b39ecd4beff171db7f849b
NAME <USER@DOMAIN>
2008-02-04 22:03:00 +0000

    [ 1886435 ] Incorrect format for _UID fields
    git-svn-id: svn://svn.webtrees.net/trunk@2508 4201c862-f99b-1943-94d0-25fd05010cbc

--- phpGedView/includes/functions_import.php
+++ phpGedView/includes/functions_import.php
@@ -980,11 +980,11 @@ function setup_database() {
 			$sql = "ALTER TABLE " . $TBLPREFIX . "places DROP COLUMN p_gid";
 			$res = dbquery($sql); //print "p_gid dropped<br/>\n";
 		}
- 		if (!$has_places_std_soundex) {
+		if (!$has_places_std_soundex) {
 			$sql = "ALTER TABLE ".$TBLPREFIX."places ADD p_std_soundex text";	
 			$res = dbquery($sql);//print "p_std_soundex added<br/>\n";
 		}
- 		if (!$has_places_dm_soundex) {
+		if (!$has_places_dm_soundex) {
 			$sql = "ALTER TABLE ".$TBLPREFIX."places ADD p_dm_soundex text";	
 			$res = dbquery($sql);//print "p_dm_soundex added<br/>\n";
 		}
@@ -1011,7 +1011,7 @@ function setup_database() {
 		}
 	}
 	if (!$has_dates || stristr($DBTYPE, "mysql") === false && // AFTER keyword only in mysql
-	 (!$has_dates_mon || !$has_dates_datestamp || !$has_dates_juliandays)) {
+	(!$has_dates_mon || !$has_dates_datestamp || !$has_dates_juliandays)) {
 		create_dates_table();
 	} else { // check columns in the table
 		if (!$has_dates_mon) {
@@ -1770,7 +1770,6 @@ function cleanup_tags_y(& $irec) {
 		"BAPM","BARM","BASM","BLES","CHRA","CONF","FCOM","ORDN","NATU","EMIG",
 		"IMMI","CENS","PROB","WILL","GRAD","RETI");
 	$irec .= "\r\n1";
-	//	$ft = preg_match_all("/1\s(_?\w+)\s/", $irec, $match);
 	$ft = preg_match_all("/1\s(\w+)\s/", $irec, $match);
 	for ($i = 0; $i < $ft; $i++) {
 		$sfact = $match[1][$i];
@@ -1788,50 +1787,29 @@ function cleanup_tags_y(& $irec) {
 		}
 	}
 	$irec = substr($irec, 0, -3);
-	//	return $irec;
 }
 
-/**
- * Generates a Universally Unique IDentifier, version 4.
- *
- * RFC 4122 (http://www.ietf.org/rfc/rfc4122.txt) defines a special type of Globally
- * Unique IDentifiers (GUID), as well as several methods for producing them. One
- * such method, described in section 4.4, is based on truly random or pseudo-random
- * number generators, and is therefore implementable in a language like PHP.
- *
- * We choose to produce pseudo-random numbers with the Mersenne Twister, and to always
- * limit single generated numbers to 16 bits (ie. the decimal value 65535). That is
- * because, even on 32-bit systems, PHP's RAND_MAX will often be the maximum *signed*
- * value, with only the equivalent of 31 significant bits. Producing two 16-bit random
- * numbers to make up a 32-bit one is less efficient, but guarantees that all 32 bits
- * are random.
- *
- * The algorithm for version 4 UUIDs (ie. those based on random number generators)
- * states that all 128 bits separated into the various fields (32 bits, 16 bits, 16 bits,
- * 8 bits and 8 bits, 48 bits) should be random, except : (a) the version number should
- * be the last 4 bits in the 3rd field, and (b) bits 6 and 7 of the 4th field should
- * be 01. We try to conform to that definition as efficiently as possible, generating
- * smaller values where possible, and minimizing the number of base conversions.
- *
- * @copyright  Copyright (c) CFD Labs, 2006. This function may be used freely for
- *              any purpose ; it is distributed without any form of warranty whatsoever.
- * @author      David Holmes <dholmes@cfdsoftware.net>
- *
- * @return  string  A UUID, made up of 36 hex digits
- */
+// Create a pseudo-random UUID, as per RFC4122
 function uuid() {
-
-	// The field names refer to RFC 4122 section 4.1.2
-
-	return strtoupper(sprintf('%04x%04x%04x%03x4%04x%04x%04x%04x%04x', mt_rand(0, 65535), mt_rand(0, 65535), // 32 bits for "time_low"
-	mt_rand(0, 65535), // 16 bits for "time_mid"
-	mt_rand(0, 4095), // 12 bits before the 0100 of (version) 4 for "time_hi_and_version"
-	bindec(substr_replace(sprintf('%016b', mt_rand(0, 65535)), '01', 6, 2)),
-	// 8 bits, the last two of which (positions 6 and 7) are 01, for "clk_seq_hi_res"
-	// (hence, the 2nd hex digit after the 3rd hyphen can only be 1, 5, 9 or d)
-	// 8 bits for "clk_seq_low"
-	mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535) // 48 bits for "node" 
-	));
+	return sprintf(
+		'%02X%02X%02X%02X-%02X%02X-%02X%02X-%02X%02X-%02X%02X%02X%02X%02X%02X',
+		rand(0, 255),
+		rand(0, 255),
+		rand(0, 255),
+		rand(0, 255),
+		rand(0, 255),
+		rand(0, 255),
+		rand(0, 255)&0x3f|0x80, // Set the version to random (0100xxxx)
+		rand(0, 255),
+		rand(0, 255)&0x0f|0x40, // Set the variant to RFC4122 (10xxxxxx)
+		rand(0, 255),
+		rand(0, 255),
+		rand(0, 255),
+		rand(0, 255),
+		rand(0, 255),
+		rand(0, 255),
+		rand(0, 255)
+	);
 }
 
 /**
