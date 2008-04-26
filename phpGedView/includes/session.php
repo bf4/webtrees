e6595ef2764b43b24a612b2063cf2801d4b08097
NAME <USER@DOMAIN>
2008-04-26 18:34:41 +0000

    Updating version numbers
    git-svn-id: svn://svn.webtrees.net/trunk@2927 4201c862-f99b-1943-94d0-25fd05010cbc

--- phpGedView/includes/session.php
+++ phpGedView/includes/session.php
@@ -30,7 +30,7 @@ if (stristr($_SERVER["SCRIPT_NAME"], basename(__FILE__))!==false) {
 }
 
 //-- version of PhpGedView  (Let's keep this in an obvious place -- not buried in code)
-$VERSION = "4.1.5";
+$VERSION = "4.1.6";
 $VERSION_RELEASE = "";
 $REQUIRED_PRIVACY_VERSION = "3.1";
 
