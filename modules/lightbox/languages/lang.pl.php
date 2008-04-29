4d6c3d339ce0eca439e0ce72034b2c43d024d6f7
NAME <USER@DOMAIN>
2008-04-29 15:40:01 +0000

    lightbox - polish translation
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@2945 4201c862-f99b-1943-94d0-25fd05010cbc

---
+++ modules/lightbox/languages/lang.pl.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * Lightbox Album module for phpGedView
+ *
+ * Display media Items using Lightbox
+ *
+ * phpGedView: Genealogy Viewer
+ * Copyright (C) 2002 to 2008  John Finlay and Others
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @package PhpGedView
+ * @subpackage Module
+ * @version $Id:
+ */
+
+//-- security check, only allow access from module.php
+if (stristr($_SERVER["SCRIPT_NAME"], basename(__FILE__))!==false) {
+	print "Nie można uzyskać bezpośredniego dostępu do pliku.";
+	exit;
+}
+
+// Added in VERSION 4.1.4 
+
+$pgv_lang["lb_details"]			= "Szczegóły";
+$pgv_lang["lb_detail_info"]		= "Pokaż szczegóły - Strona przeglądarki multimediów";
+$pgv_lang["lb_pause_ss"]		= "Pauza";
+$pgv_lang["lb_start_ss"]		= "Start";
+$pgv_lang["lb_music"]			= "Wyłącz/Włącz Muzykę";
+$pgv_lang["lb_zoom_off"]		= "Wyłącz zoom";
+$pgv_lang["lb_zoom_on"]			= "Zoom jest włączony ... Użyj rolki myszki lub klawiszy I oraz O aby powiększyć lub pomniejszyć";
+$pgv_lang["lb_close_win"]		= "Zamknij";
+
+//Config Parameters -------------------------------------
+$pgv_lang["configure_lightbox"]		= "Konfiguracja <br />Lightbox-Album";
+$pgv_lang["mediatab"]       		= "Strona osobista - Multimedia";
+$pgv_lang["lb_appearance"]			= "Wyświetl zakładkę";
+$pgv_lang["lb_linkAppearance"]		= "Wygląd odnośnika";
+$pgv_lang["lb_MP3Only"]				= "tylko format mp3";
+$pgv_lang["lb_admin_error"]			= "Strona tylko dla Administratora";
+$pgv_lang["lb_toAlbumPage"]			= "Powrót do Albumu";
+
+$pgv_lang["lb_icon"]				= "Ikona";
+$pgv_lang["lb_text"]				= "Tekst";
+$pgv_lang["lb_both"]				= "Ikona i tekst";
+$pgv_lang["lb_none"]				= "Brak";
+
+$pgv_lang["lb_al_head_links"]			= "Strona osobista - Album - Nagłówek";
+$pgv_lang["lb_al_thumb_links"]			= "Strona osobista - Album - Miniaturki";
+$pgv_lang["lb_ml_thumb_links"]			= "Strona multimediów - Miniaturki";
+$pgv_lang["lb_music_file"]				= "Muzyka podczas pokazu slajdów";
+$pgv_lang["lb_musicFileAdvice"]			= "Lokalizacja pliku dźwiękowego (puste jeśli bez muzyki)";
+$pgv_lang["lb_ss_speed"]				= "Szybkość pokazu slajdów";
+$pgv_lang["lb_ss_SpeedAdvice"]			= "Szybkość pokazu slajdów w sekundach na jeden obraz";
+
+$pgv_lang["lb_transition"]				= "Opóźnienie zmiany obrazów";
+$pgv_lang["lb_normal"]					= "Normalne";
+$pgv_lang["lb_double"]					= "Podwójne";
+$pgv_lang["lb_warp"]					= "Różne";
+$pgv_lang["lb_url_dimensions"]			= "Rozmiary okna";
+$pgv_lang["lb_url_dimensionsAdvice"]	= "Szerokość i wysokość okna w pikselach";
+$pgv_lang["lb_width"]					= "Szerokość";
+$pgv_lang["lb_height"]					= "Wysokość";
+
+
+// ---------------------------------------------------------------------
+
+
+$pgv_lang["lb_help"] 		 = "Album - Pomoc";
+$pgv_lang["lightbox"]		 = "Album";
+$pgv_lang["showmenu"] 		 = "Pokaż Menu:";
+
+$pgv_lang["TYPE__other"] 	 = "Inne";
+
+$pgv_lang["TYPE__footnotes"] = "Notes";
+
+$pgv_lang["census_text"]  	 = "" ;
+
+$pgv_lang["lb_edit_details"] 	= "Edytuj szczegóły";
+$pgv_lang["lb_view_details"] 	= "Pokaż szczegóły";
+$pgv_lang["lb_edit_media"] 		= "Edytuj szczegóły dla tych multimediów";
+$pgv_lang["lb_delete_media"] 	= "Usuń multimedia - usuwa tylko odnośnik do strony osobistej - nie usuwa pliku lub innych odnośników";
+$pgv_lang["lb_view_media"] 		= "Zobacz szczegóły oraz inne opcje multimediów - Strona przeglądarki multimediów";
+$pgv_lang["lb_add_media"] 		= "Dodaj nowe <br />multimedia";
+$pgv_lang["lb_add_media_full"] 	= "Dodaj nowe multimedia dla tej osoby";
+$pgv_lang["lb_link_media"] 		= "Ustaw odnośnik do<br />istniejących multimediów";
+$pgv_lang["lb_link_media_full"] = "Powiąż tą osobę z istniejącymi multimediami";
+
+$pgv_lang["lb_slide_show"] 		= "Pokaz slajdów";
+$pgv_lang["turn_edit_ON"] 		= "Włącz tryb edycji";
+$pgv_lang["turn_edit_OFF"] 		= "Wyłącz tryb edycji";
+
+$pgv_lang["lb_source_avail"] 	= "Dostępne informacje źródłowe";
+
+$pgv_lang["lb_private"] 		= "Obraz połączony<br /> z danymi prywatnymi";
+$pgv_lang["lb_view_source_tip"] = "Pokaż źródło: ";
+$pgv_lang["lb_view_details_tip"] = "Pokaż szczegóły: ";
+
+?>
