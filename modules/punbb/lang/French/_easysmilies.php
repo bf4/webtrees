333184e1fb1303e7cfd3b9868265f7b01577bcfd
NAME <USER@DOMAIN>
2008-05-07 23:59:19 +0000

    [ 1952760 ] Punbb easymilies FRA translation by Jean-Marie [FRA] - elliott1961
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@3021 4201c862-f99b-1943-94d0-25fd05010cbc

---
+++ modules/punbb/lang/French/_easysmilies.php
@@ -0,0 +1,32 @@
+<?php
+
+// Language definitions used in the Easy Smilies mod
+$lang_smiley = array(
+
+	// Validation
+	'Create Smiley Code None'				=>	'Vous n\'avez pas entré de code.',
+	'Create Smiley Image None'				=>	'Vous n\'avez pas entré le nom d\'une image.',
+	'Delete Smiley None'				=>	'Vous n\'avez pas indiqué quelle émoticône doit être effacée.',
+
+	// Easy Smilies (create a smiley)
+	'Description 1'				=>	'Ce plugin permet d\'insérer facilement des émoticônes dans votre forum en les stockant dans votre base de données.',
+	'Description 2'				=>	'Les émoticônes automatisées sont une façon de penser.',
+	'Successful Creation'				=>	'Vous avez créé une nouvelle émoticône avec succès.',
+	'Submit New Smiley'				=>	'Créer une nouvelle émoticône',
+	'Instructions'				=>	'Faîtes ce que le formulaire demande.',
+	'Smiley Code'				=>	'Code de l\'émoticône',
+	'Smiley Image'				=>	'Image de l\'émoticône',
+	'Smiley Code Description'				=>	'Entrez le texte du code qui correspondra à l\'affichage de l\'émoticône (ex. :O)).',
+	'Smiley Image Description'				=>	'Entrez le nom de l\'image qui correspond au texte du code ci-dessus (ex. bignose.png).',
+	'Submit Smiley'				=>	'Soumettre l\'émoticône',
+
+	// Easy Smilies (delete a smiley)
+	'Delete Smiley Redirect'			=>	'Les émoticônes ont été effacées avec succès. Vous allez être redirigé maintenant.',
+	'Current Smilies'				=>	'Emoticônes installées',
+	'Code'				=>	'Code',
+	'Image'				=>	'Image',
+	'Image Filename'				=>	'Nom du fichier de l\'image',
+	'Delete Smiley'				=>	'Effacer',
+);
+
+?>
