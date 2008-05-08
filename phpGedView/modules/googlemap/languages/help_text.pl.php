9e7ff9da8de5417388340fd11e8c51c9d43423df
NAME <USER@DOMAIN>
2008-05-08 01:42:20 +0000

    update GM language files
    git-svn-id: svn://svn.webtrees.net/trunk@3022 4201c862-f99b-1943-94d0-25fd05010cbc

--- phpGedView/modules/googlemap/languages/help_text.pl.php
+++ phpGedView/modules/googlemap/languages/help_text.pl.php
@@ -33,7 +33,7 @@ $pgv_lang["GOOGLEMAP_CONFIG"]           = "Konfiguracja Googlemap";
 $pgv_lang["GOOGLEMAP_CONFIG_help"]      = "~#pgv_lang[GOOGLEMAP_CONFIG]#~<br /><br />W tym miejscu możesz skonfigurować wszystkie aspekty modułu  GoogleMap.";
 
 $pgv_lang["GOOGLEMAP_ENABLE"]           = "Włącz Googlemap";
-$pgv_lang["GOOGLEMAP_ENABLE_help"]      = "~#pgv_lang[GOOGLEMAP_ENABLE]#~<br /><br />Używająć tej opcji możesz włączać i wyłączać Googlemap.<br/>Jeśli moduł jest wyłączony zakładka Mapa na stronie z danymi osoby jest pokazana, ale nie wyświetla mapy. Odnośnik konfiguracyjny dla administratora jest dostępny. Jeśli moduł jest wyłączony hierarchia miejsc jest wyświetlana standardowo.";
+$pgv_lang["GOOGLEMAP_ENABLE_help"]      = "~#pgv_lang[GOOGLEMAP_ENABLE]#~<br /><br />Używająć tej opcji możesz włączać i wyłączać Googlemap.<br />Jeśli moduł jest wyłączony zakładka Mapa na stronie z danymi osoby jest pokazana, ale nie wyświetla mapy. Odnośnik konfiguracyjny dla administratora jest dostępny. Jeśli moduł jest wyłączony hierarchia miejsc jest wyświetlana standardowo.";
 
 $pgv_lang["GOOGLEMAP_API_KEY"]          = "Google-map API key";
 $pgv_lang["GOOGLEMAP_API_KEY_help"]     = "~#pgv_lang[GOOGLEMAP_API_KEY]#~<br /><br />Wprowadź tu swój klucz Google Map API. Możesz go otrzymać tutaj: <a target=\"_blank\" href=\"http://www.google.com/apis/maps/\">http://www.google.com/apis/maps/</a>";
@@ -54,7 +54,7 @@ $pgv_lang["GM_DEFAULT_LEVEL_0"]         = "Domyślna wartość głównego poziom
 $pgv_lang["GM_DEFAULT_LEVEL_0_help"]    = "~#pgv_lang[GM_DEFAULT_LEVEL_0]#~<br /><br />Tu możesz zdefiniować domyślną wartość dla głównego poziomu w hierarchii miejsc. Jeśli miejsce nie zostanie znaleznione jego nazwa zostanie dodana jako najwyższy poziom (kraj) i baza zostanie przeszukana ponownie.";
 
 $pgv_lang["GM_NOF_LEVELS"]              = "Liczba poziomów miejsc";
-$pgv_lang["GM_NOF_LEVELS_help"]         = "~#pgv_lang[GM_NOF_LEVELS]#~<br /><br />W tym miejscu można określić liczbę poziomów w hierarchii miejsc, która jest używana przez moduł Googlemap.<br/>Wartością domyślną jest 4 (Kraj, województwo, powiat, miejscowość). Jeśli chcesz dodać dodatkowe poziomy (na przykład szkoły, cmentarze) możesz zmienić liczbę poziomów. Jeśli chcesz usunąc poziom (na przykład Kraj) możesz zmniejszyć tą wartość, ale pamiętaj, że pliki zawierające współrzędne miejsc mają strukturę 4-poziomową.";
+$pgv_lang["GM_NOF_LEVELS_help"]         = "~#pgv_lang[GM_NOF_LEVELS]#~<br /><br />W tym miejscu można określić liczbę poziomów w hierarchii miejsc, która jest używana przez moduł Googlemap.<br />Wartością domyślną jest 4 (Kraj, województwo, powiat, miejscowość). Jeśli chcesz dodać dodatkowe poziomy (na przykład szkoły, cmentarze) możesz zmienić liczbę poziomów. Jeśli chcesz usunąc poziom (na przykład Kraj) możesz zmniejszyć tą wartość, ale pamiętaj, że pliki zawierające współrzędne miejsc mają strukturę 4-poziomową.";
 
 $pgv_lang["GM_NAME_PREFIX"]             = "Przedrostek dla nazw w tym poziomie";
 $pgv_lang["GM_NAME_PREFIX_help"]        = "~#pgv_lang[GM_NAME_PREFIX]#~<br /><br />Ta wartość będzie dodana na początku nazw tego poziomu. Różne wartości mogą być używane, ale należy rozdzielić je średnikiem.";
@@ -63,10 +63,10 @@ $pgv_lang["GM_NAME_POSTFIX"]            = "Przyrostek dla nazw w tym poziomie";
 $pgv_lang["GM_NAME_POSTFIX_help"]       = "~#pgv_lang[GM_NAME_POSTFIX]#~<br /><br />Ta wartość będzie dodana na końcu nazw tego poziomu. Różne wartości mogą być używane, ale należy rozdzielić je średnikiem.";
 
 $pgv_lang["GM_NAME_PRE_POST"]           = "Porządek przedrostka/przyrostka";
-$pgv_lang["GM_NAME_PRE_POST_help"]      = "~#pgv_lang[GM_NAME_PRE_POST]#~<br /><br />To pole wyznacza porządek w jakim nazwy będą używać przedrostka i przyrostka. Możliwe wartości:<br/><ul><li>Brak</li><li>Normalny, przedrostek, przyrostek, oba</li><li>Normalny, przyrostek, przedrostek, oba</li><li>Przedrostek, przyrostek, oba, normalny</li><li>Przyrostek, przedrostek, oba, normalny</li><li>Przedrostek, przyrostek, normalny, oba</li><li>Przyrostek, przedrostek, normalny, oba</li></ul>";
+$pgv_lang["GM_NAME_PRE_POST_help"]      = "~#pgv_lang[GM_NAME_PRE_POST]#~<br /><br />To pole wyznacza porządek w jakim nazwy będą używać przedrostka i przyrostka. Możliwe wartości:<br /><ul><li>Brak</li><li>Normalny, przedrostek, przyrostek, oba</li><li>Normalny, przyrostek, przedrostek, oba</li><li>Przedrostek, przyrostek, oba, normalny</li><li>Przyrostek, przedrostek, oba, normalny</li><li>Przedrostek, przyrostek, normalny, oba</li><li>Przyrostek, przedrostek, normalny, oba</li></ul>";
 
 $pgv_lang["PL_EDIT_LOCATION"]           = "Edytuj lub usuń miejsce";
-$pgv_lang["PL_EDIT_LOCATION_help"]      = "W tym miejscu można edytować lub usuwać miejsca. Jeśli klikniejsz na Edytuj zostanie otwarte nowe okno, w którym będzie można zmienić dane geograficzne.<br/>Jeśli klikniesz na czerwony krzyżyk miejsce zostanie usunięte. Usunąć można jedynie miejsca, które nie mają miejsc podrzędnych.";
+$pgv_lang["PL_EDIT_LOCATION_help"]      = "W tym miejscu można edytować lub usuwać miejsca. Jeśli klikniejsz na Edytuj zostanie otwarte nowe okno, w którym będzie można zmienić dane geograficzne.<br />Jeśli klikniesz na czerwony krzyżyk miejsce zostanie usunięte. Usunąć można jedynie miejsca, które nie mają miejsc podrzędnych.";
 
 $pgv_lang["PL_ADD_LOCATION"]            = "Dodaj nowe miejsce";
 $pgv_lang["PL_ADD_LOCATION_help"]       = "Użyj w celu dodania miejsca do tablicy. Miejsce będzie dodane w bieżącym poziomie.";
@@ -100,7 +100,7 @@ $pgv_lang["PLE_PRECISION"]              = "Precyzja";
 $pgv_lang["PLE_PRECISION_help"]         = "W tym miejscu możesz ustawić precyzję. Liczba miejsc po przecinku we współrzędnych odnosi się do tej wartości.";
 
 $pgv_lang["PLE_LATLON_CTRL"]            = "Wprowadź współrzędne";
-$pgv_lang["PLE_LATLON_CTRL_help"]       = "W tym miejscu możesz ustawić współrzędne. Najpierw ustaw szerokość i długość geograficzną (E/W lub N/S). Następnie wprowadź współrzędne. Powinny być w formacie dziesiętnym.<br/>Wartości dziesiętne można obliczyć przeliczając minuty i sekundy według podanego wzoru:<br/>stopnie_dziesiętne = ((sekundy / 60) + minuty) / 60 + stopnie.";
+$pgv_lang["PLE_LATLON_CTRL_help"]       = "W tym miejscu możesz ustawić współrzędne. Najpierw ustaw szerokość i długość geograficzną (E/W lub N/S). Następnie wprowadź współrzędne. Powinny być w formacie dziesiętnym.<br />Wartości dziesiętne można obliczyć przeliczając minuty i sekundy według podanego wzoru:<br />stopnie_dziesiętne = ((sekundy / 60) + minuty) / 60 + stopnie.";
 
 $pgv_lang["PLE_ZOOM"]                   = "Powiększenie";
 $pgv_lang["PLE_ZOOM_help"]              = "W tym miejscu możesz ustawić powiększenie. Ta wartość będzie używana jako minimalna wartość podczas wyświetlania miejsc na mapie.";
@@ -119,21 +119,21 @@ $pgv_lang["PLIF_CLEAN"]                 = "Wyczyść bazę miejsc";
 $pgv_lang["PLIF_CLEAN_help"]            = "Jeśli ta opcja jest włączona baza lokalizacji miejsc zostanie wyczyszczona. To oznacza, że tylko miejsca występujące w tej tablicy zostaną usunięte. Ta opcja nie zmienia miejsc w pliku GEDCOM.";
 
 $pgv_lang["PLIF_UPDATE"]                = "Aktualizuj tylko istniejące miejsca";
-$pgv_lang["PLIF_UPDATE_help"]           = "Aktualizuj tylko istniejące w bazie miejsca.<br/>Jeśli ta opcja jest włączona tylko istniejące miejsca będą zaktualizowane. This can be used to fill in latitude and longitude of places that have been imported from a GEDCOM. No new places will be added to the database.";
+$pgv_lang["PLIF_UPDATE_help"]           = "Aktualizuj tylko istniejące w bazie miejsca.<br />Jeśli ta opcja jest włączona tylko istniejące miejsca będą zaktualizowane. This can be used to fill in latitude and longitude of places that have been imported from a GEDCOM. No new places will be added to the database.";
 
 $pgv_lang["PLIF_OVERWRITE"]             = "Nadpisać dane lokalizacji danymi z pliku";
-$pgv_lang["PLIF_OVERWRITE_help"]        = "Nadpisać dane lokalizacji w bazie danymi z pliku.<br/>Jeśli ta opcja jest włączona, dane w bazie (współrzędne, powiększenie i flaga) zostaną nadpisane danymi z pliku, jeśli w pliku będą dostępne. Jeśli miejsce nie istnieje w bazie zostanie stworzony nowy wpis, ale tylko gdy opcja Aktualizuj tylko istniejące miejsca nie będzie zaznaczona.";
+$pgv_lang["PLIF_OVERWRITE_help"]        = "Nadpisać dane lokalizacji w bazie danymi z pliku.<br />Jeśli ta opcja jest włączona, dane w bazie (współrzędne, powiększenie i flaga) zostaną nadpisane danymi z pliku, jeśli w pliku będą dostępne. Jeśli miejsce nie istnieje w bazie zostanie stworzony nowy wpis, ale tylko gdy opcja Aktualizuj tylko istniejące miejsca nie będzie zaznaczona.";
 
 $pgv_lang["PLE_ACTIVE"]             	= "Pokaż miejsca nieaktywne";
-$pgv_lang["PLE_ACTIVE_help"]        	= "<strong>Lista miejsc w tablicy GoogleMaps, które nie są używane w pliku GEDCOM.</strong><br/><br/>Domyślnie jest włączona opcja wyświetlania miejsc, które występują w tablicy GoogleMaps i pliku GEDCOM jednocześnie.<br/><br/>Jeśli ta opcja jest zaznaczona i zostanie wciśnięty przycisk \"Pokaż\" na liście zostaną wyświetlone wszystkie miejsca z tego poziomu.";
+$pgv_lang["PLE_ACTIVE_help"]        	= "<strong>Lista miejsc w tablicy GoogleMaps, które nie są używane w pliku GEDCOM.</strong><br /><br />Domyślnie jest włączona opcja wyświetlania miejsc, które występują w tablicy GoogleMaps i pliku GEDCOM jednocześnie.<br /><br />Jeśli ta opcja jest zaznaczona i zostanie wciśnięty przycisk \"Pokaż\" na liście zostaną wyświetlone wszystkie miejsca z tego poziomu.";
 
 // Help text for placecheck.php
 $pgv_lang["GOOGLEMAP_PLACECHECK"]       = "Sprawdź miejsca";
-$pgv_lang["GOOGLEMAP_PLACECHECK_help"]  = "~#pgv_lang[GOOGLEMAP_PLACECHECK]#~<br /><br /><strong>To narzędzie</strong> pozwala na porównanie miejsc z pliku GEDCOM i tablicy GoogleMaps.<BR/><BR/><strong>Dane</strong> mogą być wyświetlone dla danego pliku GEDCOM; dla danego kraju w tym pliku; i dla innych obszarów (na przykład województwo lub powiat) w danym kraju.<BR/><BR/><strong>Miejsca</strong> są wyświetlane alfabetycznie.<BR/><BR/><strong>Na liście</strong> wyników porównania możesz kliknąć na nazwę miejsca, zostaniesz odesłany do jednej z trzech opcji:<BR/><BR/><strong>1 - </strong>Dla miejsc z pliku GEDCOM zostaniesz odesłany do Hierarchii miejsc. Tam będziesz mógł zobaczyć wszystko co jest połączone z danym miejscem.<BR/><BR/><strong>2 - </strong>Dla miejsc istniejących w pliku GEDCOM, ale nie istniejących w tablicy Googlemap (wyświetlone na czerwono) zostaniesz odesłany do opcji \"Dodaj miejsce\".<BR/><BR/><strong>3 - </strong>Dla miejsc istniejących w pliku GEDCOM i tablicy Googlemap (prawdopodobnie bez wspórzędnych) zostaniesz odesłany do opcji \"Edytuj miejsce\". Tam będziesz mógł edytować opcje wykorzystywane do wyświetlania danego miejsca.<BR/><BR/><strong>Najeżdżając</strong> kursorem na miesjce zostanie wyświetlona informacja na temat ustawionego powiększenia dla tego miejsca.";
+$pgv_lang["GOOGLEMAP_PLACECHECK_help"]  = "~#pgv_lang[GOOGLEMAP_PLACECHECK]#~<br /><br /><strong>To narzędzie</strong> pozwala na porównanie miejsc z pliku GEDCOM i tablicy GoogleMaps.<br /><br /><strong>Dane</strong> mogą być wyświetlone dla danego pliku GEDCOM; dla danego kraju w tym pliku; i dla innych obszarów (na przykład województwo lub powiat) w danym kraju.<br /><br /><strong>Miejsca</strong> są wyświetlane alfabetycznie.<br /><br /><strong>Na liście</strong> wyników porównania możesz kliknąć na nazwę miejsca, zostaniesz odesłany do jednej z trzech opcji:<br /><br /><strong>1 - </strong>Dla miejsc z pliku GEDCOM zostaniesz odesłany do Hierarchii miejsc. Tam będziesz mógł zobaczyć wszystko co jest połączone z danym miejscem.<br /><br /><strong>2 - </strong>Dla miejsc istniejących w pliku GEDCOM, ale nie istniejących w tablicy Googlemap (wyświetlone na czerwono) zostaniesz odesłany do opcji \"Dodaj miejsce\".<br /><br /><strong>3 - </strong>Dla miejsc istniejących w pliku GEDCOM i tablicy Googlemap (prawdopodobnie bez wspórzędnych) zostaniesz odesłany do opcji \"Edytuj miejsce\". Tam będziesz mógł edytować opcje wykorzystywane do wyświetlania danego miejsca.<br /><br /><strong>Najeżdżając</strong> kursorem na miesjce zostanie wyświetlona informacja na temat ustawionego powiększenia dla tego miejsca.";
 $pgv_lang["PLACECHECK_FILTER"]       	= "Sprawdź miejsca - Opcje filtrowania listy";
 $pgv_lang["PLACECHECK_FILTER_help"]  	= "~#pgv_lang[PLACECHECK_FILTER]#~<br /><br />Tutaj możesz określać miejsca jakie będą wyświetlone.";
 $pgv_lang["PLACECHECK_MATCH"]       	= "Dołącz identyczne miejsca";
-$pgv_lang["PLACECHECK_MATCH_help"]  	= "~#pgv_lang[PLACECHECK_MATCH]#~<br /><br />Domyślnie nie są wyświetlane miejsca będące identyczne w pliku GEDCOM i tablicy GoogleMap.<br/>Identyczne, to znaczy wszystkie poziomy są takie same w pliku GEDCOM i tablicy GoogleMap i w tablicy są podane współrzędne dla każdego poziomu.<br/><br/>Zaznacz w celu dołączenia do listy tych miejsc";
+$pgv_lang["PLACECHECK_MATCH_help"]  	= "~#pgv_lang[PLACECHECK_MATCH]#~<br /><br />Domyślnie nie są wyświetlane miejsca będące identyczne w pliku GEDCOM i tablicy GoogleMap.<br />Identyczne, to znaczy wszystkie poziomy są takie same w pliku GEDCOM i tablicy GoogleMap i w tablicy są podane współrzędne dla każdego poziomu.<br /><br />Zaznacz w celu dołączenia do listy tych miejsc";
 
 //wooc Options for Place Hierarchy display
 $pgv_lang["GOOGLEMAP_PH"]				= "Użyj Googlemap do wyświetlania hierarchii miejsc";
@@ -143,7 +143,7 @@ $pgv_lang["GOOGLEMAP_PH_MAP_SIZE_help"]	= "~#pgv_lang[GOOGLEMAP_PH_MAP_SIZE]#~<b
 $pgv_lang["GOOGLEMAP_PH_MARKER"]		= "Typ znacznika miejsca w hierarchii miejsc";
 $pgv_lang["GOOGLEMAP_PH_MARKER_help"]	= "~#pgv_lang[GOOGLEMAP_PH_MARKER]#~<br /><br />Tutaj możesz określić czy do wyświetlania miejsc w hierarchii będzie użyty standardowy znacznik czy przypisana flaga (jeśli brak przypisanej flagi, będzie wyświetlony standardowy znacznik).";
 $pgv_lang["GM_DISP_SHORT_PLACE"]		= "Wyświetl skrócone nazwy miejsc";
-$pgv_lang["GM_DISP_SHORT_PLACE_help"]	= "~#pgv_lang[GM_DISP_SHORT_PLACE]#~<br /><br />Tutaj możesz określić czy miejsca wyświetlane w hierarchii będą miały wyświetlone pełne nazwy czy tylko będzie wyświetlona nazwa aktualnego poziomu/miejsca.";
+$pgv_lang["GM_DISP_SHORT_PLACE_help"]	= "~#pgv_lang[GM_DISP_SHORT_PLACE]#~<br /><br />Tutaj możesz określić czy miejsca wyświetlane w hierarchii będą miały wyświetlone pełne nazwy czy tylko będzie wyświetlona nazwa aktualnego poziomu/miejsca.<br /><b>Przykłady:<br />Pełna nazwa: </b>Warszawa, Mazowieckie, Polska<br /><b>Skrócona nazwa: </b>Warszawa<br /><b>Pełna nazwa: </b>Mazowieckie, Polska<br /><b>Skrócona nazwa: </b>Mazowieckie";
 $pgv_lang["GM_DISP_COUNT"]				= "Wyświetl liczbę osób i rodzin połączonych z danym miejscem";
 $pgv_lang["GM_DISP_COUNT_help"]			= "~#pgv_lang[GM_DISP_COUNT]#~<br /><br />Możesz określić czy liczba osób i rodzin powiązana z danym miejscem będzie wyświetlana. Przy dużej ilości osób i miejsc zaleca się wyłączenie tej opcji.";
 $pgv_lang["GOOGLEMAP_PH_WHEEL"]			= "Użyj rolki myszki do przybliżania/oddalania";
