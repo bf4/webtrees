3d02c2b7085afa1d86d340b6f09ed9ef63b10847
NAME <USER@DOMAIN>
2008-05-26 10:32:17 +0000

    Force update of $Id: versioning line
    git-svn-id: svn://svn.webtrees.net/trunk@3126 4201c862-f99b-1943-94d0-25fd05010cbc

--- phpGedView/themes/simplygreen/toplinks.php
+++ phpGedView/themes/simplygreen/toplinks.php
@@ -3,7 +3,7 @@
  * Top-of-page menu for SimplyGreen theme
  *
  * PhpGedView: Genealogy Viewer
- * Copyright (C) 2002 to 2008  John Finlay and others.  All rights reserved.
+ * Copyright (c) 2002 to 2008  John Finlay and others.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
