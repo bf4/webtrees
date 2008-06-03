fdb8f66b31120b2f2f67bf76129c0926f23fa11d
NAME <USER@DOMAIN>
2008-06-03 23:15:46 +0000

    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView/blocks@3200 4201c862-f99b-1943-94d0-25fd05010cbc

--- block_htmlplus.php
+++ block_htmlplus.php
@@ -5,7 +5,7 @@
  * This block will print advanced HTML text with keyword support entered by an admin
  *
  * phpGedView: Genealogy Viewer
- * Copyright (C) 2002 to 2008  John Finlay and Others
+ * Copyright (C) 2002 to 2008  PGV Development Team.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -101,6 +101,12 @@ function print_htmlplus_block($block=true, $config='', $side, $index)
 	{
 		$stats = new stats($GEDCOM);
 	}
+	
+	// Make some values from the GEDCOM's 0 HEAD record visible to the world
+	global $CREATED_SOFTWARE, $CREATED_VERSION, $CREATED_DATE;
+	$CREATED_SOFTWARE = $stats->gedcomCreatedSoftware();
+	$CREATED_VERSION = $stats->gedcomCreatedVersion();
+	$CREATED_DATE = $stats->gedcomDate();	
 
 	/*
 	 * First Pass.
