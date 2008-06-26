5108a082634b65c14e51b1c89d6532e99bd65e31
NAME <USER@DOMAIN>
2008-06-26 17:22:46 +0000

    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@3405 4201c862-f99b-1943-94d0-25fd05010cbc

--- modules/lightbox/languages/help_text.en.php
+++ modules/lightbox/languages/help_text.en.php
@@ -5,7 +5,7 @@
  * Display media Items using Lightbox
  *
  * phpGedView: Genealogy Viewer
- * Copyright (C) 2002 to 2007  PhpGedView developers
+ * Copyright (C) 2007 to 2008  PGV Development Team.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -37,96 +37,17 @@ if (stristr($_SERVER["SCRIPT_NAME"], basename(__FILE__))!==false) {
 
 // Lightbox general help file  ---------------------------------------------------------------------------------------------------------
 $pgv_lang["lb_generalLegend"]		 = "Lightbox Album - General Help";
-
-$pgv_lang["lb_general_help"]		 = "~#pgv_lang[lb_generalLegend]#~";
-$pgv_lang["lb_general_help"]		.= "<br /><br />";
-$pgv_lang["lb_general_help"]		.= "#pgv_lang[lb_general_help1]#";
-$pgv_lang["lb_general_help"]		.= "#pgv_lang[lb_general_help1a]#";
-$pgv_lang["lb_general_help"]		.= "#pgv_lang[lb_general_help1b]#";
-$pgv_lang["lb_general_help"]		.= "#pgv_lang[lb_general_help1c]#";
-$pgv_lang["lb_general_help"]		.= "#pgv_lang[lb_general_help1d]#";
-$pgv_lang["lb_general_help"]		.= "#pgv_lang[lb_general_help2]#";
-$pgv_lang["lb_general_help"]		.= "#pgv_lang[lb_general_help2a]#";
-$pgv_lang["lb_general_help"]		.= "#pgv_lang[lb_general_help3]#";
-$pgv_lang["lb_general_help"]		.= "#pgv_lang[lb_general_help3a]#";
-$pgv_lang["lb_general_help"]		.= "#pgv_lang[lb_general_help3b]#";
-$pgv_lang["lb_general_help"]		.= "#pgv_lang[lb_general_help3c]#";
-$pgv_lang["lb_general_help"]		.= "#pgv_lang[lb_general_help4]#";
-$pgv_lang["lb_general_help"]		.= "#pgv_lang[lb_general_help4a]#";
-$pgv_lang["lb_general_help"]		.= "#pgv_lang[lb_general_help5]#";
-$pgv_lang["lb_general_help"]		.= "#pgv_lang[lb_general_help5a]#";
-$pgv_lang["lb_general_help"]		.= "#pgv_lang[lb_general_help6]#";
-$pgv_lang["lb_general_help"]		.= "#pgv_lang[lb_general_help6a]#";
-$pgv_lang["lb_general_help"]		.= "#pgv_lang[lb_general_help7]#";
-$pgv_lang["lb_general_help"]		.= "#pgv_lang[lb_general_help7a]#";
-$pgv_lang["lb_general_help"]		.= "#pgv_lang[lb_general_help8]#";
-$pgv_lang["lb_general_help"]		.= "#pgv_lang[lb_general_help8a]#";
-$pgv_lang["lb_general_help"]		.= "#pgv_lang[lb_general_help9]#";
-$pgv_lang["lb_general_help"]		.= "#pgv_lang[lb_general_help9a]#";
-$pgv_lang["lb_general_help"]		.= "#pgv_lang[lb_general_help10]#";
-$pgv_lang["lb_general_help"]		.= "#pgv_lang[lb_general_help10a]#";
-
-$pgv_lang["lb_general_help1"]		 = "<br />~1 - To view Notes or details associated with an image~<br />";
-$pgv_lang["lb_general_help1a"]		 = "<b>Drop-Down Menu:</b><br />";
-$pgv_lang["lb_general_help1a"]		.= "Hover over the \"View\" link below the thumbnail and a dropdown menu will appear.";
-$pgv_lang["lb_general_help1a"]		.= "The options are \"View Notes\" (If there are any), \"View details\" (The default) and also \"View Source\" (if you are logged in and there is a source item).";
-$pgv_lang["lb_general_help1a"]		.= "<br />";
-$pgv_lang["lb_general_help1b"]		 = "<br /><b>Viewing:</b>";
-$pgv_lang["lb_general_help1b"]		.= "<br />Clicking View Notes will show a \"Tooltip\" with the note information inside. Click again to turn off the \"Tooltip\". ";
-$pgv_lang["lb_general_help1b"]		.= "<br />Clicking \"View Details\" will take you to the mediaviewer page, and \"View Source\" will (if authorized) take you to the Source page for the media Item";
-$pgv_lang["lb_general_help1b"]		.= "<br />";
-$pgv_lang["lb_general_help1c"]		 = "<br /><b>Editing:</b>";
-$pgv_lang["lb_general_help1c"]		.= "<br />(There are additional \"Edit\" options for Editors and above)";
-$pgv_lang["lb_general_help1c"]		.= "<br />";
-$pgv_lang["lb_general_help1d"]		 = "<br /><b>Opened Lightbox Image:</b>";
-$pgv_lang["lb_general_help1d"]		.= "<br />When viewing an opened Lightbox image, the View Notes and View Details icons can be clicked on the border below the image.";
-$pgv_lang["lb_general_help1d"]		.= "<br />";
-
-$pgv_lang["lb_general_help2"]		 = "<br />~2 - To view an image~<br />";
-$pgv_lang["lb_general_help2a"]		 = "Click on any thumbnail. The title of the image will appear at the top of the overlaid image.  ";
-$pgv_lang["lb_general_help2a"]		.= "<br />";
-
-$pgv_lang["lb_general_help3"]		 = "<br />~3 - To use zoom mode~<br />";
-$pgv_lang["lb_general_help3a"]		 = "<b>NOTE:</b><br />";
-$pgv_lang["lb_general_help3a"]		.= " The slideshow must be paused to see the Zoom icon.";
-$pgv_lang["lb_general_help3a"]		.= "<br />";
-$pgv_lang["lb_general_help3b"]		 = "<br /><b>Enable Zoom:</b><br />";
-$pgv_lang["lb_general_help3b"]		.= "When the green Plus icon at the bottom right of the image is visible, Zoom is already enabled. Use the mouse wheel up and down to resize. (Or use keys \"i\" and \"o\") The icon will change to a red Minus.";
-$pgv_lang["lb_general_help3b"]		.= "<br />";
-$pgv_lang["lb_general_help3b"]		.= "When the image is re-sized larger than the viewed page, drag and drop the image, or use the arrow keys to move the image around.";
-$pgv_lang["lb_general_help3b"]		.= "<br />";
-$pgv_lang["lb_general_help3c"]		 = "<br /><b>Disable Zoom:</b><br />";
-$pgv_lang["lb_general_help3c"]		.= "Doubleclick inside the image, or click on the red Minus icon at the bottom right, to get out of Zoom mode. (Or use the \"z\" key)";
-$pgv_lang["lb_general_help3c"]		.= "<br />";
-
-$pgv_lang["lb_general_help4"]		 = "<br />~4 - To close an image~<br />";
-$pgv_lang["lb_general_help4a"]		 = "Click outside of the image, or, click on the red X icon at bottom right, or use the \"x\" key. ";
-$pgv_lang["lb_general_help4a"]		.= "<br />";
-
-$pgv_lang["lb_general_help5"]		 = "<br />~5 - To view the next or previous image~";
-$pgv_lang["lb_general_help5a"]		 = "<br />As you mouse over the image when NOT in Zoom mode, a <b>&lt;</b> symbol will appear on the left side, and a <b>&gt;</b> on the right. Click anywhere in the right half of the image to see the next image. Click anywhere in the left half to see the previous one.";
-$pgv_lang["lb_general_help5a"]		.= "<br />";
-
-$pgv_lang["lb_general_help6"]		 = "<br />~6 - To jump to any other image in the Album~<br />";
-$pgv_lang["lb_general_help6a"]		 = "As you mouse over the top 1 cm of the image when NOT in Zoom mode, a thumbnail Gallery will appear. If necessary, move the mouse cursor left and right to make other sections of this thumbnail Gallery show.  Click any Gallery thumbnail to jump directly to the associated image.";
-$pgv_lang["lb_general_help6a"]		.= "<br /><br />";
-$pgv_lang["lb_general_help6a"]		.= "<b>Next</b>, <b>Previous</b> and <b>Jump</b> may be done whether the slideshow is running or paused.";
-$pgv_lang["lb_general_help6a"]		.= "<br /> ";
-
-$pgv_lang["lb_general_help7"]		 = "<br />~7 - To run the slide show~<br />";
-$pgv_lang["lb_general_help7a"]		 = "Click on the Start icon at bottom left. If there is a sound track file, the Speaker icon will appear.  Click on the Speaker icon to toggle the sound track on and off. Click on the Pause icon to stop the slide show.";
-$pgv_lang["lb_general_help7a"]		.= "<br /> ";
-
-$pgv_lang["lb_general_help8"]		 = "<br />~8 - Navigation ...~<br />";
-$pgv_lang["lb_general_help8a"]		 = "Use the \"View Album of ...\" table at the right of the image icon table to directly choose another person's Album view. ";
-$pgv_lang["lb_general_help8a"]		.= "<br />";
-
-$pgv_lang["lb_general_help9"]		 = "<br />~9 - Note:~<br />";
-$pgv_lang["lb_general_help9a"]		 = "Thumbnails which are NOT images, such as PDF files and audio, book, and video Media types, may be viewed individually, but will not be in the slide show.";
-$pgv_lang["lb_general_help9a"]		.= "<br /> ";
-
-$pgv_lang["lb_general_help10"]		 = "<br />~10 - Note for Administrator:~<br />";
-$pgv_lang["lb_general_help10a"]		 = "If any files of the usual image formats (jpg, bmp, gif, etc.) representing image types such as photo, certificate, document, etc. appear in the <b>Other</b> row, it means that the Media \"TYPE\" has nor been set for these objects.  You may wish to edit the Media \"TYPE\" for these items.";
+$pgv_lang["lb_general_help"]		 = "~#pgv_lang[lb_generalLegend]#~<br /><br /><ul>#pgv_lang[lb_general_help1]##pgv_lang[lb_general_help2]##pgv_lang[lb_general_help3]##pgv_lang[lb_general_help4]##pgv_lang[lb_general_help5]##pgv_lang[lb_general_help6]##pgv_lang[lb_general_help7]##pgv_lang[lb_general_help8]##pgv_lang[lb_general_help9]##pgv_lang[lb_general_help10]#</ul>";
+$pgv_lang["lb_general_help1"]		 = "<li>~To view Notes or details associated with an image~<br /><b>Drop-Down Menu:</b><br />Hover over the <b>#pgv_lang[lb_viewedit]#</b> link below the thumbnail and a dropdown menu will appear. The options are <b>#pgv_lang[lb_viewnotes]#</b> (If there are any), <b>#pgv_lang[lb_viewdetails]#</b> (the default) and also <b>#pgv_lang[lb_viewsource]#</b> (if you are logged in and there is a source item).<br /><br /><b>Viewing:</b><br />Clicking <b>#pgv_lang[lb_viewnotes]#</b> will show a <b>#pgv_lang[lb_balloon_true]#</b> with the Note information inside. Click again to turn off the <b>#pgv_lang[lb_balloon_true]#</b>.<br />Clicking <b>#pgv_lang[lb_viewdetails]#</b> will take you to the Mediaviewer page, and <b>#pgv_lang[lb_viewsource]#</b> will (if authorized) take you to the Source page for the media item.<br /><br /><b>Editing:</b><br />(There are additional Edit options for Editors and above)<br /><br /><b>Opened Lightbox Image:</b><br />When viewing an opened Lightbox image, the <b>#pgv_lang[lb_viewnotes]#</b> and <b>#pgv_lang[lb_viewdetails]#</b> icons can be clicked on the border below the image.<br /><br /></li>";
+$pgv_lang["lb_general_help2"]		 = "<li>~To view an image~<br />Click on any thumbnail. The title of the image will appear at the top of the overlaid image.<br /><br /></li>";
+$pgv_lang["lb_general_help3"]		 = "<li>~To use zoom mode~<br /><b>NOTE:</b><br />The slideshow must be paused to see the Zoom icon.<br /><br /><b>Enable Zoom:</b><br />When the green Plus icon at the bottom right of the image is visible, Zoom is already enabled. Use the mouse wheel up and down to resize. (Or use keys <b>i</b> and <b>o</b>) The icon will change to a red Minus.<br />When the image is re-sized larger than the viewed page, drag and drop the image, or use the arrow keys to move the image around.<br /><br /><b>Disable Zoom:</b><br />Doubleclick inside the image, or click on the red Minus icon at the bottom right, to get out of Zoom mode. (Or use the <b>z</b> key)<br /><br /></li>";
+$pgv_lang["lb_general_help4"]		 = "<li>~To close an image~<br />Click outside of the image, or, click on the red <b>X</b> icon at bottom right, or use the <b>x</b> key.<br /><br /></li>";
+$pgv_lang["lb_general_help5"]		 = "<li>~To view the next or previous image~<br />As you mouse over the image when NOT in Zoom mode, a <b>&lt;</b> symbol will appear on the left side, and a <b>&gt;</b> on the right. Click anywhere in the right half of the image to see the next image. Click anywhere in the left half to see the previous one.<br /><br /></li>";
+$pgv_lang["lb_general_help6"]		 = "<li>~To jump to any other image in the Album~<br />As you mouse over the top 1 cm of the image when NOT in Zoom mode, a thumbnail Gallery will appear. If necessary, move the mouse cursor left and right to make other sections of this thumbnail Gallery show.  Click any Gallery thumbnail to jump directly to the associated image.<br /><br /><b>Next</b>, <b>Previous</b> and <b>Jump</b> may be done whether the slideshow is running or paused.<br /><br /></li>";
+$pgv_lang["lb_general_help7"]		 = "<li>~To run the slide show~<br />Click on the Start icon at bottom left. If there is a sound track file, the Speaker icon will appear.  Click on the Speaker icon to toggle the sound track on and off. Click on the Pause icon to stop the slide show.<br /><br /></li>";
+$pgv_lang["lb_general_help8"]		 = "<li>~Navigation ...~<br />Use the <b>#pgv_lang[view_lightbox]#</b> table at the right of the image icon table to directly choose another person's Album view.<br /><br /></li>";
+$pgv_lang["lb_general_help9"]		 = "<li>~Note:~<br />Thumbnails which are NOT images, such as PDF files and audio, book, and video Media types, may be viewed individually, but will not be in the slide show.<br /><br /></li>";
+$pgv_lang["lb_general_help10"]		 = "<li>~Note for Administrator:~<br />If any files of the usual image formats (jpg, bmp, gif, etc.) representing image types such as photo, certificate, document, etc. appear in the <b>Other</b> row, the <b>#factarray[TYPE]#</b> value has not been set for these Media objects.  You may wish to edit the Media object's details to set this value.</li>";
 //End Lightbox General Help File ----------------------------------------------------------------------------------------------------------------------------- 
 
 $pgv_lang["lb_tt_balloonLegend"]		= "Album Tab Thumbnail - Notes Link Tooltip";
