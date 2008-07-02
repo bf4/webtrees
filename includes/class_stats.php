76d6ff43cd585a2b7bb27f07ff36b78fd64cea37
NAME <USER@DOMAIN>
2008-07-02 21:07:44 +0000

    Correct list of given names in Advanced HTML block and in Top 10 Given Names block
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@3450 4201c862-f99b-1943-94d0-25fd05010cbc

--- includes/class_stats.php
+++ includes/class_stats.php
@@ -1604,12 +1604,13 @@ class stats {
 					$allNames = get_indi_names($row[1], false, false);		// Get all names (except Married name)
 					foreach ($allNames as $name) {
 						$firstnamestring = preg_replace(':/.*/:', '', $name[0]);		// Remove surname
-						$firstnamestring = str_replace(array('*', '.', '-', '_', ',', '(', ')', '[', ']', '{', '}', '@'), ' ', $firstnamestring); 
-						$nameList = explode(" ", $firstnamestring);
+						$firstnamestring = ' '.str_replace(array('*', '.', '-', '_', ',', '(', ')', '[', ']', '{', '}', '@'), ' ', $firstnamestring).' '; 
+						// Remove names within quotes and apostrophes
+						$firstnamestring = preg_replace(array(": '.*' :", ': ".*" :'), ' ', $firstnamestring);
+						$firstnamestring = preg_replace(": (\xC2\xAB|\xC2\xBB|\xEF\xB4\xBF|\xEF\xB4\xBE|\xE2\x80\xBA|\xE2\x80\xB9|\xE2\x80\x9E|\xE2\x80\x9C|\xE2\x80\x9D|\xE2\x80\x9A|\xE2\x80\x98|\xE2\x80\x99).*(\xC2\xAB|\xC2\xBB|\xEF\xB4\xBF|\xEF\xB4\xBE|\xE2\x80\xBA|\xE2\x80\xB9|\xE2\x80\x9E|\xE2\x80\x9C|\xE2\x80\x9D|\xE2\x80\x9A|\xE2\x80\x98|\xE2\x80\x99) :", ' ', $firstnamestring);
+
+						$nameList = explode(" ", trim($firstnamestring));
 						foreach ($nameList as $givnName) {
-							// Remove names within quotes and apostrophes
-							$givnName = preg_replace(array(":^'.*'$:", ':^".*"$:'), '', $givnName);
-							$givnName = preg_replace(":^(\xC2\xAB|\xC2\xBB|\xEF\xB4\xBF|\xEF\xB4\xBE|\xE2\x80\xBA|\xE2\x80\xB9|\xE2\x80\x9E|\xE2\x80\x9C|\xE2\x80\x9D|\xE2\x80\x9A|\xE2\x80\x98|\xE2\x80\x99).*(\xC2\xAB|\xC2\xBB|\xEF\xB4\xBF|\xEF\xB4\xBE|\xE2\x80\xBA|\xE2\x80\xB9|\xE2\x80\x9E|\xE2\x80\x9C|\xE2\x80\x9D|\xE2\x80\x9A|\xE2\x80\x98|\xE2\x80\x99)$:", '', $givnName);
 							$givnName = trim($givnName);
 
 							// Ignore single letters
