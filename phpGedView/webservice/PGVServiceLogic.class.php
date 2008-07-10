f78b90f6839c0e8f0f51bfd64af4675d417dddf3
NAME <USER@DOMAIN>
2008-07-10 13:51:18 +0000

    fixed [ 2014909 ] 3520 WebService Error
    git-svn-id: svn://svn.webtrees.net/trunk@3524 4201c862-f99b-1943-94d0-25fd05010cbc

--- phpGedView/webservice/PGVServiceLogic.class.php
+++ phpGedView/webservice/PGVServiceLogic.class.php
@@ -324,7 +324,7 @@ class PGVServiceLogic extends GenealogyService
 		{
 			if (!empty($gedcom))
 			{
-				if (((empty($_SESSION['readonly']))&& PGV_USER_CAN_EDIT))&&(displayDetailsById($RID)))
+				if (empty($_SESSION['readonly'])&& PGV_USER_CAN_EDIT && displayDetailsById($RID))
 				{
 					$gedrec = preg_replace(array("/\\\\+r/","/\\\\+n/"), array("\r","\n"), $gedcom);
 					$success = replace_gedrec($RID, $gedrec);
@@ -1195,7 +1195,7 @@ class PGVServiceLogic extends GenealogyService
 		}
 		else if ($position=='new') {
 			//AddToLog("getXref position=new type=$type readonly=".$_SESSION['readonly']." username=".getUserName());
-			if ((empty($_SESSION['readonly']))&& PGV_USER_CAN_EDIT)) {
+			if (empty($_SESSION['readonly']) && PGV_USER_CAN_EDIT) {
 				if ((empty($type))||(!in_array($type, array("INDI","FAM","SOUR","REPO","NOTE","OBJE","OTHER")))) {
 					addDebugLog("getXref type=$type position=$position ERROR 18: Invalid \$type specification.  Valid types are INDI, FAM, SOUR, REPO, NOTE, OBJE, or OTHER");
 					//print "ERROR 18: Invalid \$type specification.  Valid types are INDI, FAM, SOUR, REPO, NOTE, OBJE, or OTHER\n";
