24f941c43296a336d169a8c0459ccb5ab677a716
NAME <USER@DOMAIN>
2008-07-11 16:58:07 +0000

    fixing upgrade error
    git-svn-id: svn://svn.webtrees.net/trunk@3534 4201c862-f99b-1943-94d0-25fd05010cbc

--- phpGedView/includes/mutex_class.php
+++ phpGedView/includes/mutex_class.php
@@ -63,6 +63,7 @@ class Mutex {
 		//-- check if this mutex already exists
 		$sql="SELECT 1 FROM {$TBLPREFIX}mutex WHERE mx_name='".$DBCONN->escapeSimple($name)."'";
 		$res=dbquery($sql);
+		if (DB::isError($res)) return false;
 		$num_rows=$res->numRows();
 		$res->free();
 		//-- mutex doesn't exist so create it
@@ -92,6 +93,7 @@ class Mutex {
 			//-- allow the same session to get the mutex more than once
 			$sql = "SELECT 1 FROM {$TBLPREFIX}mutex WHERE mx_name='".$DBCONN->escapeSimple($this->name)."' AND (mx_thread='0' OR mx_thread='".session_id()."' OR mx_time < ".(time()-300).")";
 			$res = dbquery($sql);
+			if (DB::isError($res)) return false;
 			if ($res->numRows() > 0) {
 				$available = true;
 			}
