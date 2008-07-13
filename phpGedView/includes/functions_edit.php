c14cbe65d72675812294c2efa7e26bbe7fb2e22c
NAME <USER@DOMAIN>
2008-07-13 05:12:08 +0000

    fixing record instead of event object
    git-svn-id: svn://svn.webtrees.net/trunk@3561 4201c862-f99b-1943-94d0-25fd05010cbc

--- phpGedView/includes/functions_edit.php
+++ phpGedView/includes/functions_edit.php
@@ -742,7 +742,10 @@ function print_indi_form($nextaction, $famid, $linenum="", $namerec="", $famtag=
 		print $pgv_lang["admin_override"]."</td><td class=\"optionbox wrap\">\n";
 		print "<input type=\"checkbox\" name=\"preserve_last_changed\" />\n";
 		print $pgv_lang["no_update_CHAN"]."<br />\n";
-		if (isset($famrec)) echo format_fact_date(get_sub_record(1, "1 CHAN", $famrec), false, true);
+		if (isset($famrec)) {
+			$event = new Event(get_sub_record(1, "1 CHAN", $famrec));
+			echo format_fact_date($event, false, true);
+		}
 		print "</td></tr>\n";
 	}
 	print "</table>\n";
