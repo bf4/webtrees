c517d2cd185db81c4d4bac2172706770b0346d18
NAME <USER@DOMAIN>
2008-08-14 18:41:44 +0000

    Redirect requests for "Spanish (Latin America)" to "Spanish". Use this until we can fix the program (so we can remove the files altogether).
    git-svn-id: svn://svn.webtrees.net/trunk@3667 4201c862-f99b-1943-94d0-25fd05010cbc

---
+++ phpGedView/languages/countries.es-ar.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * see http://unstats.un.org/unsd/methods/m49/m49alpha.htm
+ * see http://www.foreignword.com/countries/  for a comprehensive list, with translations
+ * see http://susning.nu/Landskod  (list #7) for another list, taken from ISO standards
+ * see http://helpdesk.rootsweb.com/codes for a comprehensive list of Chapman codes.
+ * see http://www.rootsweb.com/~wlsgfhs/ChapmanCodes.htm for another list of Chapman codes
+ *
+ * The list that follows is the list of Chapman country codes, with additions from the
+ * other sources mentioned above.
+ *
+ * These codes do not appear in the two Chapman lists cited:
+ *		ALA		Åland Islands
+ *		COD		Congo (Brazzaville)		This country was known as Zaire
+ *		NFK		Norfolk Island
+ *		PRI		Puerto Rico				Chapman lists this as a state of the USA
+ *		SCG		Serbia and Montenegro	Chapman lists these separately
+ *		TLS		Timor-Leste
+ *		UMI		US Minor Outlying Islands
+ *		VIR		US Virgin Islands		Chapman lists this as a state of the USA
+ *		
+ * These Chapman country codes do not appear in the list following:
+ *		UEL		United Empire Loyalist		This is NOT a country or region, it's
+ *											a group of people
+ *		UK		United Kingdom				This is the only two-letter country code,
+ *											and GBR or one of its components should be
+ *											used instead.
+ *		SLK		Slovakia					This code, listed in the last source cited,
+ *											should be SVK
+ *		SLO		Slovenia					This code, listed in the last source cited,
+ *											should be SVN
+ *		SAM		South America				This code, listed in the last source cited,
+ *											is not precise enough
+ *		TMP		East Timor					Official name is TLS "Timor-Leste"
+ *		HOL		Holland						Official name is NLD "Netherlands"
+ *		ESM		Western Samoa				Official name is WSM "Samoa"
+ *											
+ * @package PhpGedView
+ * @subpackage Languages
+ * @version $Id$
+ */
+
+if (stristr($_SERVER["SCRIPT_NAME"], basename(__FILE__))!==false) {
+	print "You cannot access a language file directly.";
+	exit;
+}
+
+require "languages/countries.es.php";		// Load Spanish file instead
+
+?>
