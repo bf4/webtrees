c4a3e2bb23d399aedbd0054a93f54948a7db1749
NAME <USER@DOMAIN>
2008-09-02 15:58:39 +0000

    [ 2082505 ] [3724] Indiv list diplay error
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@3766 4201c862-f99b-1943-94d0-25fd05010cbc

--- includes/functions_print_lists.php
+++ includes/functions_print_lists.php
@@ -321,7 +321,7 @@ function print_indi_table($datalist, $legend="", $option="") {
 		if ($birth_dates[0]->isOK() && $death_dates[0]->isOK()) {
 			$age = GedcomDate::GetAgeYears($birth_dates[0], $death_dates[0]);
 			$age_jd = $death_dates[0]->MinJD()-$birth_dates[0]->MinJD();
-			echo "<a name=\"".$age_jd."\" title=\"".$pgv_lang["age"].": ".$age."\" class=\"list_item age\">".$age."</a>";
+			echo '<a name="', $age_jd, '" class="list_item age">', $age, '</a>';
 			$deat_by_age[max(0,min($MAX_ALIVE_AGE, $age))] .= $person->getSex();
 		} else {
 			echo '<a name="-1">&nbsp;</a>';
