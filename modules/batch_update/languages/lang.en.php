7fa703152c3418af92cf3c2b235d01025883b050
NAME <USER@DOMAIN>
2008-09-05 21:20:29 +0000

    Add new module for batch updates
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@3789 4201c862-f99b-1943-94d0-25fd05010cbc

---
+++ modules/batch_update/languages/lang.en.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * English Language file for PhpGedView.
+ *
+ * phpGedView: Genealogy Viewer
+ * Copyright (C) 2002 to 2008  PGV Development Team. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @package PhpGedView
+ * @subpackage GoogleMap
+ * @version $Id$
+ */
+if (stristr($_SERVER["SCRIPT_NAME"], basename(__FILE__))!==false) {
+	print "You cannot access a language file directly.";
+	exit;
+}
+
+$pgv_lang["batch_update"]="Batch Update";
+$pgv_lang["bu_update_chan"]="Update the CHAN record";
+$pgv_lang["bu_nothing"]="Nothing found.";
+$pgv_lang["bu__desc"]="Select a batch update from this list.";
+$pgv_lang["bu_button_update"]="Update";
+$pgv_lang["bu_button_update_all"]="Update all";
+$pgv_lang["bu_button_delete"]="Delete";
+$pgv_lang["bu_button_delete_all"]="Delete all";
+
+$pgv_lang["bu_search_replace"]="Search and Replace";
+$pgv_lang["bu_search_replace_desc"]="Search and/or replace data in your GEDCOM using simple searches or advanced pattern matching.";
+$pgv_lang["bu_search"]="Search text/pattern";
+$pgv_lang["bu_replace"]="Replacement text";
+$pgv_lang["bu_method"]="Search method";
+$pgv_lang["bu_exact"]="Exact text";
+$pgv_lang["bu_exact_desc"]="Match the exact text, even if it occurs in the middle of a word.";
+$pgv_lang["bu_words"]="Whole words only";
+$pgv_lang["bu_words_desc"]="Match the exact text, unless it occurs in the middle of a word";
+$pgv_lang["bu_wildcards"]="Wildcards";
+$pgv_lang["bu_wildcards_desc"]="Use a &laquo;?&raquo; to match a single character, use &laquo;*&raquo; to match zero or more characters.";
+$pgv_lang["bu_regex"]="Regular expression";
+$pgv_lang["bu_regex_desc"]="Regular expressions are an advanced pattern matching technique.  See <a href=\"http://php.net/manual/en/regexp.reference.php\" target=\"_new\">php.net/manual/en/regexp.reference.php</a> for futher details.";
+$pgv_lang["bu_regex_bad"]="The regex appears to contain an error.  It can't be used.";
+$pgv_lang["bu_case"]="Case insensitive";
+$pgv_lang["bu_case_desc"]="Tick this box to match both upper and lower case letters.";
+
+$pgv_lang["bu_birth_death"]="Add missing birth and death records";
+$pgv_lang["bu_birth_death_desc"]="You can improve the performance of PGV by ensuring that all individuals have a &laquo;start of life&raquo; event and (where appropriate) an &laquo;end of life&raquo; event.";
+
+$pgv_lang["bu_married_names"]="Add missing married names";
+$pgv_lang["bu_married_names_desc"]="You can make it easier to search for married women by recording their married name.<br/>However not all women take their husband's surname, so beware of introducing incorrect data into your GEDCOM.";
+$pgv_lang["bu_surname_option"]="Surname Option";
+$pgv_lang["bu_surname_replace"]="Wife's surname replaced by husband's surname";
+$pgv_lang["bu_surname_add"]="Wife's maiden surname becomes new given name";
+?>
