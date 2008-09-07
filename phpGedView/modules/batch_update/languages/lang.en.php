891bc58244dfe3f7fe211ffe4eb1d8998e06de17
NAME <USER@DOMAIN>
2008-09-07 10:15:22 +0000

    Fix typos in comments
    git-svn-id: svn://svn.webtrees.net/trunk@3799 4201c862-f99b-1943-94d0-25fd05010cbc

--- phpGedView/modules/batch_update/languages/lang.en.php
+++ phpGedView/modules/batch_update/languages/lang.en.php
@@ -20,7 +20,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  * @package PhpGedView
- * @subpackage GoogleMap
+ * @subpackage BatchUpdate
  * @version $Id$
  */
 if (stristr($_SERVER["SCRIPT_NAME"], basename(__FILE__))!==false) {
