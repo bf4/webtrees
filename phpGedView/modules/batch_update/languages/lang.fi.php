56ebea339d41278c018cf8abb6ba6666deb8f36a
NAME <USER@DOMAIN>
2008-09-11 07:49:40 +0000

    git-svn-id: svn://svn.webtrees.net/trunk@3837 4201c862-f99b-1943-94d0-25fd05010cbc

--- phpGedView/modules/batch_update/languages/lang.fi.php
+++ phpGedView/modules/batch_update/languages/lang.fi.php
@@ -22,7 +22,6 @@
  * @package PhpGedView
  * @subpackage BatchUpdate
  * @translator Matti Valve 
-@version $Id
  */
 if (stristr($_SERVER["SCRIPT_NAME"], basename(__FILE__))!==false) {
 	print "You cannot access a language file directly.";
