615eb10936413cefe88aaf81906806a5d7540696
NAME <USER@DOMAIN>
2008-10-16 21:05:08 +0000

    Update: chinese language files for lightbox and date format (mr_bobwang)
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@4100 4201c862-f99b-1943-94d0-25fd05010cbc

---
+++ modules/lightbox/languages/lang.zh.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * Chinese language file for Lightbox Album module
+ *
+ * Display media Items using Lightbox
+ *
+ * phpGedView: Genealogy Viewer
+ * Copyright (C) 2002 to 2008  PGV Development Team. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @package PhpGedView
+ * @subpackage Module
+ * @version $Id$
+ * @translator: mr_bobwang
+ */
+
+if (!defined('PGV_PHPGEDVIEW')) {
+	header('HTTP/1.0 403 Forbidden');
+	exit;
+}
+
+// Added in VERSION 4.1.4 
+
+$pgv_lang["lb_details"]			= "详细资料";
+$pgv_lang["lb_detail_info"]		= "查看媒体文件的详细资料 ...  和其他媒体的选项 - 媒体播放页面";
+$pgv_lang["lb_pause_ss"]		= "停止自动播放";
+$pgv_lang["lb_start_ss"]		= "开始自动播放";
+$pgv_lang["lb_music"]			= "音乐开关";
+$pgv_lang["lb_zoom_off"]		= "关闭放大功能";
+$pgv_lang["lb_zoom_on"]			= "放大功能被启动 ... 用鼠标轮或 i 和 o 键来放大和缩小";
+$pgv_lang["lb_close_win"]		= "关闭窗口";
+
+
+
+
+// VERSION 4.1.3 
+
+//Config Parameters -------------------------------------
+$pgv_lang["configure_lightbox"]			= "图片集播放器设置";
+$pgv_lang["mediatab"]       			= "个人页面 - 媒体";
+$pgv_lang["lb_appearance"]				= "外表";
+$pgv_lang["lb_linkAppearance"]			= "连接外表";
+$pgv_lang["lb_MP3Only"]					= "(只能放mp3)";
+$pgv_lang["lb_admin_error"]				= "本页只是个管理员所用";
+$pgv_lang["lb_toAlbumPage"]				= "回到图片集页面";
+
+$pgv_lang["lb_icon"]					= "点击图";
+$pgv_lang["lb_text"]					= "文字";
+$pgv_lang["lb_both"]					= "都有";
+$pgv_lang["lb_none"]					= "没有";
+
+$pgv_lang["lb_al_head_links"]			= "个人页面 - 图片集图条";
+$pgv_lang["lb_al_thumb_links"]			= "个人页面 - 图片集指图";
+$pgv_lang["lb_ml_thumb_links"]			= "媒体页面 - 指图";
+$pgv_lang["lb_music_file"]				= "图片播放的伴随声音文件";
+$pgv_lang["lb_musicFileAdvice"]			= "声音文件的地址（如果没有文件那就空白）";
+$pgv_lang["lb_ss_speed"]				= "图片播放速度";
+$pgv_lang["lb_ss_SpeedAdvice"]			= "图片播放计时（秒）";
+
+$pgv_lang["lb_transition"]				= "图片变化速度";
+$pgv_lang["lb_normal"]					= "正常";
+$pgv_lang["lb_double"]					= "两倍";
+$pgv_lang["lb_warp"]					= "弹性图片连接";
+$pgv_lang["lb_url_dimensions"]			= "URL窗口的尺寸";
+$pgv_lang["lb_url_dimensionsAdvice"]	= "URL窗口的宽度和高度";
+$pgv_lang["lb_width"]					= "宽度";
+$pgv_lang["lb_height"]					= "高度";
+									
+
+// ---------------------------------------------------------------------
+
+
+$pgv_lang["lb_help"] 		 = "图片集帮助";
+$pgv_lang["lightbox"]		 = "图片集";
+$pgv_lang["showmenu"] 		 = "显示菜单:";
+
+$pgv_lang["TYPE__other"] 	 = "其他";
+
+$pgv_lang["TYPE__footnotes"] = "脚注";
+
+$pgv_lang["census_text"]  	 = "\"These census images have been obtained from \"The National Archives\", the custodian of the original records, ";
+$pgv_lang["census_text"] 	.= "and appear here with their approval on the condition that no commercial use is made of them without permission." . "\n" ;
+$pgv_lang["census_text"] 	.= "Requests for commercial publication of these or other census images appearing on this website should be directed to: ";
+$pgv_lang["census_text"] 	.= "Image Library, The National Archives, Kew, Surrey, TW9 4DU, United Kingdom.\"" . "\n" ;
+
+$pgv_lang["lb_edit_details"] 	= "编辑详细资料";
+$pgv_lang["lb_view_details"] 	= "看详细资料";
+$pgv_lang["lb_edit_media"] 		= "编辑媒体的详细资料";
+$pgv_lang["lb_delete_media"] 	= "删除这个媒体文件和这个人的连接，文件还在系统里";
+$pgv_lang["lb_view_media"] 		= "View this Media Item's Details \nPlus other Media Options - MediaViewer page";
+$pgv_lang["lb_add_media"] 		= "添加新的媒体文件";
+$pgv_lang["lb_add_media_full"] 	= "给这个人添加新的媒体文件";
+$pgv_lang["lb_link_media"] 		= "连接一个现有的媒体文件";
+$pgv_lang["lb_link_media_full"] = "连接这个人到一个现有的媒体文件";
+
+$pgv_lang["lb_slide_show"] 		= "图片自动播放";
+$pgv_lang["turn_edit_ON"] 		= "打开编辑功能";
+$pgv_lang["turn_edit_OFF"] 		= "关闭编辑功能";
+
+$pgv_lang["lb_source_avail"] 	= "资料源信息存在 - 点击这。";
+
+$pgv_lang["lb_private"] 		= "图片连接到 <br /> 一个有隐私功能所隐蔽的个人";
+$pgv_lang["lb_view_source_tip"] = "看来源资料 : ";
+$pgv_lang["lb_view_details_tip"] = "看媒体资料 : ";
+
+
+
+?>
