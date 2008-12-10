19dfea17f378913fdcab4f80713ba61e23670256
NAME <USER@DOMAIN>
2008-12-10 12:51:58 +0000

    Add access keys to "simply..." themes
    git-svn-id: svn://svn.webtrees.net/trunk@4491 4201c862-f99b-1943-94d0-25fd05010cbc

--- phpGedView/themes/simplygreen/toplinks.php
+++ phpGedView/themes/simplygreen/toplinks.php
@@ -119,6 +119,7 @@ $menubar = new MenuBar();
 	</table>
 	<br />
 </div>
+<?php require './includes/accesskeyHeaders.php'; ?>
 </div>
 <!-- close div for div id="header" -->
 <?php print "<div id=\"content\" style=\"direction: $TEXT_DIRECTION;\">\n"; ?>
