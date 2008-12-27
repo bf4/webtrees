786399effcba6841672a9ef83987150c94e3c859
NAME <USER@DOMAIN>
2008-12-27 23:27:22 +0000

    Merging change r4553,4564,4598 from the trunk; [ 2459204 ] unable to update with GDBI (fisharebest) [ 2432314 ] client.php 4498 inconsistency (fisharebest) [ 2432443 ] error in item called by client.php
    git-svn-id: svn://svn.webtrees.net/branches/phpGedView-4.1@4599 4201c862-f99b-1943-94d0-25fd05010cbc

--- client.php
+++ client.php
@@ -189,7 +189,7 @@ case 'getvar':
 case 'update':
 	$xref=safe_REQUEST($_REQUEST,'xref', PGV_REGEX_XREF);
 	if ($xref) {
-		$gedrec=safe_REQUEST($_REQUEST,'gedrec', '.*'); // raw data may contain any characters
+		$gedrec=safe_REQUEST($_REQUEST,'gedrec', PGV_REGEX_UNSAFE); // raw data may contain any characters
 		if ($gedrec) {
 			if (empty($_SESSION['readonly']) && PGV_USER_CAN_EDIT && displayDetailsById($xref)) {
 				$gedrec = preg_replace(array("/\\\\+r/","/\\\\+n/"), array("\r","\n"), $gedrec);
@@ -361,36 +361,6 @@ case 'getxref':
 		$xref=get_prev_xref($xref, $GED_ID);
 		addDebugLog($action." type=$type position=$position SUCCESS\n$xref");
 		print "SUCCESS\n$xref\n";
-		break;
-	case 'all':
-		switch($type) {
-			case "INDI":
-				$sql="SELECT i_id FROM {$TBLPREFIX}individuals WHERE i_file={$GED_ID} ORDER BY i_id";
-				break;
-			case "FAM":
-				$sql="SELECT f_id FROM {$TBLPREFIX}families WHERE f_file={$GED_ID} ORDER BY f_id";
-				break;
-			case "SOUR":
-				$sql="SELECT s_id FROM {$TBLPREFIX}sources WHERE s_file={$GED_ID} ORDER BY s_id";
-				break;
-			case "OBJE":
-				$sql="SELECT m_media FROM {$TBLPREFIX}media WHERE m_gedfile={$GED_ID} ORDER BY m_media";
-				break;
-			case "OTHER":
-				$sql="SELECT o_id FROM {$TBLPREFIX}other WHERE o_file={$GED_ID} AND o_type NOT IN ('REPO', 'NOTE') ORDER BY o_id";
-				break;
-			default:
-				$sql="SELECT o_id FROM {$TBLPREFIX}other WHERE o_file={$GED_ID} AND o_type='{$type}' ORDER BY o_id";
-				break;
-		}
-		$res = dbquery($sql);
-		print "SUCCESS\n";
-		while ($row = $res->fetchRow()) {
-			print "$row[0]\n";
-		}
-		$res->free();
-		addDebugLog($action." type=$type position=$position ".$msg_out);
-		print $msg_out;
 		break;
 	case 'new':
 		if (empty($_SESSION['readonly']) && PGV_USER_CAN_EDIT) {
