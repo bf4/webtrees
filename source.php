2b403986068a50c0c13dfde0cde0ee855a6e645a
NAME <USER@DOMAIN>
2009-01-01 14:18:10 +0000

    Tidy up code
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@4629 4201c862-f99b-1943-94d0-25fd05010cbc

--- source.php
+++ source.php
@@ -1,152 +1,127 @@
 <?php
 /**
- * Displays the details about a source record.  Also shows how many people and families
- * reference this source.
- *
- * phpGedView: Genealogy Viewer
- * Copyright (C) 2002 to 2008 PGV Development Team.  All rights reserved.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- * @package PhpGedView
- * @subpackage Charts
- * @version $Id$
- */
+* Displays the details about a source record.  Also shows how many people and families
+* reference this source.
+*
+* phpGedView: Genealogy Viewer
+* Copyright (C) 2002 to 2008 PGV Development Team.  All rights reserved.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*
+* @package PhpGedView
+* @subpackage Charts
+* @version $Id$
+*/
 
 require './config.php';
+require './includes/controllers/source_ctrl.php';
+require './includes/functions/functions_print_lists.php';
 
-require_once 'includes/functions/functions_print_lists.php';
-require_once 'includes/controllers/source_ctrl.php';
-
-$controller = new SourceController();
+$controller=new SourceController();
 $controller->init();
 
-global $linkToID;
+// Tell addmedia.php what to link to
+$linkToID = $controller->sid;
 
 print_header($controller->getPageTitle());
-$linkToID = $controller->sid;	// -- Tell addmedia.php what to link to
 
-// LBox =============================================================================
-// Get Javascript variables from lb_config.php ---------------------------
- if (file_exists("modules/lightbox/album.php")) {
-	include('modules/lightbox/lb_defaultconfig.php');
-	if (file_exists('modules/lightbox/lb_config.php')) include('modules/lightbox/lb_config.php');
-	include('modules/lightbox/functions/lb_call_js.php');
+// LightBox
+if (is_dir('./modules/lightbox')) {
+	include './modules/lightbox/lb_defaultconfig.php';
+	if (file_exists('modules/lightbox/lb_config.php')) {
+		include './modules/lightbox/lb_config.php';
+	}
+	include './modules/lightbox/functions/lb_call_js.php';
+	loadLangFile('lightbox:lang');
 }
-// LBox  ============================================================================
 
-loadLangFile("lightbox:lang");
+if ($controller->source->isMarkedDeleted()) {
+	echo '<span class="error">', $pgv_lang['record_marked_deleted'], '</span>';
+}
 
-?>
-<?php if ($controller->source->isMarkedDeleted()) print "<span class=\"error\">".$pgv_lang["record_marked_deleted"]."</span>"; ?>
-<script language="JavaScript" type="text/javascript">
-<!--
-	function show_gedcom_record() {
-		var recwin = window.open("gedrecord.php?pid=<?php print $controller->sid ?>", "_blank", "top=0,left=0,width=600,height=400,scrollbars=1,scrollable=1,resizable=1");
-	}
-	function showchanges() {
-		window.location = 'source.php?sid=<?php print $controller->sid; ?>&show_changes=yes';
-	}
-//-->
-</script>
-<table class="list_table">
-	<tr>
-		<td>
-<?php
-	if ($controller->accept_success) print "<b>".$pgv_lang["accept_successful"]."</b><br />";
-?>
-			<span class="name_head"><?php print PrintReady($controller->source->getFullName()); if ($SHOW_ID_NUMBERS) print " " . getLRM() . "(".$controller->sid.")" . getLRM(); ?></span><br />
-		</td>
-		<td valign="top" class="noprint">
-		<?php if (!$controller->isPrintPreview()) {
-			$editmenu = $controller->getEditMenu();
-			$othermenu = $controller->getOtherMenu();
-			if ($editmenu!==false || $othermenu!==false) {
-		?>
-			<table class="sublinks_table" cellspacing="4" cellpadding="0">
-				<tr>
-					<td class="list_label <?php print $TEXT_DIRECTION?>" colspan="2"><?php print $pgv_lang['source_menu']?></td>
-				</tr>
-				<tr>
-					<?php if ($editmenu!==false) { ?>
-					<td class="sublinks_cell <?php print $TEXT_DIRECTION?>">
-					<?php $editmenu->printMenu(); ?>
-					</td>
-					<?php
-					}
-					if ($othermenu!==false) {
-					?>
+echo PGV_JS_START;
+echo 'function show_gedcom_record() {';
+echo ' var recwin=window.open("gedrecord.php?pid=', $controller->sid, '", "_blank", "top=0,left=0,width=600,height=400,scrollbars=1,scrollable=1,resizable=1");';
+echo '}';
+echo 'function showchanges() {';
+echo ' window.location="source.php?sid=', $controller->sid, '&show_changes=yes"';
+echo '}';
+echo PGV_JS_END;
 
-					<td class="sublinks_cell <?php print $TEXT_DIRECTION?>">
-					<?php $othermenu->printMenu(); ?>
-					</td>
-					<?php } ?>
-				</tr>
-			</table>
-			<?php }
+echo '<table class="list_table"><tr><td>';
+if ($controller->accept_success) {
+	echo '<b>', $pgv_lang['accept_successful'], '</b><br />';
+}
+echo '<span class="name_head">', PrintReady($controller->source->getFullName());
+if ($SHOW_ID_NUMBERS) {
+	echo ' ', getLRM(), '(', $controller->sid, ')', getLRM(); 
+}
+echo '</span><br /></td><td valign="top" class="noprint">';
+if (!$controller->isPrintPreview()) {
+	$editmenu=$controller->getEditMenu();
+	$othermenu=$controller->getOtherMenu();
+	if ($editmenu || $othermenu) {
+		echo '<table class="sublinks_table" cellspacing="4" cellpadding="0">';
+		echo '<tr><td class="list_label ', $TEXT_DIRECTION, '" colspan="2">', $pgv_lang['source_menu'], '</td></tr>';
+		echo '<tr>';
+		if ($editmenu) {
+			echo '<td class="sublinks_cell ', $TEXT_DIRECTION, '">', $editmenu->printMenu(), '</td>';
 		}
-		?>
-		</td>
-	</tr>
-	<tr>
-		<td colspan="2">
-			<table class="facts_table">
-<?php
-$sourcefacts = $controller->source->getFacts();
-foreach($sourcefacts as $indexval => $fact) {
-	if (!empty($fact)) {
-		if ($fact->getTag()=="NOTE") {
+		if ($othermenu) {
+			echo '<td class="sublinks_cell ', $TEXT_DIRECTION, '">', $othermenu->printMenu(), '</td>';
+		}
+		echo '</tr></table>';
+	}
+}
+echo '</td></tr><tr><td colspan="2"><table class="facts_table">';
+
+$sourcefacts=$controller->source->getFacts();
+foreach ($sourcefacts as $fact) {
+	if ($fact) {
+		if ($fact->getTag()=='NOTE') {
 			print_main_notes($fact->getGedcomRecord(), 1, $controller->sid, $fact->getLineNumber());
-		}
-		else {
+		} else {
 			print_fact($fact);
 		}
 	}
 }
+
 // Print media
 print_main_media($controller->sid);
 
-//-- new fact link
-if ((!$controller->isPrintPreview())&&($controller->userCanEdit())) {
-	print_add_new_fact($controller->sid, $sourcefacts, "SOUR");
-		// -- new media
-	print "<tr><td class=\"descriptionbox\">";
-	print_help_link("add_media_help", "qm", "add_media_lbl");
-	print $pgv_lang["add_media_lbl"] . "</td>";
-	print "<td class=\"optionbox\">";
-	print "<a href=\"javascript: ".$pgv_lang["add_media_lbl"]."\" onclick=\"window.open('addmedia.php?action=showmediaform&linktoid={$controller->sid}', '_blank', 'top=50,left=50,width=600,height=500,resizable=1,scrollbars=1'); return false;\">".$pgv_lang["add_media"]."</a>";
-	print "<br />\n";
-	print '<a href="javascript:;" onclick="window.open(\'inverselink.php?linktoid='.$controller->sid.'&linkto=source\', \'_blank\', \'top=50,left=50,width=600,height=500,resizable=1,scrollbars=1\'); return false;">'.$pgv_lang["link_to_existing_media"].'</a>';
-	print "</td></tr>\n";
-
+// new fact link
+if (!$controller->isPrintPreview() && $controller->userCanEdit()) {
+	print_add_new_fact($controller->sid, $sourcefacts, 'SOUR');
+	// new media
+	echo '<tr><td class="descriptionbox">';
+	print_help_link('add_media_help', 'qm', 'add_media_lbl');
+	echo $pgv_lang['add_media_lbl'] . '</td>';
+	echo '<td class="optionbox">';
+	echo '<a href="javascript: ', $pgv_lang['add_media_lbl'], '" onclick="window.open(\'addmedia.php?action=showmediaform&linktoid=', $controller->sid, '\', \'_blank\', \'top=50,left=50,width=600,height=500,resizable=1,scrollbars=1\'); return false;">', $pgv_lang['add_media'], '</a>';
+	echo '<br />';
+	echo '<a href="javascript:;" onclick="window.open(\'inverselink.php?linktoid='.$controller->sid.'&linkto=source\', \'_blank\', \'top=50,left=50,width=600,height=500,resizable=1,scrollbars=1\'); return false;">'.$pgv_lang['link_to_existing_media'].'</a>';
+	echo '</td></tr>';
 }
-?>
-		</table>
-		<br /><br />
-		</td></tr>
-		<tr class="center"><td colspan="2">
-<?php
-//Print the tasks table
-if (file_exists("modules/research_assistant/research_assistant.php") && ($SHOW_RESEARCH_ASSISTANT>=PGV_USER_ACCESS_LEVEL)) {
- include_once('modules/research_assistant/research_assistant.php');
- $mod = new ra_functions();
+echo '</table><br /><br /></td></tr><tr class="center"><td colspan="2">';
+
+// Print the tasks table
+if (file_exists('./modules/research_assistant/research_assistant.php') && $SHOW_RESEARCH_ASSISTANT>=PGV_USER_ACCESS_LEVEL) {
+	include_once './modules/research_assistant/research_assistant.php';
+	$mod=new ra_functions();
 	$mod->Init();
- $out = $mod->getSourceTasks($controller->sid);
- print $out;
-	echo "</td></tr>";
-	echo "<tr class=\"center\"><td colspan=\"2\">";
+	echo $mod->getSourceTasks($controller->sid), '</td></tr><tr class="center"><td colspan="2">';
 }
 
 // Individuals linked to this source
@@ -159,8 +134,7 @@ if ($controller->source->countLinkedFamilies()) {
 	print_fam_table($controller->source->fetchLinkedFamilies(), $controller->source->getFullName());
 }
 
+echo '</td></tr></table>';
+
+print_footer();
 ?>
-	</td>
-</tr>
-</table>
-<?php print_footer(); ?>
