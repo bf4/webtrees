551bdbedb352215586b560b331160c3752c7a218
NAME <USER@DOMAIN>
2009-01-05 23:28:27 +0000

    Consistent use of newlines
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@4682 4201c862-f99b-1943-94d0-25fd05010cbc

--- edit_merge.php
+++ edit_merge.php
@@ -1,30 +1,30 @@
 <?php
 /**
- * Merge Two Gedcom Records
- *
- * This page will allow you to merge 2 gedcom records
- *
- * phpGedView: Genealogy Viewer
- * Copyright (C) 2002 to 2008  PGV Development Team.  All rights reserved.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- * @package PhpGedView
- * @subpackage Edit
- * @version $Id$
- */
+* Merge Two Gedcom Records
+*
+* This page will allow you to merge 2 gedcom records
+*
+* phpGedView: Genealogy Viewer
+* Copyright (C) 2002 to 2008  PGV Development Team.  All rights reserved.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*
+* @package PhpGedView
+* @subpackage Edit
+* @version $Id$
+*/
 
 require './config.php';
 
@@ -225,17 +225,17 @@ if ($action!="choose") {
 							replace_gedrec($gid, $newrec);
 						}
 					}
-					$newgedrec = "0 @$gid1@ $type1\r\n";
+					$newgedrec = "0 @$gid1@ $type1\n";
 					for($i=0; ($i<count($facts1) || $i<count($facts2)); $i++) {
 						if (isset($facts1[$i])) {
 							if (in_array($i, $keep1)) {
-								$newgedrec .= $facts1[$i]["subrec"]."\r\n";
+								$newgedrec .= $facts1[$i]["subrec"]."\n";
 								print $pgv_lang["adding"]." ".$facts1[$i]["fact"]." ".$pgv_lang["from"]." $gid1<br />\n";
 							}
 						}
 						if (isset($facts2[$i])) {
 							if (in_array($i, $keep2)) {
-								$newgedrec .= $facts2[$i]["subrec"]."\r\n";
+								$newgedrec .= $facts2[$i]["subrec"]."\n";
 								print $pgv_lang["adding"]." ".$facts2[$i]["fact"]." ".$pgv_lang["from"]." $gid2<br />\n";
 							}
 						}
