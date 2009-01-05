3aa96ca977d5bce4fa423cf77ce196c353c4c18e
NAME <USER@DOMAIN>
2009-01-05 00:07:00 +0000

    require -> require_once
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@4656 4201c862-f99b-1943-94d0-25fd05010cbc

--- export_gedcom.php
+++ export_gedcom.php
@@ -25,7 +25,7 @@
  */
 
 require './config.php';
-require './includes/functions/functions_export.php';
+require_once './includes/functions/functions_export.php';
 
 // Which gedcoms do we have permission to export?
 $gedcoms=array();
