62c07822f5bb426cf7e55dc9025048c27f572fcb
NAME <USER@DOMAIN>
2009-01-17 20:36:55 +0000

    Fix: improve check for presence of modules
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@4835 4201c862-f99b-1943-94d0-25fd05010cbc

--- ancestry.php
+++ ancestry.php
@@ -39,7 +39,7 @@ print_header($controller->name . " " . $pgv_lang['ancestry_chart']);
 require 'js/autocomplete.js.htm';
 
 // LightBox
-if ($MULTI_MEDIA && is_dir('./modules/lightbox')) {
+if ($MULTI_MEDIA && file_exists('./modules/lightbox.php')) {
 	include './modules/lightbox/lb_defaultconfig.php';
 	if (file_exists('modules/lightbox/lb_config.php')) {
 		include './modules/lightbox/lb_config.php';
