011db1627f8d40138dba538ddce5f5962e334ac3
NAME <USER@DOMAIN>
2009-02-13 01:47:12 +0000

    Add support for Serbian (Latin alphabet)
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@5024 4201c862-f99b-1943-94d0-25fd05010cbc

---
+++ languages/editor.sr.php
@@ -0,0 +1,237 @@
+<?php
+/**
+ * Serbian Language file for PhpGedView.
+ *
+ * PhpGedView: Genealogy Viewer
+ * Copyright (C) 2009  PGV Development Team.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @package PhpGedView
+ * @translator Vojin Damjanac
+ * @version $Id$
+ */
+
+if (!defined('PGV_PHPGEDVIEW')) {
+	header('HTTP/1.0 403 Forbidden');
+	exit;
+}
+
+$pgv_lang["add_marriage"]			= "Dodaj detalje sklapanja braka";
+$pgv_lang["edit_concurrency_change"] = "Zadnju promenu upisa izvršio <i>#CHANGEUSER#</i> u #CHANGEDATE#";
+##$pgv_lang["edit_concurrency_msg2"]	= "The record with id #PID# was changed by another user since you last accessed it.";
+##$pgv_lang["edit_concurrency_msg1"]	= "An error occurred while creating the Edit form.  Another user may have changed this record since you previously viewed it.";
+##$pgv_lang["edit_concurrency_reload"]	= "Please reload the previous page to make sure you are working with the most recent record.";
+##$pgv_lang["admin_override"]			= "Admin Option";
+$pgv_lang["no_update_CHAN"]			= "CHAN (Poslednja promena) upis ne treba ažurirati";
+##$pgv_lang["select_events"]			= "Select Events";
+##$pgv_lang["source_events"]			= "Associate events with this source";
+##$pgv_lang["advanced_name_fields"]	= "Additional names (nickname, married name, etc.)";
+$pgv_lang["accept_changes"] 		= "Prihvati / Odbaci promene";
+$pgv_lang["replace"]				= "Zameni upis";
+##$pgv_lang["append"] 				= "Append record";
+##$pgv_lang["review_changes"] 		= "Review GEDCOM Changes";
+##$pgv_lang["remove_object"]			= "Remove object";
+##$pgv_lang["remove_links"]			= "Remove links";
+##$pgv_lang["media_not_deleted"]		= "Media directory not removed.";
+##$pgv_lang["thumbs_not_deleted"]		= "Thumbnail directory not removed.";
+##$pgv_lang["thumbs_deleted"]			= "Thumbnail directory successfully removed.";
+##$pgv_lang["show_thumbnail"]			= "Show thumbnails";
+##$pgv_lang["link_media"]				= "Link Media";
+##$pgv_lang["to_person"]				= "To Person";
+##$pgv_lang["to_family"]				= "To Family";
+##$pgv_lang["to_source"]				= "To Source";
+$pgv_lang["edit_fam"]				= "Edituj porodicu";
+##$pgv_lang["edit_repo"]				= "Edit Repository";
+##$pgv_lang["copy"]					= "Copy";
+##$pgv_lang["cut"]					= "Cut";
+##$pgv_lang["sort_by_birth"]			= "Sort by birth dates";
+$pgv_lang["reorder_children"]		= "Promeni redosled dece";
+##$pgv_lang["reorder_media"]					= "Re-order media";
+##$pgv_lang["reorder_media_title"]			= "Drag-and-drop thumbnails to re-order media items";
+##$pgv_lang["reorder_media_window"]			= "Re-order media (window)";
+##$pgv_lang["reorder_media_window_title"]		= "Click a row, then drag-and-drop to re-order media ";
+##$pgv_lang["reorder_media_save"]				= "Saves the sorted media to the database";
+##$pgv_lang["reorder_media_reset"]			= "Reset to the original order";
+##$pgv_lang["reorder_media_cancel"]			= "Quit and return";
+##$pgv_lang["add_from_clipboard"]		= "Add from Clipboard: ";
+##$pgv_lang["record_copied"]			= "Record copied to clipboard";
+##$pgv_lang["add_unlinked_person"]	= "Add an unlinked person";
+##$pgv_lang["add_unlinked_source"]	= "Add an unlinked source";
+##$pgv_lang["server_file"]				= "File name on server";
+##$pgv_lang["server_file_advice"]			= "Do not change to keep original file name.";
+##$pgv_lang["server_file_advice2"]		= "You may enter a URL, beginning with &laquo;http://&raquo;.";
+##$pgv_lang["server_folder_advice"]		= "You can enter up to #GLOBALS[MEDIA_DIRECTORY_LEVELS]# folder names to follow the default &laquo;#GLOBALS[MEDIA_DIRECTORY]#&raquo;.<br />Do not enter the &laquo;#GLOBALS[MEDIA_DIRECTORY]#&raquo; part of the destination folder name.";
+##$pgv_lang["server_folder_advice2"]		= "This entry is ignored if you have entered a URL into the file name field.";
+##$pgv_lang["add_linkid_advice"]			= "Enter or search for the ID of the person, family, or source to which this media item should be linked.";
+##$pgv_lang["use_browse_advice"]			= "Use the &laquo;Browse&raquo; button to search your local computer for the desired file.";
+##$pgv_lang["add_media_other_folder"]		= "Other folder... please type in";
+##$pgv_lang["add_media_file"]				= "Existing Media file on server";
+##$pgv_lang["main_media_ok1"]				= "Main media file <b>#GLOBALS[oldMediaName]#</b> successfully renamed to <b>#GLOBALS[newMediaName]#</b>.";
+##$pgv_lang["main_media_ok2"]				= "Main media file <b>#GLOBALS[oldMediaName]#</b> successfully moved from <b>#GLOBALS[oldMediaFolder]#</b> to <b>#GLOBALS[newMediaFolder]#</b>.";
+##$pgv_lang["main_media_ok3"]				= "Main media file successfully moved and renamed from <b>#GLOBALS[oldMediaFolder]##GLOBALS[oldMediaName]#</b> to <b>#GLOBALS[newMediaFolder]##GLOBALS[newMediaName]#</b>.";
+##$pgv_lang["main_media_fail0"]			= "Main media file <b>#GLOBALS[oldMediaFolder]##GLOBALS[oldMediaName]#</b> does not exist.";
+##$pgv_lang["main_media_fail1"]			= "Main media file <b>#GLOBALS[oldMediaName]#</b> could not be renamed to <b>#GLOBALS[newMediaName]#</b>.";
+##$pgv_lang["main_media_fail2"]			= "Main media file <b>#GLOBALS[oldMediaName]#</b> could not be moved from <b>#GLOBALS[oldMediaFolder]#</b> to <b>#GLOBALS[newMediaFolder]#</b>.";
+##$pgv_lang["main_media_fail3"]			= "Main media file could not be moved and renamed from <b>#GLOBALS[oldMediaFolder]##GLOBALS[oldMediaName]#</b> to <b>#GLOBALS[newMediaFolder]##GLOBALS[newMediaName]#</b>.";
+##$pgv_lang["resn_disabled"]				= "Note: You must enable the 'Use GEDCOM (RESN) Privacy restriction' feature for this setting to take effect.";
+##$pgv_lang["thumb_media_ok1"]			= "Thumbnail file <b>#GLOBALS[oldMediaName]#</b> successfully renamed to <b>#GLOBALS[newMediaName]#</b>.";
+##$pgv_lang["thumb_media_ok2"]			= "Thumbnail file <b>#GLOBALS[oldMediaName]#</b> successfully moved from <b>#GLOBALS[oldThumbFolder]#</b> to <b>#GLOBALS[newThumbFolder]#</b>.";
+##$pgv_lang["thumb_media_ok3"]			= "Thumbnail file successfully moved and renamed from <b>#GLOBALS[oldThumbFolder]##GLOBALS[oldMediaName]#</b> to <b>#GLOBALS[newThumbFolder]##GLOBALS[newMediaName]#</b>.";
+##$pgv_lang["thumb_media_fail0"]			= "Thumbnail file <b>#GLOBALS[oldThumbFolder]##GLOBALS[oldMediaName]#</b> does not exist.";
+##$pgv_lang["thumb_media_fail1"]			= "Thumbnail file <b>#GLOBALS[oldMediaName]#</b> could not be renamed to <b>#GLOBALS[newMediaName]#</b>.";
+##$pgv_lang["thumb_media_fail2"]			= "Thumbnail file <b>#GLOBALS[oldMediaName]#</b> could not be moved from <b>#GLOBALS[oldThumbFolder]#</b> to <b>#GLOBALS[newThumbFolder]#</b>.";
+##$pgv_lang["thumb_media_fail3"]			= "Thumbnail file could not be moved and renamed from <b>#GLOBALS[oldThumbFolder]##GLOBALS[oldMediaName]#</b> to <b>#GLOBALS[newThumbFolder]##GLOBALS[newMediaName]#</b>.";
+##$pgv_lang["add_asso"]				= "Add a new Associate";
+$pgv_lang["edit_sex"]				= "Edituj pol";
+##$pgv_lang["add_obje"]				= "Add a new Multimedia object";
+$pgv_lang["add_name"]				= "Dodaj novo ime";
+##$pgv_lang["edit_raw"]				= "Edit raw GEDCOM record";
+##$pgv_lang["label_add_remote_link"]  = "Add Link";
+##$pgv_lang["label_gedcom_id"]        = "Database ID";
+##$pgv_lang["label_local_id"]         = "Person ID";
+##$pgv_lang["accept"] 				= "Accept";
+$pgv_lang["accept_all"] 			= "Prihvati sve izmene";
+##$pgv_lang["accept_gedcom"]			= "Decide for each change to either accept or reject it.<br /><br />To accept all changes at once, click <b>\"Accept all changes\"</b> in the box below.<br />To get more information about a change,<br />click <b>\"View change diff\"</b> to see the differences,<br />or click <b>\"View GEDCOM record\"</b> to see the new data in GEDCOM format.";
+$pgv_lang["accept_successful"]		= "Promene su uspešno upisane u bazu podataka";
+$pgv_lang["add_child"]				= "Dodaj dete";
+$pgv_lang["add_child_to_family"]	= "Dodaj dete ovoj porodici";
+$pgv_lang["add_fact"]				= "Dodaj novi podatak";
+$pgv_lang["add_father"] 			= "Dodaj oca";
+$pgv_lang["add_husb"]				= "Dodaj supruga";
+##$pgv_lang["add_opf_child"]				= "Add a child to create a one-parent family";
+##$pgv_lang["add_husb_to_family"] 	= "Add a husband to this family";
+##$pgv_lang["add_media"]				= "Add a new Media item";
+##$pgv_lang["add_media_lbl"]			= "Add Media";
+$pgv_lang["add_mother"] 			= "Dodaj majku";
+$pgv_lang["add_new_chil"] 			= "Dodaj dete";
+$pgv_lang["add_new_husb"]			= "Dodaj supruga";
+$pgv_lang["add_new_wife"]			= "Dodaj suprugu";
+$pgv_lang["add_note"]				= "Dodaj novu belešku";
+$pgv_lang["add_note_lbl"]			= "Dodaj belešku";
+$pgv_lang["add_sibling"]			= "Dodaj brata ili sestru";
+$pgv_lang["add_son_daughter"]		= "Dodaj sina ili ćerku";
+##$pgv_lang["add_source"] 			= "Add a new Source Citation";
+##$pgv_lang["add_source_lbl"] 		= "Add Source Citation";
+$pgv_lang["add_wife"]				= "Dodaj suprugu";
+$pgv_lang["add_wife_to_family"] 	= "Dodaj suprugu ovoj porodici";
+##$pgv_lang["advanced_search_discription"] = "Advanced site search";
+##$pgv_lang["auto_thumbnail"]			= "Automatic thumbnail";
+$pgv_lang["basic_search"]			= "pretraga";
+##$pgv_lang["basic_search_discription"] = "Basic site search";
+$pgv_lang["birthdate_search"]		= "Datum rođenja: ";
+##$pgv_lang["birthplace_search"]		= "Birth Place: ";
+##$pgv_lang["change"]					= "Change";
+##$pgv_lang["change_family_instr"]	= "Use this page to change or remove family members.<br /><br />For each member in the family, you can use the Change link to choose a different person to fill that role in the family.  You can also use the Remove link to remove that person from the family.<br /><br />When you have finished changing the family members, click the Save button to save the changes.<br />";
+$pgv_lang["change_family_members"]	= "Promeni članove porodice";
+##$pgv_lang["changes_occurred"]		= "The following changes were made to this record:";
+##$pgv_lang["confirm_remove"]			= "Are you sure you want to remove this person from the family?";
+##$pgv_lang["confirm_remove_object"]	= "Are you sure you want to remove this object from the database?";
+##$pgv_lang["create_repository"]		= "Create Repository";
+##$pgv_lang["create_source"]			= "Create a new source";
+##$pgv_lang["current_person"]         = "Same as current";
+$pgv_lang["date"]					= "Datum";
+$pgv_lang["deathdate_search"]		= "Datum smrti: ";
+$pgv_lang["deathplace_search"]		= "Mesto smrti: ";
+##$pgv_lang["delete_dir_success"]		= "Media and thumbnail directories successfully removed.";
+##$pgv_lang["delete_file"]			= "Delete file";
+##$pgv_lang["delete_repo"]			= "Delete Repository";
+##$pgv_lang["directory_not_empty"]	= "Directory not empty.";
+##$pgv_lang["directory_not_exist"]	= "Directory does not exist.";
+##$pgv_lang["error_remote"]           = "You have selected a remote site.";
+##$pgv_lang["error_same"]             = "You have selected the same site.";
+##$pgv_lang["external_file"]			= "This media object does not exist as a file on this server.  It cannot be deleted, moved, or renamed.";
+##$pgv_lang["file_missing"]			= "No file was received. Please upload again.";
+##$pgv_lang["file_partial"]			= "File was only partially uploaded, please try again";
+##$pgv_lang["file_success"]			= "File successfully uploaded";
+##$pgv_lang["file_too_big"]			= "Uploaded file exceeds the allowed size";
+##$pgv_lang["file_no_temp_dir"]		= "Missing PHP temporary directory";
+##$pgv_lang["file_cant_write"]		= "PHP failed to write to disk";
+##$pgv_lang["file_bad_extension"]		= "PHP blocked file by extension";
+##$pgv_lang["file_unkown_err"]		= "Unknown file upload error code #pgv_lang[global_num1]#. Please report this as a bug.";
+##$pgv_lang["folder"]		 			= "Folder on server";
+##$pgv_lang["gedcom_editing_disabled"]	= "Editing this GEDCOM has been disabled by the administrator.";
+##$pgv_lang["gedcomid"]				= "GEDCOM INDI record ID";
+##$pgv_lang["gedrec_deleted"] 		= "GEDCOM record successfully deleted.";
+##$pgv_lang["gen_thumb"]				= "Create thumbnail";
+##$pgv_lang["gen_missing_thumbs"]		= "Create missing thumbnails";
+##$pgv_lang["gen_missing_thumbs_lbl"]	= "Missing thumbnails";
+##$pgv_lang["gender_search"]			= "Gender: ";
+##$pgv_lang["generate_thumbnail"]		= "Generate thumbnail automatically from ";
+##$pgv_lang["hebrew_givn"]			= "Hebrew Given Names";
+##$pgv_lang["hebrew_surn"]			= "Hebrew Surname";
+##$pgv_lang["hide_changes"]			= "Click here to hide changes.";
+##$pgv_lang["highlighted"]			= "Highlighted Image";
+##$pgv_lang["illegal_chars"]			= "Blank name or illegal characters in name";
+##$pgv_lang["invalid_search_multisite_input"] = "Please enter one of the following:  Name, Birth Date, Birth Place, Death Date, Death Place, and Gender ";
+##$pgv_lang["invalid_search_multisite_input_gender"] = "Please search again with more information than just gender";
+##$pgv_lang["label_diff_server"]      = "New remote site";
+##$pgv_lang["label_location"]         = "Site Location";
+##$pgv_lang["label_password_id2"]		= "Password: ";
+##$pgv_lang["label_rel_to_current"]   = "Relationship to current person";
+##$pgv_lang["label_same_server"]      = "Local site";
+##$pgv_lang["label_site"]             = "Site";
+##$pgv_lang["label_site_url"]         = "Site URL:";
+##$pgv_lang["label_username_id2"]		= "Username: ";
+##$pgv_lang["lbl_server_list"]        = "Existing remote site";
+##$pgv_lang["lbl_type_server"]		= "Type in a new site.";
+##$pgv_lang["link_as_child"]			= "Link this person to an existing family as a child";
+##$pgv_lang["link_as_husband"]		= "Link this person to an existing family as a husband";
+##$pgv_lang["link_success"]			= "Successfully added link";
+##$pgv_lang["link_to_existing_media"]		= "Link to an existing Media item";
+##$pgv_lang["max_media_depth"]		= "You can enter no more than #GLOBALS[MEDIA_DIRECTORY_LEVELS]# subdirectory names";
+##$pgv_lang["max_upload_size"]		= "Maximum upload size: ";
+##$pgv_lang["media_deleted"]			= "Media directory successfully removed.";
+##$pgv_lang["media_exists"]			= "Media file already exists.";
+##$pgv_lang["media_file"] 			= "Media file to upload";
+##$pgv_lang["media_file_deleted"]		= "Media file successfully deleted.";
+##$pgv_lang["media_file_moved"]			= "Media file moved.";
+##$pgv_lang["media_file_not_moved"]	= "Media file could not be moved.";
+##$pgv_lang["media_file_not_renamed"]	= "Media file could not be moved or renamed.";
+##$pgv_lang["media_thumb_exists"]		= "Media thumbnail already exists.";
+##$pgv_lang["multiple_gedcoms"]		= "This file is linked to another genealogical database on this server.  It cannot be deleted, moved, or renamed until these links have been removed.";
+##$pgv_lang["must_provide"]			= "You must provide a ";
+##$pgv_lang["name_search"]			= "Name: ";
+##$pgv_lang["new_repo_created"]		= "New Repository created";
+##$pgv_lang["new_source_created"] 	= "New source created successfully.";
+##$pgv_lang["no_changes"] 			= "There are currently no changes to be reviewed.";
+##$pgv_lang["no_known_servers"]		= "No known Servers<br />No results will be found";
+##$pgv_lang["no_temple"]				= "No Temple - Living Ordinance";
+##$pgv_lang["no_upload"]				= "Uploading media files is not allowed because multi-media items have been disabled or because the media directory is not writable.";
+##$pgv_lang["paste_id_into_field"]	= "Paste the following ID into your editing fields to reference the newly created record ";
+##$pgv_lang["paste_rid_into_field"]	= "Paste the following Repository ID into your editing fields to reference this Repository ";
+##$pgv_lang["privacy_not_granted"]	= "You have no access to";
+##$pgv_lang["privacy_prevented_editing"]	= "Privacy settings prevent you from editing this record.";
+##$pgv_lang["record_marked_deleted"]		= "This record has been marked for deletion upon admin approval.";
+$pgv_lang["replace_with"]			= "Zemeni sa";
+##$pgv_lang["show_changes"]			= "This record has been updated.  Click here to show changes.";
+##$pgv_lang["thumb_genned"]			= "Thumbnail #thumbnail# generated automatically.";
+##$pgv_lang["thumbgen_error"]			= "Thumbnail #thumbnail# could not be generated automatically.";
+##$pgv_lang["thumbnail"]				= "Thumbnail to upload";
+##$pgv_lang["title_remote_link"]      = "Add Remote Link";
+$pgv_lang["undo"]					= "Opozovi";
+$pgv_lang["undo_all"]				= "Opozovi sve promene";
+##$pgv_lang["undo_all_confirm"]		= "Are you sure you want to undo all of the changes for this GEDCOM?";
+$pgv_lang["undo_successful"]		= "Opoziv useo";
+$pgv_lang["update_successful"]		= "Uspešno ažurirano";
+##$pgv_lang["upload"]					= "Upload";
+##$pgv_lang["upload_error"]			= "There was an error uploading your file.";
+##$pgv_lang["copy_error"]				= "The file #GLOBALS[whichFile2]# could not be copied from #GLOBALS[whichFile1]#";
+##$pgv_lang["upload_media"]			= "Upload Media files";
+##$pgv_lang["upload_media_help"]		= "~#pgv_lang[upload_media]#~<br /><br />Select files from your local computer to upload to your server.  All files will be uploaded to the directory <b>#MEDIA_DIRECTORY#</b> or to one of its sub-directories.<br /><br />Folder names you specify will be appended to #MEDIA_DIRECTORY#. For example, #MEDIA_DIRECTORY#myfamily. If the thumbnail directory does not exist, it is created automatically.";
+##$pgv_lang["upload_successful"]		= "Upload successful.";
+##$pgv_lang["view_change_diff"]		= "View Change Diff";
+
+?>
