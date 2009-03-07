17de4cf1df6a99931547dadcd3399ddaf159a431
NAME <USER@DOMAIN>
2009-03-07 08:22:43 +0000

    Raw gedcom data now no longer contains \r or \t chars
    git-svn-id: svn://svn.webtrees.net/trunk@5192 4201c862-f99b-1943-94d0-25fd05010cbc

--- phpGedView/edit_merge.php
+++ phpGedView/edit_merge.php
@@ -64,10 +64,6 @@ if ($action!="choose") {
 		else $gedrec2 = find_updated_record($gid2);
 		$GEDCOM=$ged;
 
-		// Ensure line endings are the same, otherwise we can't match records
-		$gedrec1=preg_replace('/[\r\n]+/', "\n", $gedrec1);
-		$gedrec2=preg_replace('/[\r\n]+/', "\n", $gedrec2);
-
 		// Fetch the original XREF - may differ in case from the supplied value
 		$tmp=new Person($gedrec1); $gid1=$tmp->getXref();
 		$tmp=new Person($gedrec2); $gid2=$tmp->getXref();
