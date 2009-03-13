6626a1c673333160830a40364c5752cf5a2f785b
NAME <USER@DOMAIN>
2009-03-13 20:00:36 +0000

    Fix broken module translations and a few fixes
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@5220 4201c862-f99b-1943-94d0-25fd05010cbc

--- modules/lightbox/languages/lang.es.php
+++ modules/lightbox/languages/lang.es.php
@@ -1,3 +1,39 @@
+<?php
+/**
+ * Lightbox Album module for phpGedView
+ *
+ * Display media Items using Lightbox
+ *
+ * phpGedView: Genealogy Viewer
+ * Copyright (C) 2002 to 2008  PGV Development Team.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @package PhpGedView
+ * @subpackage Module
+ * @version $Id$
+ * @author Brian Holland
+ * @translator: Julio Sánchez Fernández
+ */
+
+if (!defined('PGV_PHPGEDVIEW')) {
+	header('HTTP/1.0 403 Forbidden');
+	exit;
+}
+
+// Added in VERSION 4.1.6
 $pgv_lang["lb_toAdminConfigPage"]	= "Volver a la página de Administración";
 $pgv_lang["lb_manage"]				= "Gestionar la configuración de Lightbox";
 $pgv_lang["lb_generalhelp"]			= "Página personal - Ayuda general de Lightbox";
@@ -59,7 +95,7 @@ $pgv_lang["lb_url_dimensions"]			= "Dimensiones de las ventanas de URL";
 $pgv_lang["lb_url_dimensionsAdvice"]	= "Anchura y altura en píxeles de las ventanas para URL";
 $pgv_lang["lb_width"]					= "Anchura";
 $pgv_lang["lb_height"]					= "Altura";
-
+									
 
 // ---------------------------------------------------------------------
 
