e6681fd82d829fc1485243b3f0eabf526cfeded7
NAME <USER@DOMAIN>
2009-03-20 00:36:44 +0000

    Minor updates
    git-svn-id: svn://svn.webtrees.net/branches/phpGedView-ca@5237 4201c862-f99b-1943-94d0-25fd05010cbc

--- modules/census_assistant/census_5_input.php
+++ modules/census_assistant/census_5_input.php
@@ -1,6 +1,33 @@
 <?php
+/**
+ * Census Assistant Control module for phpGedView
+ *
+ * Census input table area
+ *
+ * phpGedView: Genealogy Viewer
+ * Copyright (C) 2002 to 2008  PGV Development Team.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @package PhpGedView
+ * @subpackage Census Assistant
+ * @version $Id$
+ */
 
 ?>
+
 <!--
 #tblSample td, th { padding: 0.5; }
 .classy0 { background-color: #234567; color: #89abcd; }
