6bf9c29b09df10ab3c981b17f8272f95a9f5683e
NAME <USER@DOMAIN>
2009-04-04 19:12:51 +0000

    Updating version number
    git-svn-id: svn://svn.webtrees.net/tags/phpGedView-4.2.1@5333 4201c862-f99b-1943-94d0-25fd05010cbc

--- includes/session.php
+++ includes/session.php
@@ -32,7 +32,7 @@ if (stristr($_SERVER["SCRIPT_NAME"], basename(__FILE__))!==false) {
 // Identify ourself
 define('PGV_PHPGEDVIEW',      'PhpGedView');
 define('PGV_VERSION',         '4.2.1');
-define('PGV_VERSION_RELEASE', 'svn'); // 'svn', 'beta', 'rc1', '', etc.
+define('PGV_VERSION_RELEASE', ''); // 'svn', 'beta', 'rc1', '', etc.
 define('PGV_VERSION_TEXT',    trim(PGV_VERSION.' '.PGV_VERSION_RELEASE));
 define('PGV_PHPGEDVIEW_URL',  'http://www.phpgedview.net');
 define('PGV_PHPGEDVIEW_WIKI', 'http://wiki.phpgedview.net');
