ea996472f1be0824f377949d4856eb91ce82bbd8
NAME <USER@DOMAIN>
2009-04-07 06:36:59 +0000

    Migrate from Pear::DB to PDO
    git-svn-id: svn://svn.webtrees.net/trunk@5355 4201c862-f99b-1943-94d0-25fd05010cbc

--- phpGedView/includes/functions/functions_mediadb.php
+++ phpGedView/includes/functions/functions_mediadb.php
@@ -530,7 +530,7 @@ function get_medialist($currentdir = false, $directory = "", $linkonly = false, 
 	$myDir = str_replace($MEDIA_DIRECTORY, "", $directory);
 	$sql = "SELECT m_id, m_file, m_media, m_gedrec, m_titl FROM {$TBLPREFIX}media WHERE m_gedfile={$GEDCOMS[$GEDCOM]['id']}";
 	if ($random == true) {
-		$sql=sql_limit_select_query("{$sql} ORDER BY ".PGV_DB_RANDOM, 5);
+		$sql=PGV_DB::limit_query("{$sql} ORDER BY ".PGV_DB_RANDOM, 5);
 		$res = & dbquery($sql, true);
 	} else {
 		$sql .= " AND (m_file ".PGV_DB_LIKE." '%" . $DBCONN->escapeSimple($myDir) . "%' OR m_file ".PGV_DB_LIKE." '%://%') ORDER BY m_id desc";
