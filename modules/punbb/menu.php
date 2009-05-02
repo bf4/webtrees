7d0bb6138055c9691c11c0e444fff400d97fb848
NAME <USER@DOMAIN>
2009-05-02 00:58:25 +0000

    Hide top menu for unconfigured PunBB or Gallery2
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@5422 4201c862-f99b-1943-94d0-25fd05010cbc

--- modules/punbb/menu.php
+++ modules/punbb/menu.php
@@ -2,7 +2,7 @@
 /**
 * Menu for punBB Module
 * phpGedView: Genealogy Viewer
-* Copyright (C) 2002 to 2009 John Finlay and Others
+ * Copyright (C) 2002 to 2009  PGV Development Team.  All rights reserved.
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -46,7 +46,7 @@ class punbb_ModuleMenu
 	{
 		global $TEXT_DIRECTION, $PGV_IMAGE_DIR, $PGV_IMAGES, $GEDCOM, $pgv_lang;
 		global $gallery;
-		if(!file_exists('modules/punbb.php')){return null;}
+		if(!file_exists('modules/punbb.php') || !file_exists('modules/punbb/config.php')){return null;}
 
 		if($TEXT_DIRECTION == 'rtl'){$ff = '_rtl';}else{$ff = '';}
 
