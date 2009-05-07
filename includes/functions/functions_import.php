1f3f0a380d87f80bc0c5c94b61a1c1456dfc8090
NAME <USER@DOMAIN>
2009-05-07 06:05:00 +0000

    Fix bug from 5451
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@5463 4201c862-f99b-1943-94d0-25fd05010cbc

--- includes/functions/functions_import.php
+++ includes/functions/functions_import.php
@@ -1071,7 +1071,7 @@ function update_media($gid, $gedrec, $update = false) {
 		$media = new Media($gedrec);
 		//--check if we already have a similar object
 		$new_media = Media::in_obje_list($media);
-		if ($new_media === false) {
+		if (!$new_media) {
 			$sql_insert_media->execute(array($m_id, $new_m_media, $media->ext, $media->title, $media->file, $GEDCOMS[$FILE]["id"], $gedrec));
 			$media_count++;
 		} else {
