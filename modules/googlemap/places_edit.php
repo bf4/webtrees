c04387b72bfd76759d5616ea92cae2c02642e4e6
NAME <USER@DOMAIN>
2009-05-07 18:32:13 +0000

    Migrate from Pear::DB to PDO
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@5465 4201c862-f99b-1943-94d0-25fd05010cbc

--- modules/googlemap/places_edit.php
+++ modules/googlemap/places_edit.php
@@ -132,31 +132,37 @@ function getHighestIndex() {
 $where_am_i=place_id_to_hierarchy($placeid);
 $level=count($where_am_i);
 
-if ($action=='addrecord') {
+if ($action=='addrecord' && PGV_USER_IS_ADMIN) {
+	$statement=
+		PGV_DB::prepare("INSERT INTO {$TBLPREFIX}placelocation (pl_id, pl_parent_id, pl_level, pl_place, pl_long, pl_lati, pl_zoom, pl_icon) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+
 	if (($_POST['LONG_CONTROL'] == '') || ($_POST['NEW_PLACE_LONG'] == '') || ($_POST['NEW_PLACE_LATI'] == '')) {
-		$sql = "INSERT INTO {$TBLPREFIX}placelocation (pl_id, pl_parent_id, pl_level, pl_place, pl_long, pl_lati, pl_zoom, pl_icon) VALUES (".(getHighestIndex()+1).", {$placeid}, {$level}, '".$DBCONN->escapeSimple(stripLRMRLM($_POST['NEW_PLACE_NAME']))."', '' , '', {$_POST['NEW_ZOOM_FACTOR']}, '{$_POST['icon']}');";
+		$statement->execute(array(getHighestIndex()+1, $placeid, $level, stripLRMRLM($_POST['NEW_PLACE_NAME']), null, null, $_POST['NEW_ZOOM_FACTOR'], $_POST['icon']));
 	} else {
-		$sql = "INSERT INTO {$TBLPREFIX}placelocation (pl_id, pl_parent_id, pl_level, pl_place, pl_long, pl_lati, pl_zoom, pl_icon) VALUES (".(getHighestIndex()+1).", {$placeid}, {$level}, '".$DBCONN->escapeSimple(stripLRMRLM($_POST['NEW_PLACE_NAME']))."', '{$_POST['LONG_CONTROL'][3]}{$_POST['NEW_PLACE_LONG']}', '{$_POST['LATI_CONTROL'][3]}{$_POST['NEW_PLACE_LATI']}', {$_POST['NEW_ZOOM_FACTOR']}, '{$_POST['icon']}');";
+		$statement->execute(array(getHighestIndex()+1, $placeid, $level, stripLRMRLM($_POST['NEW_PLACE_NAME']), $_POST['LONG_CONTROL'][3].$_POST['NEW_PLACE_LONG'], $_POST['LATI_CONTROL'][3].$_POST['NEW_PLACE_LATI'], $_POST['NEW_ZOOM_FACTOR'], $_POST['icon']));
 	}
-	if (PGV_USER_IS_ADMIN) {
-		$res = dbquery($sql);
+
+	if ($EDIT_AUTOCLOSE && !PGV_DEBUG) {
+		echo "\n<script type=\"text/javascript\">\n<!--\nedit_close();\n//-->\n</script>";
 	}
-	if ($EDIT_AUTOCLOSE && !PGV_DEBUG) echo "\n<script type=\"text/javascript\">\n<!--\nedit_close();\n//-->\n</script>";
 	echo "<div class=\"center\"><a href=\"javascript:;\" onclick=\"edit_close();return false;\">".$pgv_lang["close_window"]."</a></div><br />\n";
 	print_simple_footer();
 	exit;
 }
 
-if ($action=='updaterecord') {
+if ($action=='updaterecord' && PGV_USER_IS_ADMIN) {
+	$statement=
+		PGV_DB::prepare("UPDATE {$TBLPREFIX}placelocation SET pl_place=?, pl_lati=?, pl_long=?, pl_zoom=?, pl_icon=? WHERE pl_id=?");
+
 	if (($_POST['LONG_CONTROL'] == '') || ($_POST['NEW_PLACE_LONG'] == '') || ($_POST['NEW_PLACE_LATI'] == '')) {
-		$sql = "UPDATE {$TBLPREFIX}placelocation SET pl_place='".$DBCONN->escapeSimple(stripLRMRLM($_POST['NEW_PLACE_NAME']))."', pl_lati='', pl_long='', pl_zoom={$_POST['NEW_ZOOM_FACTOR']}, pl_icon='{$_POST['icon']}' WHERE pl_id={$placeid}";
+		$statement->execute(array(stripLRMRLM($_POST['NEW_PLACE_NAME']), null, null, $_POST['NEW_ZOOM_FACTOR'], $_POST['icon'], $placeid));
 	} else {
-		$sql = "UPDATE {$TBLPREFIX}placelocation SET pl_place='".$DBCONN->escapeSimple(stripLRMRLM($_POST['NEW_PLACE_NAME']))."',pl_lati='{$_POST['LATI_CONTROL'][3]}{$_POST['NEW_PLACE_LATI']}', pl_long='{$_POST['LONG_CONTROL'][3]}{$_POST['NEW_PLACE_LONG']}',pl_zoom={$_POST['NEW_ZOOM_FACTOR']}, pl_icon='{$_POST['icon']}' WHERE pl_id={$placeid}";
+		$statement->execute(array(stripLRMRLM($_POST['NEW_PLACE_NAME']), $_POST['LATI_CONTROL'][3].$_POST['NEW_PLACE_LATI'], $_POST['LONG_CONTROL'][3].$_POST['NEW_PLACE_LONG'], $_POST['NEW_ZOOM_FACTOR'], $_POST['icon'], $placeid));
 	}
-	if (PGV_USER_IS_ADMIN) {
-		$res = dbquery($sql);
+
+	if ($EDIT_AUTOCLOSE && !PGV_DEBUG) {
+		echo "\n<script type=\"text/javascript\">\n<!--\nedit_close();\n//-->\n</script>";
 	}
-	if ($EDIT_AUTOCLOSE && !PGV_DEBUG) echo "\n<script type=\"text/javascript\">\n<!--\nedit_close();\n//-->\n</script>";
 	echo "<div class=\"center\"><a href=\"javascript:;\" onclick=\"edit_close();return false;\">".$pgv_lang["close_window"]."</a></div><br />\n";
 	print_simple_footer();
 	exit;
