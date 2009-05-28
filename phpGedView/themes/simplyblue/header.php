7cbc670c7a1bbd34945363d4519556c3b5fd3072
NAME <USER@DOMAIN>
2009-05-28 19:47:46 +0000

    Fix: html attributes in header containing html markup
    git-svn-id: svn://svn.webtrees.net/trunk@5595 4201c862-f99b-1943-94d0-25fd05010cbc

--- phpGedView/themes/simplyblue/header.php
+++ phpGedView/themes/simplyblue/header.php
@@ -29,27 +29,24 @@ if (!defined('PGV_PHPGEDVIEW')) {
 	exit;
 }
 
-global $SEARCH_SPIDER, $TEXT_DIRECTION;
-
 $displayDate=timestamp_to_gedcom_date(client_time())->Display(false);
-if (!isset($view)) $view = safe_REQUEST($_REQUEST, 'view', PGV_REGEX_XREF);
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=<?php print $CHARACTER_SET; ?>" />
-		<?php if( $FAVICON ) { ?><link rel="shortcut icon" href="<?php print $FAVICON; ?>" type="image/x-icon" /> <?php	} ?>
+		<meta http-equiv="Content-Type" content="text/html; charset=<?php echo $CHARACTER_SET; ?>" />
+		<?php if ($FAVICON) { ?><link rel="shortcut icon" href="<?php echo $FAVICON; ?>" type="image/x-icon" /> <?php } ?>
 
-		<title><?php print $title; ?></title>
+		<title><?php echo $title; ?></title>
 		<?php if ($ENABLE_RSS && !$REQUIRE_AUTHENTICATION){ ?>
-			<link href="<?php print encode_url("{$SERVER_URL}rss.php?ged={$GEDCOM}"); ?>" rel="alternate" type="<?php print $applicationType; ?>" title=" <?php print PrintReady(strip_tags($GEDCOM_TITLE), TRUE); ?>" />
+			<link href="<?php echo encode_url("{$SERVER_URL}rss.php?ged={$GEDCOM}"); ?>" rel="alternate" type="<?php echo $applicationType; ?>" title=" <?php echo htmlspecialchars($GEDCOM_TITLE); ?>" />
 		<?php } ?>
-		<link rel="stylesheet" href="<?php print $stylesheet; ?>" type="text/css" media="all" />
-		<?php if ((!empty($rtl_stylesheet))&&($TEXT_DIRECTION=="rtl")) {?> <link rel="stylesheet" href="<?php print $rtl_stylesheet; ?>" type="text/css" media="all" /> <?php } ?>
+		<link rel="stylesheet" href="<?php echo $stylesheet; ?>" type="text/css" media="all" />
+		<?php if ((!empty($rtl_stylesheet))&&($TEXT_DIRECTION=="rtl")) {?> <link rel="stylesheet" href="<?php echo $rtl_stylesheet; ?>" type="text/css" media="all" /> <?php } ?>
 		<?php if ($use_alternate_styles && $BROWSERTYPE != "other") { ?>
-			<link rel="stylesheet" href="<?php print $THEME_DIR.$BROWSERTYPE; ?>.css" type="text/css" media="all" />
-		<?php	}
+			<link rel="stylesheet" href="<?php echo $THEME_DIR.$BROWSERTYPE; ?>.css" type="text/css" media="all" />
+		<?php }
 		// Additional css files required (Only if Lightbox installed)
 		if (is_dir('modules/lightbox/css')) {
 			if ($TEXT_DIRECTION=='rtl') {
@@ -61,42 +58,42 @@ if (!isset($view)) $view = safe_REQUEST($_REQUEST, 'view', PGV_REGEX_XREF);
 			}
 		} ?>
 
-	<link rel="stylesheet" href="<?php print $print_stylesheet; ?>" type="text/css" media="print" />
+	<link rel="stylesheet" href="<?php echo $print_stylesheet; ?>" type="text/css" media="print" />
 	<?php if ($BROWSERTYPE == "msie") { ?>
 	<style type="text/css">
 		FORM { margin-top: 0px; margin-bottom: 0px; }
 	</style>
 	<?php }
 	if ($view!="preview" && $view!="simple") { ?>
-		<?php if (!empty($META_AUTHOR)) { ?><meta name="author" content="<?php print PrintReady(strip_tags($META_AUTHOR), TRUE); ?>" /><?php } ?>
-		<?php if (!empty($META_PUBLISHER)) { ?><meta name="publisher" content="<?php print PrintReady(strip_tags($META_PUBLISHER), TRUE); ?>" /><?php } ?>
-		<?php if (!empty($META_COPYRIGHT)) { ?><meta name="copyright" content="<?php print PrintReady(strip_tags($META_COPYRIGHT), TRUE); ?>" /><?php } ?>
-		<meta name="keywords" content="<?php print PrintReady(strip_tags($META_KEYWORDS), TRUE).PrintReady(strip_tags($surnameList), TRUE);?>" />
-		<?php if (!empty($META_DESCRIPTION)) {?><meta name="description" content="<?php print preg_replace("/\"/", "", PrintReady(strip_tags($META_DESCRIPTION), TRUE));?>" /><?php } ?>
-		<?php if (!empty($META_PAGE_TOPIC)) {?><meta name="page-topic" content="<?php print preg_replace("/\"/", "", PrintReady(strip_tags($META_PAGE_TOPIC), TRUE));?>" /><?php } ?>
-		<?php if (!empty($META_AUDIENCE)) {?><meta name="audience" content="<?php print PrintReady(strip_tags($META_AUDIENCE), TRUE);?>" /><?php } ?>
-		<?php if (!empty($META_PAGE_TYPE)) {?><meta name="page-type" content="<?php print PrintReady(strip_tags($META_PAGE_TYPE), TRUE);?>" /><?php } ?>
-		<?php if (!empty($META_ROBOTS)) {?><meta name="robots" content="<?php print PrintReady(strip_tags($META_ROBOTS), TRUE);?>" /><?php } ?>
-		<?php if (!empty($META_REVISIT)) {?><meta name="revisit-after" content="<?php print PrintReady(strip_tags($META_REVISIT), TRUE);?>" /><?php } ?>
-		<meta name="generator" content="<?php echo PGV_PHPGEDVIEW." - ".PGV_PHPGEDVIEW_URL;?>" />
-	<?php }	?>
-	<?php print $javascript; ?>
-	<?php print $head; //-- additional header information ?>
+		<?php if (!empty($META_AUTHOR)) { ?><meta name="author" content="<?php echo htmlspecialchars($META_AUTHOR); ?>" /><?php } ?>
+		<?php if (!empty($META_PUBLISHER)) { ?><meta name="publisher" content="<?php echo htmlspecialchars($META_PUBLISHER); ?>" /><?php } ?>
+		<?php if (!empty($META_COPYRIGHT)) { ?><meta name="copyright" content="<?php echo htmlspecialchars($META_COPYRIGHT); ?>" /><?php } ?>
+		<meta name="keywords" content="<?php echo htmlspecialchars($META_KEYWORDS.$surnameList); ?>" />
+		<?php if (!empty($META_DESCRIPTION)) {?><meta name="description" content="<?php echo htmlspecialchars($META_DESCRIPTION); ?>" /><?php } ?>
+		<?php if (!empty($META_PAGE_TOPIC)) {?><meta name="page-topic" content="<?php echo htmlspecialchars($META_PAGE_TOPIC); ?>" /><?php } ?>
+		<?php if (!empty($META_AUDIENCE)) {?><meta name="audience" content="<?php echo htmlspecialchars($META_AUDIENCE); ?>" /><?php } ?>
+		<?php if (!empty($META_PAGE_TYPE)) {?><meta name="page-type" content="<?php echo htmlspecialchars($META_PAGE_TYPE); ?>" /><?php } ?>
+		<?php if (!empty($META_ROBOTS)) {?><meta name="robots" content="<?php echo htmlspecialchars($META_ROBOTS); ?>" /><?php } ?>
+		<?php if (!empty($META_REVISIT)) {?><meta name="revisit-after" content="<?php echo htmlspecialchars($META_REVISIT); ?>" /><?php } ?>
+		<meta name="generator" content="<?php echo PGV_PHPGEDVIEW." - ".PGV_PHPGEDVIEW_URL; ?>" />
+	<?php } ?>
+	<?php echo $javascript; ?>
+	<?php echo $head; //-- additional header information ?>
 </head>
-<body id="body" <?php print $bodyOnLoad; ?>>
+<body id="body" <?php echo $bodyOnLoad; ?>>
 <!-- begin header section -->
 <?php
 if ($view!='simple')
 if ($view=='preview') include($print_headerfile);
 else {?>
-<div id="header" class="<?php print $TEXT_DIRECTION; ?>">
-<div class="headerimg" style="margin: 3px; background: url(<?php print $PGV_IMAGE_DIR; ?>/bannerimage.jpg) no-repeat center <?php print $TEXT_DIRECTION=="ltr"?"left":"right" ?>;">
+<div id="header" class="<?php echo $TEXT_DIRECTION; ?>">
+<div class="headerimg" style="margin: 3px; background: url(<?php echo $PGV_IMAGE_DIR; ?>/bannerimage.jpg) no-repeat center <?php echo $TEXT_DIRECTION=="ltr"?"left":"right"; ?>;">
   <table width="99%" class="headerimg" cellpadding="3" cellspacing="0" style="height:186;">
     <tr>
       <td width="760">
         &nbsp;
       </td>
-    <?php if(empty($SEARCH_SPIDER)) { ?>
+    <?php if (empty($SEARCH_SPIDER)) { ?>
       <td>
         <?php print_lang_form(1); ?>
         <?php print_theme_dropdown(); ?>
@@ -109,14 +106,14 @@ else {?>
           <?php print_gedcom_title_link(TRUE); ?>
         </div>
       </td>
-    <?php if(empty($SEARCH_SPIDER)) { ?>
+    <?php if (empty($SEARCH_SPIDER)) { ?>
       <td style="white-space: nowrap">
         <form action="search.php" method="get">
           <input type="hidden" name="action" value="general" />
           <input type="hidden" name="topsearch" value="yes" />
-          <input type="text" name="query" accesskey="<?php print $pgv_lang["accesskey_search"]?>" size="15" value="<?php print $pgv_lang['search']?>"
-              onfocus="if (this.value == '<?php print $pgv_lang['search']?>') this.value=''; focusHandler();"
-              onblur="if (this.value == '') this.value='<?php print $pgv_lang['search']?>';" />
+          <input type="text" name="query" accesskey="<?php echo $pgv_lang["accesskey_search"]?>" size="15" value="<?php echo $pgv_lang['search']?>"
+              onfocus="if (this.value == '<?php echo $pgv_lang['search']?>') this.value=''; focusHandler();"
+              onblur="if (this.value == '') this.value='<?php echo $pgv_lang['search']?>';" />
           <input type="submit" name="search" value=">" />
         </form>
         <?php print_favorite_selector(); ?>
@@ -128,13 +125,13 @@ else {?>
         <?php print_user_links(); ?>
       </td>
       <td valign="bottom">
-        <?php if(empty($SEARCH_SPIDER)) { ?>
+        <?php if (empty($SEARCH_SPIDER)) { ?>
         <div class="date">
-          <?php print $displayDate; ?>
+          <?php echo $displayDate; ?>
         </div>
         <?php } ?>
-        <a href="<?php print $HOME_SITE_URL; ?>">
-        <?php print $HOME_SITE_TEXT; ?>
+        <a href="<?php echo $HOME_SITE_URL; ?>">
+        <?php echo $HOME_SITE_TEXT; ?>
         </a>
       </td>
     </tr>
