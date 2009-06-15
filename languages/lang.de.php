cf1e1d3b2ce7d423754c3d45d2b656478c443ccf
NAME <USER@DOMAIN>
2009-06-15 02:16:29 +0000

    Language file tweaks
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@5735 4201c862-f99b-1943-94d0-25fd05010cbc

--- languages/lang.de.php
+++ languages/lang.de.php
@@ -32,6 +32,7 @@ if (!defined('PGV_PHPGEDVIEW')) {
 	exit;
 }
 
+$pgv_lang["gedcomid"]					= "ID Nummer dieser Person";
 $pgv_lang["sortby"]						= "Reihenfolge";
 $pgv_lang["exact"]						= "Genau";
 $pgv_lang["begins_with"]				= "Beginnt mit";
