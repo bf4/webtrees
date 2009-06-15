10d82b10de9ee8e24351e9e59661772a6f5b9081
NAME <USER@DOMAIN>
2009-06-15 07:29:06 +0000

    Changed german language file regarding ID spellings
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@5737 4201c862-f99b-1943-94d0-25fd05010cbc

--- languages/facts.de.php
+++ languages/facts.de.php
@@ -96,7 +96,7 @@ $factarray["FORM"]	= "Format";
 $factarray["GIVN"]	= "Vornamen";
 $factarray["GRAD"]	= "Schulabschluß";
 $factarray["HUSB"]  = "Ehemann";
-$factarray["IDNO"]	= "Identifikationsnummer";
+$factarray["IDNO"]	= "ID-Nummer";
 $factarray["IMMI"]	= "Einwanderung";
 $factarray["LEGA"]	= "Erbe";
 $factarray["MARB"]	= "Eheaufgebot";
@@ -158,7 +158,7 @@ $factarray["WILL"]	= "Testament";
 $factarray["_EMAIL"]	= "Mail-Adresse";
 $factarray["EMAIL"]	= "Mail-Adresse";
 $factarray["_TODO"]	= "Unerledigt";
-$factarray["_UID"]	= "Einmalig vorkommende ID-Nummer";
+$factarray["_UID"]	= "Universelle ID-Nummer";
 $factarray["_PRIM"]	= "Bevorzugtes Bild";
 $factarray["_DBID"] = "ID der fremden Datenbank";
 
