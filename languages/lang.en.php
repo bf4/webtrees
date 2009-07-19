b95ea3208e98f6366d264d5c1da1c04b914348f1
NAME <USER@DOMAIN>
2009-07-19 17:33:49 +0000

    added space to ahnentafel and descendency reports
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@5932 4201c862-f99b-1943-94d0-25fd05010cbc

--- languages/lang.en.php
+++ languages/lang.en.php
@@ -1469,7 +1469,7 @@ $pgv_lang["ahnentafel_buried_m"]	= "was buried";			// male
 $pgv_lang["ahnentafel_buried_f"]	= "was buried";			// female
 $pgv_lang["ahnentafel_place"]		= " in ";				// place name follows this
 $pgv_lang["ahnentafel_no_details"]	= " but the details are unknown";
-$pgv_lang["ahnentafel_on"]			= "on";
+$pgv_lang["ahnentafel_on"]			= " on";
 
 //-- Descendency report
 $pgv_lang["son_of"]					= "son of";
