ab2b92cec45f085ea7f019e49dd62d845415e97c
NAME <USER@DOMAIN>
2009-07-20 13:00:35 +0000

    Implement new function UTF8_wordwrap()
    git-svn-id: svn://svn.webtrees.net/trunk@5938 4201c862-f99b-1943-94d0-25fd05010cbc

--- phpGedView/includes/functions/functions_UTF8.php
+++ phpGedView/includes/functions/functions_UTF8.php
@@ -442,4 +442,52 @@ function UTF8_strncasecmp($text1, $text2, $maxLen=0) {
 
 	return UTF8_strcmp(UTF8_strtoupper($UTF8_text1), UTF8_strtoupper($UTF8_text2));
 }
+
+
+/*
+ * Word wrap
+ */
+function UTF8_wordwrap($text, $width=75, $break="\n", $cut=FALSE) {
+	if ($width<=0) $width = 75;
+	if (is_string($text) && strlen($text)<=$width) return $text;	// Nothing to do
+	if ($break=='') $break = "\n";
+
+	$UTF8_text = UTF8_str_split($text);
+	$UTF8_break = UTF8_str_split($break);
+	$UTF8_result = array();
+
+	while (UTF8_strlen($UTF8_text)>$width) {
+		$longWord = FALSE;
+		for ($i=$width; $i>=0; $i--) {
+			if ($UTF8_text[$i]==' ') break;
+		}
+		if ($i<0) {
+			// We're dealing with a very long word
+			// Not too sure what $cut is supposed to accomplish -- we'll just chop the word
+			$longWord = TRUE;	// This means: "not wrapping at a space"
+			$i = $width;
+		}
+
+		$thisPiece = array_slice($UTF8_text, 0, $i);
+		foreach ($thisPiece as $char) {
+			// Copy front part of input string
+			$UTF8_result[] = $char;
+		}
+		foreach ($UTF8_break as $char) {
+			// Copy separator string
+			$UTF8_result[] = $char;
+		}
+
+		if (!$longWord) $i++;		// Skip space at end of piece we've just worked on
+		$UTF8_text = UTF8_substr($UTF8_text, $i);		// Remove that piece
+	}
+
+	foreach ($UTF8_text as $char) {
+		// Copy remainder of input string
+		$UTF8_result[] = $char;
+	}
+
+	if (is_array($text)) return $UTF8_result;
+	return implode('', $UTF8_result);
+}
 ?>
