b5c23e06ee1c5a6e82b845e128d40d968f86e89f
NAME <USER@DOMAIN>
2009-08-10 18:12:16 +0000

    support tls protocol in SMTP mailer
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@6057 4201c862-f99b-1943-94d0-25fd05010cbc

--- includes/functions/functions_mail.php
+++ includes/functions/functions_mail.php
@@ -112,8 +112,8 @@ function pgvMail($to, $from, $subject, $message) {
 		$message = $htmlMessage;
 	}
 	// if SMTP mail is set active AND we have SMTP settings available, use the PHPMailer classes
-	if ( $PGV_SMTP_ACTIVE  && ( $PGV_SMTP_HOST && $PGV_SMTP_PORT ) ) {
-		require_once( 'includes/classes/class_phpmailer.php' );
+	if ($PGV_SMTP_ACTIVE  && ( $PGV_SMTP_HOST && $PGV_SMTP_PORT ) ) {
+		require_once('includes/classes/class_phpmailer.php');
 		$mail_object = new PHPMailer();
 		$mail_object->IsSMTP();
 		$mail_object->SetLanguage('en','languages/');
@@ -122,8 +122,10 @@ function pgvMail($to, $from, $subject, $message) {
 			$mail_object->Username = $PGV_SMTP_AUTH_USER;
 			$mail_object->Password = $PGV_SMTP_AUTH_PASS;
 		}
-		if ($PGV_SMTP_SSL) {
-			$mail_object->SMTPSecure = "ssl";
+		if ($PGV_SMTP_SSL=='ssl') {
+			$mail_object->SMTPSecure = 'ssl';
+		} else if ($PGV_SMTP_SSL=='tls') {
+			$mail_object->SMTPSecure = 'tls';
 		}
 		$mail_object->Host = $PGV_SMTP_HOST;
 		$mail_object->Port = $PGV_SMTP_PORT;
