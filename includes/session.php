2fa1cfc0c32a9e1979f5e32e8b6556f8011a2249
NAME <USER@DOMAIN>
2009-09-30 21:00:47 +0000

    streamline session.php - move db connection before session start - will permit database session storage
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@6173 4201c862-f99b-1943-94d0-25fd05010cbc

--- includes/session.php
+++ includes/session.php
@@ -200,11 +200,11 @@ if (version_compare(PHP_VERSION, '6.0.0', '<')) {
  *  can only be changed within limits.
  */
 $configOverride = false;
-	// Check for override of $CONFIG_VARS
+// Check for override of $CONFIG_VARS
 if (isset($_REQUEST["CONFIG_VARS"])) $configOverride = true;
 
-	// $CONFIG_VARS is safe: now check for any in its list
-	foreach($CONFIG_VARS as $VAR) {
+// $CONFIG_VARS is safe: now check for any in its list
+foreach($CONFIG_VARS as $VAR) {
 	if (isset($_REQUEST[$VAR])) {
 		$configOverride = true;
 		break ;
@@ -214,7 +214,7 @@ if (isset($_REQUEST["CONFIG_VARS"])) $configOverride = true;
 //-- check if they are trying to hack
 if ($configOverride) {
 	if (!ini_get('register_globals') || strtolower(ini_get('register_globals'))=="off") {
-		require_once 'includes/authentication.php' ;
+		require 'includes/authentication.php' ;
 		AddToLog("MSG>Configuration override detected; script terminated.");
 		AddToLog("UA>{$_SERVER["HTTP_USER_AGENT"]}<");
 		AddToLog("URI>{$_SERVER["REQUEST_URI"]}<");
@@ -225,11 +225,11 @@ if ($configOverride) {
 }
 
 //-- load file for language settings
-require_once 'includes/lang_settings_std.php';
+require 'includes/lang_settings_std.php';
 $Languages_Default = true;
 if (!strstr($_SERVER["REQUEST_URI"], "INDEX_DIRECTORY=") && file_exists($INDEX_DIRECTORY . "lang_settings.php")) {
 	$DefaultSettings = $language_settings; // Save default settings, so we can merge properly
-	require_once "{$INDEX_DIRECTORY}lang_settings.php";
+	require "{$INDEX_DIRECTORY}lang_settings.php";
 	$ConfiguredSettings = $language_settings; // Save configured settings, same reason
 	$language_settings = array_merge($DefaultSettings, $ConfiguredSettings); // Copy new langs into config
 	// Now copy new language settings into existing configuration
@@ -294,13 +294,39 @@ if (empty($PGV_MEMORY_LIMIT)) $PGV_MEMORY_LIMIT = "32M";
 @ini_set('memory_limit', $PGV_MEMORY_LIMIT);
 
 //--load common functions
-require_once 'includes/functions/functions.php';
-require_once 'includes/functions/functions_name.php';
+require 'includes/functions/functions.php';
+require 'includes/functions/functions_name.php';
 //-- set the error handler
-$OLD_HANDLER = set_error_handler("pgv_error_handler");
+set_error_handler("pgv_error_handler");
 
 //-- setup execution timer
 $start_time = microtime(true);
+
+//-- load db specific functions
+require 'includes/functions/functions_db.php';
+
+// -- load the authentication system, also logging
+require 'includes/authentication.php';
+ 
+// Connect to the database
+require 'includes/classes/class_pgv_db.php';
+try {
+	// remove escape codes before using PW
+	$DBPASS=str_replace(array("\\\\", "\\\"", "\\\$"), array("\\", "\"", "\$"), $DBPASS);
+	PGV_DB::createInstance($DBTYPE, $DBHOST, $DBPORT, $DBNAME, $DBUSER, $DBPASS, $DB_UTF8_COLLATION);
+	unset($DBUSER, $DBPASS);
+	try {
+		PGV_DB::updateSchema('includes/db_schema/', 'PGV_SCHEMA_VERSION', PGV_SCHEMA_VERSION);
+	} catch (PDOException $ex) {
+		// The schema update scripts should never fail.  If they do, there is no clean recovery.
+		die($ex);
+	}
+} catch (PDOException $ex) {
+	// Can't connect to the DB?  We'll get redirected to install.php later.....
+}
+
+//-- load up the code to check for spiders
+require 'includes/session_spider.php';
 
 //-- start the php session
 $time = time()+$PGV_SESSION_TIME;
@@ -316,38 +342,21 @@ if (!empty($SCRIPT_NAME)) {
 session_set_cookie_params($date, $pgv_path);
 unset($date);
 unset($pgv_path);
-if (($PGV_SESSION_TIME>0)&&(function_exists('session_cache_expire'))) session_cache_expire($PGV_SESSION_TIME/60);
-if (!empty($PGV_SESSION_SAVE_PATH)) session_save_path($PGV_SESSION_SAVE_PATH);
-if (isset($MANUAL_SESSION_START) && !empty($SID)) session_id($SID);
+if (($PGV_SESSION_TIME>0)&&(function_exists('session_cache_expire'))) {
+	session_cache_expire($PGV_SESSION_TIME/60);
+}
+if (!empty($PGV_SESSION_SAVE_PATH)) {
+	session_save_path($PGV_SESSION_SAVE_PATH);
+}
+if (isset($MANUAL_SESSION_START) && !empty($SID)) {
+	session_id($SID);
+}
 
 @session_start();
 
-//-- load db specific functions
-require_once 'includes/functions/functions_db.php';
-// -- load the authentication system, also logging
-require_once 'includes/authentication.php';
-//-- load up the code to check for spiders
-require_once 'includes/session_spider.php';
-
-// Connect to the database
-require_once 'includes/classes/class_pgv_db.php';
-try {
-	$DBPASS=str_replace(array("\\\\", "\\\"", "\\\$"), array("\\", "\"", "\$"), $DBPASS); // remove escape codes before using PW
-	PGV_DB::createInstance($DBTYPE, $DBHOST, $DBPORT, $DBNAME, $DBUSER, $DBPASS, $DB_UTF8_COLLATION);
-	unset($DBUSER, $DBPASS);
-	try {
-		PGV_DB::updateSchema('includes/db_schema/', 'PGV_SCHEMA_VERSION', PGV_SCHEMA_VERSION);
-	} catch (PDOException $ex) {
-		// The schema update scripts should never fail.  If they do, there is no clean recovery.
-		die($ex);
-	}
-} catch (PDOException $ex) {
-	// Can't connect to the DB?  We'll get redirected to install.php later.....
-}
-
 //-- import the gedcoms array
 if (file_exists("{$INDEX_DIRECTORY}gedcoms.php")) {
-	require_once "{$INDEX_DIRECTORY}gedcoms.php";
+	require "{$INDEX_DIRECTORY}gedcoms.php";
 	if (!is_array($GEDCOMS)) $GEDCOMS = array();
 	$i=0;
 	foreach ($GEDCOMS as $key=>$gedcom) {
@@ -406,20 +415,19 @@ $PRIV_HIDE   = PGV_PRIV_HIDE;
 /**
  * Load GEDCOM configuration
  */
-require_once 'config_gedcom.php';
-require_once get_config_file();
+require get_config_file();
 
 if (empty($PHPGEDVIEW_EMAIL)) {
 	$PHPGEDVIEW_EMAIL="phpgedview-noreply@".preg_replace("/^www\./i", "", $_SERVER["SERVER_NAME"]);
 }
 
-require_once 'includes/functions/functions_print.php';
-require_once 'includes/functions/functions_rtl.php';
+require 'includes/functions/functions_print.php';
+require 'includes/functions/functions_rtl.php';
 
 if ($MULTI_MEDIA) {
-	require_once 'includes/functions/functions_mediadb.php';
+	require 'includes/functions/functions_mediadb.php';
 }
-require_once 'includes/functions/functions_date.php';
+require 'includes/functions/functions_date.php';
 
 if (empty($PEDIGREE_GENERATIONS)) {
 	$PEDIGREE_GENERATIONS=$DEFAULT_PEDIGREE_GENERATIONS;
@@ -543,11 +551,11 @@ if ($ENABLE_MULTI_LANGUAGE && empty($SEARCH_SPIDER)) {
 	}
 }
 
-require_once 'includes/templecodes.php';  //-- load in the LDS temple code translations
+require 'includes/templecodes.php';  //-- load in the LDS temple code translations
 
 //-- load the privacy functions
 load_privacy_file(get_id_from_gedcom($GEDCOM));
-require_once 'includes/functions/functions_privacy.php';
+require 'includes/functions/functions_privacy.php';
 
 // Define some constants to save calculating the same value repeatedly.
 define('PGV_GEDCOM',            $GEDCOM);
@@ -650,7 +658,7 @@ if ((strstr($SCRIPT_NAME, "install.php")===false)
 
 	//-- load any editing changes
 	if (PGV_USER_CAN_EDIT && file_exists("{$INDEX_DIRECTORY}pgv_changes.php")) {
-		require_once "{$INDEX_DIRECTORY}pgv_changes.php";
+		require "{$INDEX_DIRECTORY}pgv_changes.php";
 	} else {
 		$pgv_changes = array();
 	}
@@ -658,7 +666,6 @@ if ((strstr($SCRIPT_NAME, "install.php")===false)
 	if (empty($LOGIN_URL)) {
 		$LOGIN_URL = "login.php";
 	}
-
 }
 
 //-- load the user specific theme
@@ -676,21 +683,20 @@ if (PGV_DB::isConnected() && PGV_USER_ID && !isset($_REQUEST['logout'])) {
 	}
 }
 
-if (isset($_SESSION["theme_dir"]))
-{
+if (isset($_SESSION["theme_dir"])) {
 	$THEME_DIR = $_SESSION["theme_dir"];
 	if (PGV_USER_ID) {
 		if (get_user_setting(PGV_USER_ID, 'editaccount')=='Y') unset($_SESSION["theme_dir"]);
 	}
 }
 
-if (empty($THEME_DIR)) $THEME_DIR="standard/";
-if (file_exists("{$THEME_DIR}theme.php")) {
-	require_once "{$THEME_DIR}theme.php";
-} else {
-	$THEME_DIR = "themes/standard/";
-	require_once "{$THEME_DIR}theme.php";
+if (empty($THEME_DIR)) {
+	$THEME_DIR="standard/";
 }
+if (!file_exists("{$THEME_DIR}theme.php")) {
+	$THEME_DIR = "themes/standard/";
+}
+require "{$THEME_DIR}theme.php";
 
 require './includes/hitcount.php'; //--load the hit counter
 
