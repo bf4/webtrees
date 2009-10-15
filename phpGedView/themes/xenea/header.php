be881bb6ea50d9ea0b64dedb50f04b8bd7d5518c
NAME <USER@DOMAIN>
2009-10-15 11:51:19 +0000

    implement patch 2862558 - Google Maps - IE 8 - Click Position
    git-svn-id: svn://svn.webtrees.net/trunk@6234 4201c862-f99b-1943-94d0-25fd05010cbc

--- phpGedView/themes/xenea/header.php
+++ phpGedView/themes/xenea/header.php
@@ -38,7 +38,9 @@ $displayDate=timestamp_to_gedcom_date(client_time())->Display(false, $DATE_FORMA
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=<?php echo $CHARACTER_SET; ?>" />
-		<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" />
+		<?php if ($_GET["pgvaction"]=="places_edit") { ?>
+			<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" /> <?php } 
+		?>
 		<?php if ($FAVICON) { ?><link rel="shortcut icon" href="<?php echo $FAVICON; ?>" type="image/x-icon" /> <?php } ?>
 
 		<title><?php echo $title; ?></title>
