e7d24292340ac9549794f0dedcffeacfb45ae107
NAME <USER@DOMAIN>
2009-11-03 20:53:24 +0000

    Fixed: [ 2890679 ] Sorting sources
    git-svn-id: svn://svn.webtrees.net/trunk@6294 4201c862-f99b-1943-94d0-25fd05010cbc

--- phpGedView/repo.php
+++ phpGedView/repo.php
@@ -71,7 +71,7 @@ echo '<table class="list_table"><tr><td>';
 if ($controller->accept_success) {
 	echo '<b>', $pgv_lang['accept_successful'], '</b><br />';
 }
-echo '<span class="name_head">', PrintReady($controller->repository->getFullName());
+echo '<span class="name_head">', PrintReady(htmlspecialchars($controller->repository->getFullName()));
 if ($SHOW_ID_NUMBERS) {
 	echo ' ', getLRM(), '(', $controller->rid, ')', getLRM(); 
 }
