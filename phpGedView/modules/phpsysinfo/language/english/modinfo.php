6814351bf0df21829df101424cf17497a19d7239
NAME <USER@DOMAIN>
2009-11-07 11:15:08 +0000

    Remove module phpsysinfo
    git-svn-id: svn://svn.webtrees.net/trunk@6321 4201c862-f99b-1943-94d0-25fd05010cbc

--- phpGedView/modules/phpsysinfo/language/english/modinfo.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('_MI_NAME', 'PHP SysInfo');
-define('_MI_DESC', 'PHPSysInfo is a customizable PHP Script that parses /proc, and formats information nicely. It will display information about system facts like Uptime, CPU, Memory, PCI devices, SCSI devices, IDE devices, Network adapters, Disk usage, and more.') ;
-define('_MI_PSI_THEME_TITLE', 'Theme');
-define('_MI_PSI_THEME_DESCRIPTION', 'Desc');
-?>
