f7369347765422b073b230560659a72821310d4d
NAME <USER@DOMAIN>
2009-11-08 16:17:24 +0000

    Access gedcom data via API, rather than via global variable
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@6340 4201c862-f99b-1943-94d0-25fd05010cbc

--- includes/functions/functions_import.php
+++ includes/functions/functions_import.php
@@ -980,7 +980,7 @@ function insert_media($objrec, $objlevel, $update, $gid, $ged_id, $count) {
 	}
 	//-- handle embedded OBJE records
 	else {
-		$m_media = get_new_xref("OBJE", true);
+		$m_media = get_new_xref("OBJE", $ged_id, true);
 		$objref = subrecord_createobjectref($objrec, $objlevel, $m_media);
 
 		//-- restructure the record to be a linked record
