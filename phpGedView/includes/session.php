604c0107058fd068268e3183a3542f745b84d5b9
NAME <USER@DOMAIN>
2010-01-24 18:16:28 +0000

    Move banned.php and search_engine.php to new database table.
    git-svn-id: svn://svn.webtrees.net/trunk@6830 4201c862-f99b-1943-94d0-25fd05010cbc

--- phpGedView/includes/session.php
+++ phpGedView/includes/session.php
@@ -47,7 +47,7 @@ define('PGV_DEBUG_PRIV',  false);
 define('PGV_ERROR_LEVEL', 2); // 0=none, 1=minimal, 2=full
 
 // Required version of database tables/columns/indexes/etc.
-define('PGV_SCHEMA_VERSION', 13);
+define('PGV_SCHEMA_VERSION', 14);
 
 // Environmental requirements
 define('PGV_REQUIRED_PHP_VERSION',     '5.2.0'); // 5.2.3 is recommended
