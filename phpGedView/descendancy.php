d02956e66bd46949cc92c826378180c397d16c12
NAME <USER@DOMAIN>
2010-02-03 03:42:03 +0000

    Fixed [ 2944978 ]: Bad Class in Descendancy Chart
    git-svn-id: svn://svn.webtrees.net/trunk@6910 4201c862-f99b-1943-94d0-25fd05010cbc

--- phpGedView/descendancy.php
+++ phpGedView/descendancy.php
@@ -53,7 +53,7 @@ if ($view!="preview") {
 	$show_famlink = true;
 	echo '</td><td width="50px">&nbsp;</td><td><form method="get" name="people" action="?">';
 	echo '<input type="hidden" name="show_full" value="', $controller->show_full, '" />';
-	echo '<table class="list_table', $TEXT_DIRECTION, '">';
+	echo '<table class="list_table ', $TEXT_DIRECTION, '">';
 	echo '<tr><td class="descriptionbox">';
 	print_help_link("desc_rootid_help", "qm", "root_person");
 	echo $pgv_lang["root_person"], "&nbsp;</td>";
