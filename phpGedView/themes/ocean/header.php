6099fa4183e5e66e2387dbf60c95a9557cb53903
NAME <USER@DOMAIN>
2010-02-09 02:21:28 +0000

    Replace variable $THEME_DIR with constant PGV_THEME_DIR in Theme folders
    git-svn-id: svn://svn.webtrees.net/trunk@6926 4201c862-f99b-1943-94d0-25fd05010cbc

--- phpGedView/themes/ocean/header.php
+++ phpGedView/themes/ocean/header.php
@@ -46,7 +46,7 @@ if (!defined('PGV_PHPGEDVIEW')) {
 		<link rel="stylesheet" href="<?php echo $stylesheet; ?>" type="text/css" media="all" />
 		<?php if ((!empty($rtl_stylesheet))&&($TEXT_DIRECTION=="rtl")) {?> <link rel="stylesheet" href="<?php echo $rtl_stylesheet; ?>" type="text/css" media="all" /> <?php } ?>
 		<?php if ($use_alternate_styles && $BROWSERTYPE != "other") { ?>
-			<link rel="stylesheet" href="<?php echo $THEME_DIR.$BROWSERTYPE; ?>.css" type="text/css" media="all" />
+			<link rel="stylesheet" href="<?php echo PGV_THEME_DIR.$BROWSERTYPE; ?>.css" type="text/css" media="all" />
 		<?php }
 		// Additional css files required (Only if Lightbox installed)
 		if (PGV_USE_LIGHTBOX) {
@@ -95,7 +95,7 @@ else {?>
     <td>
       <table width="100%">
         <tr>
-          <td><img src="<?php echo $THEME_DIR; ?>header.jpg" width="281" height="50" alt="" /></td>
+          <td><img src="<?php echo PGV_THEME_DIR; ?>header.jpg" width="281" height="50" alt="" /></td>
           <td>
             <table width="100%">
               <tr>
