e516e959210eea04c194dfee8a0db1e1de55debb
NAME <USER@DOMAIN>
2010-02-21 13:02:27 +0000

    Add: native support for British English
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@6953 4201c862-f99b-1943-94d0-25fd05010cbc

---
+++ languages/faqlist.en-gb.php
@@ -0,0 +1,28 @@
+<?php
+// British English definitions file for PhpGedView.
+// Based on differences from US English 11 Jan 2010.
+//
+// Copyright (C) 2010 Greg Roach.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// $Id$
+
+if (!defined('PGV_PHPGEDVIEW')) {
+	header('HTTP/1.0 403 Forbidden');
+	exit;
+}
+
+$faqlist["FAQ_030_PLACES"]="<strong>PLACES</strong>: We try, wherever known, to include the full place descriptive: city and/or township (Twp) as well as the County, State, and we usually add USA (preferred - not US, U.S., or U.S.A.) behind the state.  For foreign countries, we use the use the GEDCOM-approved 3 letter standard abbreviation rather than the country name: England [ENG], Scotland [SCT], Ireland [IRE], France [FRA], Italy [ITA], etc. The format we prefer is: <i>Indianapolis, Center Twp, Marion Co, Indiana, USA</i>  States should not be abbreviated to the two letters; we generally do not use full stops (.) in names or locations, like <i>Shelbyville, Addison Twp, Shelby Co, Indiana, USA</i> instead of <i>Shelbyville, Addison Twp., Shelby Co., IN</i> or <i>Shelbyville, Addison Township, Shelby County, Indiana, U.S.A.</i>";
