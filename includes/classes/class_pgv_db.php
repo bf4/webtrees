47adeb7e5dd9f42159035d1d8cdc8409947cf799
NAME <USER@DOMAIN>
2010-02-22 17:09:08 +0000

    Remove attempt to support ODBC
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@6970 4201c862-f99b-1943-94d0-25fd05010cbc

--- includes/classes/class_pgv_db.php
+++ includes/classes/class_pgv_db.php
@@ -230,32 +230,6 @@ class PGV_DB {
 			self::$LONGTEXT_TYPE='TEXT';
 			self::$UTF8_TABLE   ='';
 			break;
-		case 'odbc': // This DSN has not been tested!
-			self::$pdo=new PDO(
-				"odbc:$DBNAME", $DBUSER, $DBPASS,
-				array(
-					PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
-					PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_OBJ,
-					PDO::ATTR_CASE=>PDO::CASE_LOWER,
-					PDO::ATTR_AUTOCOMMIT=>true
-				)
-			);
-			self::$AUTO_ID_TYPE ='INTEGER AUTOINCREMENT'; // These values are guesses
-			self::$ID_TYPE      ='INTEGER';
-			self::$INT1_TYPE    ='INTEGER';
-			self::$INT2_TYPE    ='INTEGER';
-			self::$INT3_TYPE    ='INTEGER';
-			self::$INT4_TYPE    ='INTEGER';
-			self::$INT8_TYPE    ='INTEGER';
-			self::$CHAR_TYPE    ='VARCHAR';
-			self::$VARCHAR_TYPE ='VARCHAR';
-			self::$UNSIGNED     ='';
-			self::$LIKE         ='LIKE';
-			self::$RANDOM       ='RANDOM()';
-			self::$TEXT_TYPE    ='TEXT';
-			self::$LONGTEXT_TYPE='TEXT';
-			self::$UTF8_TABLE   ='';
-			break;
 		}
 
 		// Assign the singleton
@@ -359,9 +333,6 @@ class PGV_DB {
 		case 'pgsql':
 		case 'mssql':
 			return "MOD($x,$y)";
-		case 'odbc':
-			// We don't know the underlying database, so this is only a guess
-			return "MOD($x,$y)";
 		}
 	}
 
@@ -375,9 +346,6 @@ class PGV_DB {
 			return 'RANDOM()';
 		case 'mssql':
 			return 'NEWID()';
-		case 'odbc':
-			// We don't know the underlying database, so there is little we can do.
-			return '1';
 		}
 	}
 
@@ -576,9 +544,6 @@ class PGV_DB {
 				break;
 			case 'mssql':
 				$statement=preg_replace('/^\s*SELECT /i', "SELECT TOP {$n} ", $statement);
-				break;
-			case 'odbc':
-				// We don't know the underlying database, so just return all rows :-(
 				break;
 			}
 		}
