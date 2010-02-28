5c8bd75e8e8d105b9ff8801cfeb15d71dca964bc
NAME <USER@DOMAIN>
2010-02-28 13:59:12 +0000

    Base Release
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@7048 4201c862-f99b-1943-94d0-25fd05010cbc

---
+++ themes/pinkplastic/theme.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Pink Plastic theme
+ *
+ * PhpGedView: Genealogy Viewer
+ * Copyright (C) 2010  PGV Development Team.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @package PhpGedView
+ * @subpackage Themes/Colors
+ * @version $Id$
+ */
+
+if (!defined('PGV_PHPGEDVIEW')) {
+	header('HTTP/1.0 403 Forbidden');
+	exit;
+}
+
+$theme_name       = "Pink Plastic";
+$SHARED_THEME_DIR = "themes/colors/";
+$stylesheet       = $SHARED_THEME_DIR . "css/pinkplastic.css"; 
+$print_stylesheet = $SHARED_THEME_DIR . "css/pinkplastic.css";
+
+include('themes/colors/theme.inc');
+
+
