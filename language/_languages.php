e361ad382b634429721488ce8a7ff50fd884d4a9
NAME <USER@DOMAIN>
2010-02-28 17:44:11 +0000

    Add .PO/.MO files for Serbian
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@7060 4201c862-f99b-1943-94d0-25fd05010cbc

---
+++ language/_languages.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * This is a dummy PHP file.  It is not included by another script, and
+ * it does not need to be included in a distribution.
+ *
+ * xgettext will scan this file, adding the strings to the .PO files,
+ * thus allowing us to use them in the code as variables. e.g.
+ *
+ * $fact='BIRT';
+ * echo i18n::translate($fact);
+ *
+ * IMPORTANT: take care that these codes don't conflict with other
+ * lists/categories/strings
+ *
+ * Copyright (C) 2010 Greg Roach
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @version $Id$
+ */
+
+$languages=array(
+	// I18N: This is the RFC5646 language code for Arabic
+	'ar'   =>i18n::translate('ar'),
+	// I18N: This is the RFC5646 language code for Catalan
+	'ca'   =>i18n::translate('ca'),
+	// I18N: This is the RFC5646 language code for Czech
+	'cz'   =>i18n::translate('cz'),
+	// I18N: This is the RFC5646 language code for Danish
+	'da'   =>i18n::translate('da'),
+	// I18N: This is the RFC5646 language code for German
+	'de'   =>i18n::translate('de'),
+	// I18N: This is the RFC5646 language code for Greek
+	'el'   =>i18n::translate('el'),
+	// I18N: This is the RFC5646 language code for English
+	'en'   =>i18n::translate('en'),
+	// I18N: This is the RFC5646 language code for British English
+	'en-gb'=>i18n::translate('en-gb'),
+	// I18N: This is the RFC5646 language code for Spanish
+	'es'   =>i18n::translate('es'),
+	// I18N: This is the RFC5646 language code for Latin-American Spanish
+	'es-ar'=>i18n::translate('es-ar'),
+	// I18N: This is the RFC5646 language code for Estonian
+	'et'   =>i18n::translate('et'),
+	// I18N: This is the RFC5646 language code for Finnish
+	'fi'   =>i18n::translate('fi'),
+	// I18N: This is the RFC5646 language code for French
+	'fr'   =>i18n::translate('fr'),
+	// I18N: This is the RFC5646 language code for Hebrew
+	'he'   =>i18n::translate('he'),
+	// I18N: This is the RFC5646 language code for Hungarian
+	'hu'   =>i18n::translate('hu'),
+	// I18N: This is the RFC5646 language code for Indonesian
+	'id'   =>i18n::translate('id'),
+	// I18N: This is the RFC5646 language code for Italian
+	'it'   =>i18n::translate('it'),
+	// I18N: This is the RFC5646 language code for Lithuanian
+	'lt'   =>i18n::translate('lt'),
+	// I18N: This is the RFC5646 language code for Dutch
+	'nl'   =>i18n::translate('nl'),
+	// I18N: This is the RFC5646 language code for Norwegian
+	'no'   =>i18n::translate('no'),
+	// I18N: This is the RFC5646 language code for Polish
+	'pl'   =>i18n::translate('pl'),
+	// I18N: This is the RFC5646 language code for Portuguese
+	'pt'   =>i18n::translate('pt'),
+	// I18N: This is the RFC5646 language code for Romanian
+	'ro'   =>i18n::translate('ro'),
+	// I18N: This is the RFC5646 language code for Russian
+	'ru'   =>i18n::translate('ru'),
+	// I18N: This is the RFC5646 language code for Slovakian
+	'sk'   =>i18n::translate('sk'),
+	// I18N: This is the RFC5646 language code for Slovenian
+	'sl'   =>i18n::translate('sl'),
+	// I18N: This is the RFC5646 language code for Serbian
+	'sr'   =>i18n::translate('sr'),
+	// I18N: This is the RFC5646 language code for Swedish
+	'sv'   =>i18n::translate('sv'),
+	// I18N: This is the RFC5646 language code for Turkish
+	'tr'   =>i18n::translate('tr'),
+	// I18N: This is the RFC5646 language code for Vietnamese
+	'vi'   =>i18n::translate('vi'),
+	// I18N: This is the RFC5646 language code for Chinese
+	'zh'   =>i18n::translate('zh'),
+);
