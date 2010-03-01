e616a08fe09af5380318daafdf71dba5ec9a1de2
NAME <USER@DOMAIN>
2010-03-01 01:04:29 +0000

    Adding John's tab/module management / sidebar code. Some tidying up to do.
    git-svn-id: svn://svn.webtrees.net/trunk@7077 4201c862-f99b-1943-94d0-25fd05010cbc

---
+++ phpGedView/modules/lightbox/lightbox.php
@@ -0,0 +1,72 @@
+<?php
+require_once('modules/media/media.php');
+include_once('modules/lightbox/lb_defaultconfig.php');
+if (file_exists('modules/lightbox/lb_config.php')) include_once('modules/lightbox/lb_config.php');
+
+if (!defined('PGV_PHPGEDVIEW')) {
+	header('HTTP/1.0 403 Forbidden');
+	exit;
+}
+
+class lightbox_Tab extends media_Tab {
+
+	public function getPreLoadContent() {
+		$out = '';
+		ob_start();
+		include_once('modules/lightbox/functions/lb_call_js.php');
+		$out .= ob_get_contents();
+		ob_end_clean();
+		return $out; 
+	}
+	
+	public function getJSCallbackAllTabs() {
+		return 'CB_Init();';
+	}
+	
+	public function getContent() {
+		global $MULTI_MEDIA, $SHOW_ID_NUMBERS, $MEDIA_EXTERNAL;
+		global $pgv_lang, $pgv_changes, $factarray, $view;
+		global $GEDCOM, $MEDIATYPE, $pgv_changes;
+		global $WORD_WRAPPED_NOTES, $MEDIA_DIRECTORY, $PGV_IMAGE_DIR, $PGV_IMAGES, $TEXT_DIRECTION, $is_media;
+		global $cntm1, $cntm2, $cntm3, $cntm4, $t, $mgedrec ;
+		global $edit ;
+		global $CONTACT_EMAIL, $pid, $tabno;
+		global $Fam_Navigator, $NAV_ALBUM;
+
+		$out = "<div id=\"lightbox2\">";
+		$out .= "<span class=\"subheaders\">" . $pgv_lang["lightbox"] . "</span>";
+		$out .= "&nbsp;&nbsp;";
+		ob_start();
+		// ---------- Help link --------------------
+		print_help_link("lb_general_help", "qm", "lb_help", true);
+		// --------- Header include -------------
+		$mediacnt = $this->get_media_count();
+		require_once 'modules/lightbox/functions/lb_head.php';
+		echo "<div id=\"lightbox2_content\">";
+
+		$media_found = false;
+		if (!$this->controller->indi->canDisplayDetails()) {
+			print "<table class=\"facts_table\" cellpadding=\"0\">\n";
+			print "<tr><td class=\"facts_value\">";
+			print_privacy_error($CONTACT_EMAIL);
+			print "</td></tr>";
+			print "</table>";
+		}else{
+			if (file_exists("modules/lightbox/album.php")) {
+				include_once('modules/lightbox/album.php');
+			}
+		}
+		echo "</div>";
+
+		$out .= ob_get_contents();
+		ob_end_clean();
+		$out .= "</div>";
+		return $out;
+	}
+
+	public function hasContent() {
+		global $MULTI_MEDIA;
+		return $MULTI_MEDIA && $this->get_media_count()>0;
+	}
+}
+?>
---
+++ phpGedView/modules/lightbox/pgv_module.php
@@ -0,0 +1,48 @@
+<?php
+require_once("includes/classes/class_module.php");
+require_once("modules/lightbox/lightbox.php");
+
+// Load PGV embeding language file
+global $language_settings, $LANGUAGE, $pgv_lang, $factarray;
+
+// Load other language file if needed
+if (isset($LANGUAGE)) {
+	require_once 'modules/lightbox/languages/lang.en.php';
+	if($language_settings[$LANGUAGE]['lang_short_cut'] != 'en' && file_exists("modules/lightbox/languages/lang.{$language_settings[$LANGUAGE]['lang_short_cut']}.php")){require_once "modules/lightbox/languages/lang.{$language_settings[$LANGUAGE]['lang_short_cut']}.php";}
+}
+
+class lightbox_PGVModule extends PGVModule {
+	protected $name = 'lightbox';
+	protected $description = 'Adds a tab (Album) to the individual page which an alternate way to view and work with media.';
+	protected $version = '4.2.2';
+	protected $pgvVersion = '4.2.2';
+	protected $configLink = 'module.php?mod=lightbox&pgvaction=lb_editconfig';
+	protected $_tab = null;
+
+	/**
+	 * get the tab for this
+	 * @return Tab
+	 */
+	public function &getTab() {
+		if ($this->_tab==null) {
+			$this->_tab = new lightbox_Tab();
+			$this->_tab->setName($this->getName());
+		}
+		return $this->_tab;
+	}
+
+	/**
+	 * does this module implement a menu
+	 * should be overidden in extending classes
+	 * @return boolean
+	 */
+	public function hasMenu() { return false; }
+	/**
+	 * does this module implement a tab
+	 * should be overidden in extending classes
+	 * @return boolean
+	 */
+	public function hasTab() { return true; }
+
+}
+?>
