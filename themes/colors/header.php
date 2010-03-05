a0d671d471e3cd498ff7f672029dc768871627a2
NAME <USER@DOMAIN>
2010-03-05 21:35:28 +0000

    Update SVN special properties
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@7170 4201c862-f99b-1943-94d0-25fd05010cbc

--- themes/colors/header.php
+++ themes/colors/header.php
@@ -3,7 +3,7 @@
  * Header for colors theme
  *
  * PhpGedView: Genealogy Viewer
- * Copyright (c) 2002 to 2009  John Finlay and others.  All rights reserved.
+ * Copyright (C) 2010  PGV Development Team.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
