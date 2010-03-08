25f7cc214ca92d341228cdf01ed8e802beea326d
NAME <USER@DOMAIN>
2010-03-08 18:13:50 +0000

    Move tranlations from PHP arrays to gettext
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@7255 4201c862-f99b-1943-94d0-25fd05010cbc

--- pedigree.php
+++ pedigree.php
@@ -49,7 +49,7 @@ if (PGV_USE_LIGHTBOX) {
 
 echo '<table><tr><td valign="middle">';
 if ($controller->isPrintPreview()) {
-	echo "<h2>", str_replace("#PEDIGREE_GENERATIONS#", $PEDIGREE_GENERATIONS, $pgv_lang["gen_ped_chart"]), ":";
+	echo "<h2>", i18n::translate('%s Generation Pedigree Chart', $PEDIGREE_GENERATIONS), ":";
 } else {
 	echo "<h2>", i18n::translate('Pedigree Tree'), ":";
 }
