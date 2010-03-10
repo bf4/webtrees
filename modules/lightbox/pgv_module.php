e1731a13f723a1d6b239e49bf1fd7347cac66985
NAME <USER@DOMAIN>
2010-03-10 12:29:50 +0000

    Remove hard-coded reference to module language files
    git-svn-id: svn://svn.webtrees.net/trunk/phpGedView@7321 4201c862-f99b-1943-94d0-25fd05010cbc

--- modules/lightbox/pgv_module.php
+++ modules/lightbox/pgv_module.php
@@ -2,15 +2,6 @@
 require_once("includes/classes/class_module.php");
 require_once("modules/lightbox/lightbox.php");
 
-// Load PGV embeding language file
-global $language_settings, $LANGUAGE;
-
-// Load other language file if needed
-if (isset($LANGUAGE)) {
-	require_once 'modules/lightbox/languages/lang.en.php';
-	if($language_settings[$LANGUAGE]['lang_short_cut'] != 'en' && file_exists("modules/lightbox/languages/lang.{$language_settings[$LANGUAGE]['lang_short_cut']}.php")){require_once "modules/lightbox/languages/lang.{$language_settings[$LANGUAGE]['lang_short_cut']}.php";}
-}
-
 class lightbox_PGVModule extends PGVModule {
 	protected $name = 'lightbox';
 	protected $description = 'Adds a tab (Album) to the individual page which an alternate way to view and work with media.';
