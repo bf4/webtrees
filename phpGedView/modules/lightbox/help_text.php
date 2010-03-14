cc7d98665f05c73b7f69289ee2b2536fd6d68db9
NAME <USER@DOMAIN>
2010-03-14 19:53:05 +0000

    git-svn-id: svn://svn.webtrees.net/trunk@7451 4201c862-f99b-1943-94d0-25fd05010cbc

--- phpGedView/modules/lightbox/help_text.php
+++ phpGedView/modules/lightbox/help_text.php
@@ -47,22 +47,22 @@ case 'lb_tt_balloon':
 	
 case 'mediatab':
 	$title=i18n::translate('Media Tab Appearance');
-	$text=('This option lets you determine whether the Media tab should be shown on the #pgv_lang[indi_info]# page.<br /><br />When this option is set to <b>#pgv_lang[hide]#</b>, only the <b>#pgv_lang[lightbox]#</b> tab will be shown.<br />');
+	$text=('This option lets you determine whether the Media tab should be shown on the <i18n::translate($indi_info)> page.<br /><br />When this option is set to <b><i18n::translate($hide)></b>, only the <b><i18n::translate($lightbox)></b> tab will be shown.<br />');
 	break;
 
 case 'lb_al_head_links':
 	$title=i18n::translate('Album Tab Header Link appearance');
-	$text=('This option lets you determine whether the header area of the #pgv_lang[lightbox]# tab, which contains links to control various aspects of the Lightbox module, should contain only icons, only text, or both.<br /><br />The <b>#pgv_lang[lb_icon]#</b> option is probably not very useful, since you won't see any indication of each icon's function until your mouse hovers over the icon.<br />');
+	$text=('This option lets you determine whether the header area of the <i18n::translate($lightbox)> tab, which contains links to control various aspects of the Lightbox module, should contain only icons, only text, or both.<br /><br />The <b><i18n::translate($lb_icon)></b> option is probably not very useful, since you won\'t see any indication of each icon\'s function until your mouse hovers over the icon.<br />');
 	break;
 
 case 'lb_al_thumb_links':
-	$title=i18n::translate('lbum Tab Thumbnails Link appearance');
+	$title=i18n::translate('Album Tab Thumbnails Link appearance');
 	$text=('This option lets you determine whether the links area below each thumbnail should show an icon or text.  The links shown here let you edit the Media object\'s details or delete it.<br />');
 	break;
 
 case 'lb_ml_thumb_links':
 	$title=i18n::translate('Thumbnails Link appearance');
-	$text=('This option lets you determine whether the Links area above the Media object\'s details in the MultiMedia list should contain only icons, only text, or both.  The links shown here let you perform various editing actions on the Media object in question.<br /><br />The <b>#pgv_lang[lb_none]#</b> option completely hides these links, and thus acts as if the user did not have any editing rights.<br />');
+	$text=('This option lets you determine whether the Links area above the Media object\'s details in the MultiMedia list should contain only icons, only text, or both.  The links shown here let you perform various editing actions on the Media object in question.<br /><br />The <b><i18n::translate($lb_none)></b> option completely hides these links, and thus acts as if the user did not have any editing rights.<br />');
 	break;
 
 case 'lb_ss_speed':
@@ -77,12 +77,12 @@ case 'lb_music_file':
 
 case 'lb_transition':
 	$title=i18n::translate('Image Transition speed');
-	$text=('This option lets you specify the transition speed when the image changes.  This selection is applied during the slideshow.  It is also applied when you move to the next or previous image when the slideshow is not running.<br /><br />The <b>#pgv_lang[lb_none]#</b> option eliminates image transitions so that the new image immediately replaces the old without visible adjustment of the new image\'s dimensions.<br />');
+	$text=('This option lets you specify the transition speed when the image changes.  This selection is applied during the slideshow.  It is also applied when you move to the next or previous image when the slideshow is not running.<br /><br />The <b><i18n::translate($lb_none)></b> option eliminates image transitions so that the new image immediately replaces the old without visible adjustment of the new image\'s dimensions.<br />');
 	break;
 
 case 'lb_url_dimensions':
 	$title=i18n::translate('Lightbox URL Window dimensions');
 	$text=('When clicking on a URL image thumbnail, this option lets you specify the Lightbox URL Window dimensions in pixels.<br /><br />This should normally be less than your current browser window dimensions, and certainly less than your screen resolution.<br />');
 	break;
-	
+
 }
