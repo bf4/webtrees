42075241a828b0ff26e09439489b0a35096c6acc
NAME <USER@DOMAIN>
2010-03-21 13:39:46 +0000

    Remove old language-specific code
    git-svn-id: svn://svn.webtrees.net/trunk@7578 4201c862-f99b-1943-94d0-25fd05010cbc

--- phpGedView/includes/functions/functions.php
+++ phpGedView/includes/functions/functions.php
@@ -3517,20 +3517,6 @@ function loadLanguage($desiredLanguage="english", $forceLoad=false) {
 	global $JEWISH_ASHKENAZ_PRONUNCIATION, $CALENDAR_FORMAT;
 	global $DBTYPE, $DB_UTF8_COLLATION, $COLLATION, $DBCOLLATE;
 
-	// Need to change the collation sequence each time we change language
-	if ($DB_UTF8_COLLATION) {
-		switch ($DBTYPE) {
-		case 'mysql':
-			$DBCOLLATE="COLLATE {$COLLATION[$LANGUAGE]}";
-			break;
-		default:
-			$DBCOLLATE='';
-			break;
-		}
-	} else {
-		$DBCOLLATE='';
-	}
-
 	if (!isset($pgv_language[$desiredLanguage])) $desiredLanguage = "english";
 
 	// Make sure we start with a clean slate
