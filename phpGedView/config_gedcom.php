04418a4188cf0fdec510cc626b87ae15c812496e
NAME <USER@DOMAIN>
2010-04-03 17:32:35 +0000

    Change default of allow_change_theme to true.
    git-svn-id: svn://svn.webtrees.net/trunk@7823 4201c862-f99b-1943-94d0-25fd05010cbc

--- phpGedView/config_gedcom.php
+++ phpGedView/config_gedcom.php
@@ -136,7 +136,7 @@ $SHOW_FACT_ICONS = true;					//-- Show Fact icons on Indi page
 $FAVICON = "images/favicon.ico";		// -- change to point to your favicon, either relative or absolute
 $THEME_DIR = "themes/webtrees/";					// -- directory where display theme files are kept
 if (substr ($THEME_DIR, -1) != "/") $THEME_DIR = $THEME_DIR . "/";
-$ALLOW_THEME_DROPDOWN = false;		//-- allows the themes to display theme change dropdown
+$ALLOW_THEME_DROPDOWN = true;		//-- allows the themes to display theme change dropdown
 $SECURITY_CHECK_GEDCOM_DOWNLOADABLE = true;	//-- check for downloadability of GEDCOM
 
 $SHOW_STATS = false;					//-- Show execution stats at the bottom of the page
