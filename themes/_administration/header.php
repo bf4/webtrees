d7b99bcf1e17aa794961c10a73b4b09cd5b220b6
NAME <USER@DOMAIN>
2010-12-16 22:26:53 +0000

    Admin menu - restrict managers to certain links and simplify layout
    git-svn-id: svn://svn.webtrees.net/branches/admin-gui@10098 4201c862-f99b-1943-94d0-25fd05010cbc

--- themes/_administration/header.php
+++ themes/_administration/header.php
@@ -38,85 +38,105 @@ echo
 	'<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />',
 	'<link rel="stylesheet" href="', WT_THEME_DIR, 'jquery/jquery-ui_theme.css" type="text/css" />',
 	'<link rel="stylesheet" href="', $stylesheet, '" type="text/css" media="all" />',
-	$javascript;
-//	$head; //-- additional header information
-
-echo
+	$javascript,
+//	$head, //-- additional header information
 	'</head>',
 	'<body id="body">',
 // Header
 	'<div id="admin_head" class="ui-widget-content">',
-		'<div id="info">',
-			WT_WEBTREES, ' ', WT_VERSION_TEXT,
-			'<br />',
-			i18n::translate('Current Server Time:'), ' ', format_timestamp(time()),
-			'<br />',
-			i18n::translate('Current User Time:'), ' ', format_timestamp(client_time()),
-		'</div>',
-		'<div id="title">', i18n::translate('Administration'), '</div>',
+	'<div id="info">',
+	WT_WEBTREES, ' ', WT_VERSION_TEXT,
+	'<br />',
+	i18n::translate('Current Server Time:'), ' ', format_timestamp(time()),
+	'<br />',
+	i18n::translate('Current User Time:'), ' ', format_timestamp(client_time()),
+	'</div>',
+	'<div><a href="administration.php" id="title">', i18n::translate('Administration'), '</a></div>',
 	'</div>',
 // Side menu 
 	'<div id="admin_menu" class="ui-widget-content">',
-		'<ul>',
-			'<li>';	$class=""; if (WT_SCRIPT_NAME=="administration.php") {$class="current";} echo '<a class="',$class,'" href="administration.php">', i18n::translate('Administration'), '</a></li>',
-			'<li><a href="index.php?ctype=user">', i18n::translate('Back to My Page'), '</li>',	
-			'<li>';	$class=""; if (WT_SCRIPT_NAME=="admin_site_config.php") {$class="current";} echo '<a class="',$class, '" href="admin_site_config.php">', i18n::translate('Site'), '</a>',
-				'<ul>',
-					'<li>';	$class=""; if (WT_SCRIPT_NAME=="admin_site_config.php") {$class="current";} echo '<a class="' ,$class, '" href="admin_site_config.php">', i18n::translate('Server configuration'), '</a></li>',
-					'<li>';	$class=""; if (WT_SCRIPT_NAME=="admin_site_logs.php") {$class="current";} echo '<a class="' ,$class, '" href="admin_site_logs.php">', i18n::translate('Logs'), '</a></li>',
-					'<li>';	$class=""; if (WT_SCRIPT_NAME=="admin_site_readme.php") {$class="current";} echo '<a class="' ,$class, '" href="admin_site_readme.php">', i18n::translate('README documentation'), '</a></li>',
-					'<li>';	$class=""; if (WT_SCRIPT_NAME=="admin_site_info.php") {$class="current";} echo '<a class="' ,$class, '" href="admin_site_info.php?action=phpinfo">', i18n::translate('PHP information'), '</a></li>',
-					'<li>';	$class=""; if (WT_SCRIPT_NAME=="admin_site_manageservers.php") {$class="current";} echo '<a class="' ,$class, '" href="admin_site_manageservers.php">', i18n::translate('Manage servers'), '</a></li>',
-				'</ul>',
-			'</li>',
-			'<li>';	$class=""; if (WT_SCRIPT_NAME=="admin_trees_manage.php") {$class="current";} echo '<a class="' ,$class, '" href="admin_trees_manage.php">', i18n::translate('Family trees'), '</a>',
-				'<ul>',
-					'<li>';	$class=""; if (WT_SCRIPT_NAME=="admin_trees_manage.php") {$class="current";} echo '<a class="' ,$class, '" href="admin_trees_manage.php">', i18n::translate('Manage GEDCOMs'), '</a></li>',
-					'<li><span>', i18n::translate('Configure these trees'), ' :</span>',
-						'<ul>';
-							//-- gedcom list
-							$gedcom_titles=get_gedcom_titles();
-							if (get_site_setting('ALLOW_CHANGE_GEDCOM')) {
-								foreach ($gedcom_titles as $gedcom_title) {
-									$pagename = WT_SCRIPT_NAME;
-									$class=""; if ($gedcom_title->gedcom_title==PrintReady(get_gedcom_setting(WT_GED_ID, 'title')) && $pagename == 'admin_trees_config.php') {$class="current";} 
-									echo '<li><a class="', $class, '" href="admin_trees_config.php?ctype=gedcom&amp;ged='.rawurlencode($gedcom_title->gedcom_name), '">', $gedcom_title->gedcom_title,'</a></li>';					
-								}
-							}
-						echo '</ul>',
-					'</li>',
-				'</ul>',
-			'</li>',
-			'<li>';	$class=""; if (WT_SCRIPT_NAME=="admin_users_list.php") {$class="current";} echo '<a class="' ,$class, '" href="admin_users_list.php">', i18n::translate('Users'), '</a>',
-				'<ul>',
-					'<li>';	$class=""; if (WT_SCRIPT_NAME=="admin_users_list.php") {$class="current";} echo '<a class="' ,$class, '" href="admin_users_list.php">', i18n::translate('List users'), '</a></li>',
-					'<li>';	$class=""; if (WT_SCRIPT_NAME=="admin_users_add.php") {$class="current";} echo '<a class="' ,$class, '" href="admin_users_add.php?action=createform">', i18n::translate('Add user'), '</a></li>',
-					'<li>';	$class=""; if (WT_SCRIPT_NAME=="#") {$class="current";} echo '<a class="' ,$class, '" href="#">', i18n::translate('Bulk messaging'), '</a></li>',
-				'</ul>',
-			'</li>',
-			'<li><a href="#">', i18n::translate('Modules'), '</a>',
-				'<ul>',
-					'<li><a href="#">', i18n::translate('Manage modules'), '</a></li>',
-					'<li><a href="##tabs">', i18n::translate('Tabs - manage'), '</a></li>',
-					'<li><a href="##sidebars">', i18n::translate('Sidebar - manage'), '</a></li>',
-					'<li><a href="#.ui-menus">', i18n::translate('Menus - manage'), '</a></li>',
-					'<li><span>-----------------------------</span>',
-						'<ul>';
-							foreach (WT_Module::getInstalledModules() as $module) {
-								$status=WT_DB::prepare("SELECT status FROM `##module` WHERE module_name=?")->execute(array($module->getName()))->fetchOne();
-								if ($module instanceof WT_Module_Config)
-									echo '<li><a href="', $module->getConfigLink(), '">', $module->getTitle(), ' - configure</a></li>';
-							}
-						echo '</ul>',
-					'</li>',
-			'<li><a href="#">', i18n::translate('Media'), '</a>',
-				'<ul>',
-					'<li><a href="#">', i18n::translate('Manage media'), '</a></li>',
-					'<li><a href="#">', i18n::translate('Upload media'), '</a></li>',
-					'<li><a href="#">', i18n::translate('Media firewall'), '</a><li>',
-				'</ul>',
-			'</li>',
-		'</ul>',
+	'<ul>',
+	'<li><a ', (WT_SCRIPT_NAME=="administration.php" ? 'class="current" ' : ''), 'href="administration.php">',
+	i18n::translate('Administration'),
+	'</a></li>';
+if (WT_USER_IS_ADMIN) {
+	echo
+		'<li><ul>',
+		'<li><a ', (WT_SCRIPT_NAME=="admin_site_config.php" ? 'class="current" ' : ''), 'href="admin_site_config.php">',
+		i18n::translate('Server configuration'),
+		'</a></li>',
+		'<li><a ', (WT_SCRIPT_NAME=="admin_site_logs.php" ? 'class="current" ' : ''), 'href="admin_site_logs.php">',
+		i18n::translate('Logs'),
+		'</a></li>',
+		'<li><a ', (WT_SCRIPT_NAME=="admin_site_readme.php" ? 'class="current" ' : ''), 'href="admin_site_readme.php">', i18n::translate('README documentation'), '</a></li>',
+		'<li><a ', (WT_SCRIPT_NAME=="admin_site_info.php" ? 'class="current" ' : ''), 'href="admin_site_info.php?action=phpinfo">', i18n::translate('PHP information'), '</a></li>',
+		'<li><a ', (WT_SCRIPT_NAME=="admin_site_manageservers.php" ? 'class="current" ' : ''), 'href="admin_site_manageservers.php">', i18n::translate('Manage servers'), '</a></li>',
+		'</ul></li>',
+		'<li><a ', (WT_SCRIPT_NAME=="admin_trees_manage.php" ? 'class="current" ' : ''), 'href="admin_trees_manage.php">',
+		i18n::translate('Family trees'),
+		'</a></li>';
+} else {
+	echo '<li>', i18n::translate('Family trees'), '</li>';
+}
+echo '<li><ul>';
+//-- gedcom list
+foreach (get_all_gedcoms() as $ged_id=>$gedcom) {
+	if (userGedcomAdmin(WT_USER_ID, $ged_id)) {
+		echo
+			'<li><a ', (WT_SCRIPT_NAME=="admin_trees_config.php" && WT_GED_ID==$ged_id ? 'class="current" ' : ''), 'href="admin_trees_config.php?ged='.rawurlencode($gedcom).'">',
+			htmlspecialchars(get_gedcom_setting($ged_id, 'title')),
+			'</a></li>';
+	}
+}
+echo '</ul></li>';
+if (WT_USER_IS_ADMIN) {
+	echo
+		'<li><a ', (WT_SCRIPT_NAME=="admin_users_list.php" ? 'class="current" ' : ''), 'href="admin_users_list.php">',
+		i18n::translate('Users'),
+		'</a></li>',
+		'<li><ul>',
+		'<li><a ', (WT_SCRIPT_NAME=="admin_users_add.php" ? 'class="current" ' : ''), 'href="admin_users_add.php?action=createform">', i18n::translate('Add user'), '</a></li>',
+		'<li><a ', (WT_SCRIPT_NAME=="#" ? 'class="current" ' : ''), 'href="administration.php">',
+		i18n::translate('Bulk messaging'),
+		'</a></li>',
+		'</ul></li>',
+		'<li><a ', (WT_SCRIPT_NAME=="#" ? 'class="current" ' : ''), 'href="administration.php">',
+		i18n::translate('Modules'),
+		'</a></li>',
+		'<li><ul>',
+		'<li><a ', (WT_SCRIPT_NAME=="#" ? 'class="current" ' : ''), 'href="administration.php">',
+		i18n::translate('Tabs'),
+		'</a></li>',
+		'<li><a ', (WT_SCRIPT_NAME=="#" ? 'class="current" ' : ''), 'href="administration.php">',
+		i18n::translate('Sidebar'),
+		'</a></li>',
+		'<li><a ', (WT_SCRIPT_NAME=="#" ? 'class="current" ' : ''), 'href="administration.php">',
+		i18n::translate('Menus'),
+		'</a></li>';
+	foreach (WT_Module::getInstalledModules() as $module) {
+		if ($module instanceof WT_Module_Config) {
+			echo '<li><i><a href="', $module->getConfigLink(), '">', $module->getTitle(), '</a></i></li>';
+		}
+	}
+	echo
+		'</ul></li>',
+		'<li><a ', (WT_SCRIPT_NAME=="#" ? 'class="current" ' : ''), 'href="administration.php">',
+		i18n::translate('Media'),
+		'</a></li>',
+		'<li><ul>',
+		'<li><a ', (WT_SCRIPT_NAME=="#" ? 'class="current" ' : ''), 'href="administration.php">',
+		i18n::translate('Manage media'),
+		'</a></li>',
+		'<li><a ', (WT_SCRIPT_NAME=="#" ? 'class="current" ' : ''), 'href="administration.php">',
+		i18n::translate('Upload media'),
+		'</a></li>',
+		'<li><a ', (WT_SCRIPT_NAME=="#" ? 'class="current" ' : ''), 'href="administration.php">',
+		i18n::translate('Media firewall'),
+		'</a><li>',
+		'</ul></li>';
+}
+echo
+	'</ul>',
 	'</div>',
-// Content -->
-'<div id="admin_content" class="ui-widget-content">';
+	// Content -->
+	'<div id="admin_content" class="ui-widget-content">';
